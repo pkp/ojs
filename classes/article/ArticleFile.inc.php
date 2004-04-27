2a17125797029db338509e14dfe723cbd2ca8d9c
vikram <vikram>
2004-04-27 23:24:01 +0000

    initial version of handling article files and corresponding database entries

--- classes/article/ArticleFile.inc.php
+++ classes/article/ArticleFile.inc.php
@@ -14,7 +14,17 @@
  */
 
 class ArticleFile extends DataObject {
+
+	/**
+	 * Constructor.
+	 */
+	function ArticleFile() {
+		parent::DataObject();
+	}
 	
+	//
+	// Get/set methods
+	//
 	/**
 	 * Get ID of file.
 	 * @return int
@@ -30,6 +40,126 @@ class ArticleFile extends DataObject {
 	function setFileId($fileId) {
 		return $this->setData('fileId', $fileId);
 	}
+	
+	
+	/**
+	 * Get ID of article.
+	 * @return int
+	 */
+	function getArticleId() {
+		return $this->getData('articleId');
+	}
+	
+	/**
+	 * Set ID of article.
+	 * @param $articleId int
+	 */
+	function setArticleId($articleId) {
+		return $this->setData('articleId', $articleId);
+	}
+	
+	/**
+	 * Get file name of the file.
+	 * @ return string
+	 */
+	function getFileName($fileName) {
+		return $this->getData('fileName');	
+	}
+	
+	/**
+	 * Set file name of the file.
+	 * @param $fileName string
+	 */
+	function setFileName($fileName) {
+		return $this->setData('fileName', $fileName);	
+	}
+	
+	/**
+	 * Get file type of the file.
+	 * @ return string
+	 */
+	function getFileType($fileType) {
+		return $this->getData('fileType');	
+	}
+	
+	/**
+	 * Set file type of the file.
+	 * @param $fileType string
+	 */
+	function setFileType($fileType) {
+		return $this->setData('fileType', $fileType);	
+	}
+	
+	/**
+	 * Get type of the file.
+	 * @ return string
+	 */
+	function getType($type) {
+		return $this->getData('type');	
+	}
+	
+	/**
+	 * Set type of the file.
+	 * @param $type string
+	 */
+	function setType($type) {
+		return $this->setData('type', $type);	
+	}
+	
+	/**
+	 * Get status of the file.
+	 * @return string
+	 */
+	function getStatus($status) {
+		return $this->getData('status');	
+	}
+	
+	/**
+	 * Set status of the file.
+	 * @param $status string
+	 */
+	function setStatus($type) {
+		return $this->setData('status', $status);	
+	}
+	
+	/**
+	 * Get uploaded date of file.
+	 * @return date
+	 */
+	
+	function getDateUploaded() {
+		return $this->getData('dateUploaded');	
+	}
+	
+
+	/**
+	 * Set uploaded date of file.
+	 * @param $dateUploaded date
+	 */
+	 
+	function setDateUploaded($dateUploaded) {
+		return $this->SetData('dateUploaded', $dateUploaded);
+	}
+	
+	/**
+	 * Get modified date of file.
+	 * @return date
+	 */
+	
+	function getDateModified() {
+		return $this->getData('dateModified');	
+	}
+	
+
+	/**
+	 * Set modified date of file.
+	 * @param $dateModified date
+	 */
+	 
+	function setDateModified($dateModified) {
+		return $this->SetData('dateModified', $dateModified);
+	}
+	
 	
 }
 
---
+++ classes/article/ArticleFileDAO.inc.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * ArticleFileDAO.inc.php
+ *
+ * Copyright (c) 2003-2004 The Public Knowledge Project
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package article
+ *
+ * Class for ArticleFile DAO.
+ * Operations for retrieving and modifying ArticleFile objects.
+ *
+ * $Id$
+ */
+
+class ArticleFileDAO extends DAO {
+
+
+	/**
+	 * Constructor.
+	 */
+	function ArticleFileDAO() {
+		parent::DAO();
+	}
+	
+	/**
+	 * Retrieve an article by ID.
+	 * @param $articleId int
+	 * @return Article
+	 */
+	function &getArticleFile($fileId) {
+		$result = &$this->retrieve(
+			'SELECT a.* FROM article_files a WHERE file_id = ?', $fileId
+		);
+		
+		if ($result->RecordCount() == 0) {
+			return null;
+		} else {
+			return $this->_returnArticleFileFromRow($result->GetRowAssoc(false));
+		}
+	}
+	
+	/**
+	 * Internal function to return an ArticleFile object from a row.
+	 * @param $row array
+	 * @return Article
+	 */
+	function &_returnArticleFileFromRow(&$row) {
+		$articleFile = &new ArticleFile();
+		$articleFile->setFileId($row['file_id']);
+		$articleFile->setArticleId($row['article_id']);
+		$articleFile->setFileName($row['file_name']);
+		$articleFile->setFileType($row['file_type']);
+		$articleFile->setStatus($row['status']);
+		$articleFile->setDateUploaded($row['date_uploaded']);
+		$articleFile->setDateModified($row['date_modified']);
+		return $articleFile;
+	}
+
+	/**
+	 * Insert a new ArticleFile.
+	 * @param $articleFile ArticleFile
+	 */	
+	function insertArticle(&$articleFile) {
+		$this->update(
+			'INSERT INTO article_files
+				(article_id, file_name, file_type, status, date_uploaded, date_modified)
+				VALUES
+				(?, ?, ?, ?, ?, ?)',
+			array(
+				$articleFile->getArticleId(),
+				$articleFile->getFileName(),
+				$articleFile->getFileType(),
+				$articleFile->getStatus(),
+				$articleFile->getDateUploaded(),
+				$articleFile->getDateModified()
+			)
+		);
+		
+		$articleFile->setFileId($this->getInsertArticleFileId());
+		
+	}
+	
+	/**
+	 * Update an existing article file.
+	 * @param $article ArticleFile
+	 */
+	function updateArticleFile(&$articleFile) {
+		$this->update(
+			'UPDATE article_files
+				SET
+					article_id = ?,
+					file_name = ?,
+					file_type = ?,
+					status = ?,
+					date_uploaded = ?,
+					date_modified = ?
+				WHERE file_id = ?',
+			array(
+				$articleFile->getArticleId(),
+				$articleFile->getFileName(),
+				$articleFile->getFileType(),
+				$articleFile->getStatus(),
+				$articleFile->getDateUploaded(),
+				$articleFile->getDateModified()
+			)
+		);
+		
+	}
+	
+	/**
+	 * Delete an article file.
+	 * @param $article ArticleFile
+	 */
+	function deleteArticle(&$articleFile) {
+		return $this->deleteArticleById($articleFile->getArticleFileId());
+	}
+	
+	/**
+	 * Delete an article file by ID.
+	 * @param $articleId int
+	 */
+	function deleteArticleById($fileId) {
+		return $this->update(
+			'DELETE FROM article_files WHERE file_id = ?', $fileId
+		);
+	}
+	
+	/**
+	 * Get the ID of the last inserted article file.
+	 * @return int
+	 */
+	function getInsertArticleFileId() {
+		return $this->getInsertId('article_files', 'file_id');
+	}
+	
+}
+
+?>
