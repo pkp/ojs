5d8a09e33b500ba7a388d71de4e73a3cd61b253b
vikram <vikram>
2004-05-03 22:52:17 +0000

    added upload functions

--- classes/file/ArticleFileManager.inc.php
+++ classes/file/ArticleFileManager.inc.php
@@ -18,13 +18,15 @@ class ArticleFileManager extends FileManager {
 	
 	/** @var string the path to location of the files */
 	var $filesDir;
+	var $articleId;
 	
 	/**
 	* Constructor.
 	* Create a manager for handling article file uploads.
 	* @param $articleId int
 	*/
-	function ArticleFileManager($articleId) {	
+	function ArticleFileManager($articleId) {
+		$this->articleId = $articleId;
 		$articleDao = &DAORegistry::getDAO('ArticleDAO');
 		$article = &$articleDao->getArticle($articleId);
 		$journalId = $article->getJournalId();
@@ -33,33 +35,92 @@ class ArticleFileManager extends FileManager {
 	}
 	
 	/**
-	* Upload a file.
+	* Upload a submission file.
 	* @param $fileName string the name of the file used in the POST form
 	* @param $dest string the path where the file is to be saved
-	 * @return boolean returns true if successful
+	* @return $articleFile is null if failure
 	*/
 	function uploadSubmissionFile($fileName, $fileId = null) {
-		return uploadFile($fileName, $this->filesDir . "submission/");
+		return handleUpload($fileName, $this->filesDir . "submission/", "submission", $fileId);
 	}
-	
+	/**
+	* Upload a supp file.
+	* @param $fileName string the name of the file used in the POST form
+	* @param $dest string the path where the file is to be saved
+	* @return $articleFile is null if failure
+	*/
 	function uploadSuppFile($fileName, $fileId = null) {
-		return uploadFile($fileName, $this->filesDir . "supp/");
+		return handleUpload($fileName, $this->filesDir . "supp/", "supp", $fileId);
 	}
 	
+	/**
+	* Upload a review file.
+	* @param $fileName string the name of the file used in the POST form
+	* @param $dest string the path where the file is to be saved
+	* @return $articleFile is null if failure
+	*/
 	function uploadReviewFile($fileName, $fileId = null) {
-		return uploadFile($fileName, $this->filesDir . "review/");
+		return handleUpload($fileName, $this->filesDir . "review/", "review", $fileId);
 	}
 	
+	/**
+	* Upload a public file.
+	* @param $fileName string the name of the file used in the POST form
+	* @param $dest string the path where the file is to be saved
+	* @return $articleFile is null if failure
+	*/
 	function uploadPublicFile($fileName, $fileId = null) {
-		return uploadFile($fileName, $this->filesDir . "public/");
+		return handleUpload($fileName, $this->filesDir . "public/", "public", $fileId);
 	}
 	
 	function downloadFile($fileId) {
 		// get the files path and type
+		$articleFileDao = &DAORegistry::getDAO('ArticleFileDAO');
+		$articleFile = new ArticleFile;
+		$articleFile = $articleFileDao->getArticleFile($fileId);
+		$fileType = $articleFile->getFileType();
+		$type = $articleFile->getType();
+		$fileName = $articleFile->getFileName();
+		$filePath = $this->filesDir . "$type/$fileName"
 		
-		FileManager::downloadFile($filePath, $type);
+		FileManager::downloadFile($filePath, $fileType);
 	}
 	
+	// private
+	
+	function handleUpload($fileName, $dir, $type, $fileId = null) {
+		$articleFileDao = &DAORegistry::getDAO('ArticleFileDAO');
+		$articleFile = new ArticleFile;
+		
+		if ($fileId == null) {
+			if (uploadFile($fileName, $dir)) {
+				$articleFile->setArticleId($this->articleId);
+				$articleFile->setFileName($fileName);
+				$articleFile->setFileType($_FILES[$fileName][type]);
+				$articleFile->setType($type);
+				$articleFile->setDateUploaded(date("Y-m-d g:i:s");
+				$articleFile->setDateModified(date("Y-m-d g:i:s");
+				return $articleFileDao->insertArticleFile($articleFile);
+			} else {
+				return null;
+			}
+		} else {
+			$articleFile = $articleFileDao->getArticleFile($fileId);
+			// unlink old file
+			deleteFile($dir . $articleFile->getFileName());
+			// upload new file
+			if (uploadFile($fileName, $dir)) {
+				// update database entry for file
+				$articleFile->setFileName($fileName);
+				$articleFile->setFileType($_FILES[$fileName][type]);
+				$articleFile->setType($type);
+				$articleFile->setDateModified(date("Y-m-d g:i:s");
+				return $articleFileDao->updateArticleFile($articleFile);
+			} else {
+				return null;	
+			}
+		}	
+	}
 }
 
 ?>
