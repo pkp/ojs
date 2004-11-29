13cb753fa7817a953b1e8e2ff2dfe53f80b4b72f
jasonc <jasonc>
2004-11-29 09:14:11 +0000

    Proofreading Proof Assignment objects for Submission Editing

---
+++ classes/submission/proofAssignment/ProofAssignment.inc.php
@@ -0,0 +1,388 @@
+<?php
+
+/**
+ * ProofAssignment.inc.php
+ *
+ * Copyright (c) 2003-2004 The Public Knowledge Project
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package submission
+ *
+ * ProofAssignment class.
+ * Describes proofread assignment properties.
+ *
+ * $Id$
+ */
+
+class ProofAssignment extends DataObject {
+
+	/**
+	 * Constructor.
+	 */
+	function ProofAssignment() {
+		parent::DataObject();
+	}
+	
+	/**
+	 * Get ID of proof assignment.
+	 * @return int
+	 */
+	function getProofId() {
+		return $this->getData('proofId');
+	}
+	
+	/**
+	 * Set ID of proof assignment
+	 * @param $proofId int
+	 */
+	function setProofId($proofId) {
+		return $this->setData('proofId', $proofId);
+	}
+	
+	/**
+	 * Get ID of article.
+	 * @return int
+	 */
+	function getArticleId() {
+		return $this->getData('articleId');
+	}
+	
+	/**
+	 * Set ID of article.
+	 * @param $articleId int
+	 */
+	function setArticleId($articleId) {
+		return $this->setData('articleId', $articleId);
+	}
+	
+	/**
+	 * Get ID of proofreader.
+	 * @return int
+	 */
+	function getProofreaderId() {
+		return $this->getData('proofreaderId');
+	}
+	
+	/**
+	 * Set ID of proofreader.
+	 * @param $proofreaderId int
+	 */
+	function setProofreaderId($proofreaderId) {
+		return $this->setData('proofreaderId', $proofreaderId);
+	}
+
+	/**
+	 * Get the scheduling queue date.
+	 * @return string
+	 */
+	function getDateSchedulingQueue() {
+		return $this->getData('dateSchedulingQueue');
+	}
+	
+	/**
+	 * Set the scheduling queue date.
+	 * @param $dateSchedulingQueue string
+	 */
+	function setDateSchedulingQueue($dateSchedulingQueue) {
+		return $this->setData('dateSchedulingQueue', $dateSchedulingQueue);
+	}
+
+	/**
+	 * Get the author comments.
+	 * @return string
+	 */
+	function getAuthorComments() {
+		return $this->getData('authorComments');
+	}
+	
+	/**
+	 * Set the author comments.
+	 * @param $authorComments string
+	 */
+	function setAuthorComments($authorComments) {
+		return $this->setData('authorComments', $authorComments);
+	}
+
+	/**
+	 * Get the author notified date.
+	 * @return string
+	 */
+	function getDateAuthorNotified() {
+		return $this->getData('dateAuthorNotified');
+	}
+	
+	/**
+	 * Set the author notified date.
+	 * @param $dateAuthorNotified string
+	 */
+	function setDateAuthorNotified($dateAuthorNotified) {
+		return $this->setData('dateAuthorNotified', $dateAuthorNotified);
+	}
+
+	/**
+	 * Get the author underway date.
+	 * @return string
+	 */
+	function getDateAuthorUnderway() {
+		return $this->getData('dateAuthorUnderway');
+	}
+	
+	/**
+	 * Set the author underway date.
+	 * @param $dateAuthorUnderway string
+	 */
+	function setDateAuthorUnderway($dateAuthorUnderway) {
+		return $this->setData('dateAuthorUnderway', $dateAuthorUnderway);
+	}
+
+	/**
+	 * Get the author completed date.
+	 * @return string
+	 */
+	function getDateAuthorCompleted() {
+		return $this->getData('dateAuthorCompleted');
+	}
+	
+	/**
+	 * Set the author completed date.
+	 * @param $dateAuthorCompleted string
+	 */
+	function setDateAuthorCompleted($dateAuthorCompleted) {
+		return $this->setData('dateAuthorCompleted', $dateAuthorCompleted);
+	}
+
+	/**
+	 * Get the author acknowledged date.
+	 * @return string
+	 */
+	function getDateAuthorAcknowledged() {
+		return $this->getData('dateAuthorAcknowledged');
+	}
+	
+	/**
+	 * Set the author acknowledged date.
+	 * @param $dateAuthorAcknowledged string
+	 */
+	function setDateAuthorAcknowledged($dateAuthorAcknowledged) {
+		return $this->setData('dateAuthorAcknowledged', $dateAuthorAcknowledged);
+	}
+
+	/**
+	 * Get the proofreader comments.
+	 * @return string
+	 */
+	function getProofreaderComments() {
+		return $this->getData('proofreaderComments');
+	}
+	
+	/**
+	 * Set the proofreader comments.
+	 * @param $proofreaderComments string
+	 */
+	function setProofreaderComments($proofreaderComments) {
+		return $this->setData('proofreaderComments', $proofreaderComments);
+	}
+
+	/**
+	 * Get the proofreader notified date.
+	 * @return string
+	 */
+	function getDateProofreaderNotified() {
+		return $this->getData('dateProofreaderNotified');
+	}
+	
+	/**
+	 * Set the proofreader notified date.
+	 * @param $dateProofreaderNotified string
+	 */
+	function setDateProofreaderNotified($dateProofreaderNotified) {
+		return $this->setData('dateProofreaderNotified', $dateProofreaderNotified);
+	}
+
+	/**
+	 * Get the proofreader underway date.
+	 * @return string
+	 */
+	function getDateProofreaderUnderway() {
+		return $this->getData('dateProofreaderUnderway');
+	}
+	
+	/**
+	 * Set the proofreader underway date.
+	 * @param $dateProofreaderUnderway string
+	 */
+	function setDateProofreaderUnderway($dateProofreaderUnderway) {
+		return $this->setData('dateProofreaderUnderway', $dateProofreaderUnderway);
+	}
+
+	/**
+	 * Get the proofreader completed date.
+	 * @return string
+	 */
+	function getDateProofreaderCompleted() {
+		return $this->getData('dateProofreaderCompleted');
+	}
+	
+	/**
+	 * Set the proofreader completed date.
+	 * @param $dateProofreaderCompleted string
+	 */
+	function setDateProofreaderCompleted($dateProofreaderCompleted) {
+		return $this->setData('dateProofreaderCompleted', $dateProofreaderCompleted);
+	}
+
+	/**
+	 * Get the proofreader acknowledged date.
+	 * @return string
+	 */
+	function getDateProofreaderAcknowledged() {
+		return $this->getData('dateProofreaderAcknowledged');
+	}
+	
+	/**
+	 * Set the proofreader acknowledged date.
+	 * @param $dateProofreaderAcknowledged string
+	 */
+	function setDateProofreaderAcknowledged($dateProofreaderAcknowledged) {
+		return $this->setData('dateProofreaderAcknowledged', $dateProofreaderAcknowledged);
+	}
+
+	/**
+	 * Get the layoutEditor comments.
+	 * @return string
+	 */
+	function getLayoutEditorComments() {
+		return $this->getData('layoutEditorComments');
+	}
+	
+	/**
+	 * Set the layoutEditor comments.
+	 * @param $layoutEditorComments string
+	 */
+	function setLayoutEditorComments($layoutEditorComments) {
+		return $this->setData('layoutEditorComments', $layoutEditorComments);
+	}
+
+	/**
+	 * Get the layoutEditor notified date.
+	 * @return string
+	 */
+	function getDateLayoutEditorNotified() {
+		return $this->getData('dateLayoutEditorNotified');
+	}
+	
+	/**
+	 * Set the layoutEditor notified date.
+	 * @param $dateLayoutEditorNotified string
+	 */
+	function setDateLayoutEditorNotified($dateLayoutEditorNotified) {
+		return $this->setData('dateLayoutEditorNotified', $dateLayoutEditorNotified);
+	}
+
+	/**
+	 * Get the layoutEditor underway date.
+	 * @return string
+	 */
+	function getDateLayoutEditorUnderway() {
+		return $this->getData('dateLayoutEditorUnderway');
+	}
+	
+	/**
+	 * Set the layoutEditor underway date.
+	 * @param $dateLayoutEditorUnderway string
+	 */
+	function setDateLayoutEditorUnderway($dateLayoutEditorUnderway) {
+		return $this->setData('dateLayoutEditorUnderway', $dateLayoutEditorUnderway);
+	}
+
+	/**
+	 * Get the layoutEditor completed date.
+	 * @return string
+	 */
+	function getDateLayoutEditorCompleted() {
+		return $this->getData('dateLayoutEditorCompleted');
+	}
+	
+	/**
+	 * Set the layoutEditor completed date.
+	 * @param $dateLayoutEditorCompleted string
+	 */
+	function setDateLayoutEditorCompleted($dateLayoutEditorCompleted) {
+		return $this->setData('dateLayoutEditorCompleted', $dateLayoutEditorCompleted);
+	}
+
+	/**
+	 * Get the layoutEditor acknowledged date.
+	 * @return string
+	 */
+	function getDateLayoutEditorAcknowledged() {
+		return $this->getData('dateLayoutEditorAcknowledged');
+	}
+	
+	/**
+	 * Set the layoutEditor acknowledged date.
+	 * @param $dateLayoutEditorAcknowledged string
+	 */
+	function setDateLayoutEditorAcknowledged($dateLayoutEditorAcknowledged) {
+		return $this->setData('dateLayoutEditorAcknowledged', $dateLayoutEditorAcknowledged);
+	}
+
+	/**
+	 * Get the proofreader's first name.
+	 * @return string
+	 */
+	function getProofreaderFirstName() {
+		return $this->getData('proofreaderFirstName');
+	}
+	
+	/**
+	 * Set the proofreader's first name.
+	 * @param $proofreaderFirstName string
+	 */
+	function setProofreaderFirstName($proofreaderFirstName) {
+		return $this->setData('proofreaderFirstName', $proofreaderFirstName);
+	}
+
+	/**
+	 * Get the proofreader's last name.
+	 * @return string
+	 */
+	function getProofreaderLastName() {
+		return $this->getData('proofreaderLastName');
+	}
+	
+	/**
+	 * Set the proofreader's last name.
+	 * @param $proofreaderLastName string
+	 */
+	function setProofreaderLastName($proofreaderLastName) {
+		return $this->setData('proofreaderLastName', $proofreaderLastName);
+	}
+
+	/**
+	 * Get the proofreader's email.
+	 * @return string
+	 */
+	function getProofreaderEmail() {
+		return $this->getData('proofreaderEmail');
+	}
+	
+	/**
+	 * Set the proofreader's email.
+	 * @param $proofreaderEmail string
+	 */
+	function setProofreaderEmail($proofreaderEmail) {
+		return $this->setData('proofreaderEmail', $proofreaderEmail);
+	}
+
+	/**
+	 * Get the proofreader's full name
+	 * @return string
+	 */
+	function getProofreaderFullName() {
+		return $this->getProofreaderFirstName() . ' ' . $this->getProofreaderLastName();
+	}
+
+}
+
+?>
---
+++ classes/submission/proofAssignment/ProofAssignmentDAO.inc.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+ * ProofAssignmentsDAO.inc.php
+ *
+ * Copyright (c) 2003-2004 The Public Knowledge Project
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package submission
+ *
+ * Class for DAO relating proofreaders to articles.
+ *
+ * $Id$
+ */
+
+class ProofAssignmentDAO extends DAO {
+
+	var $userDao;
+
+	/**
+	 * Constructor.
+	 */
+	function ProofAssignmentDAO() {
+		parent::DAO();
+		$this->userDao = DAORegistry::getDAO('UserDAO');
+	}
+	
+	/**
+	 * Retrieve a proof assignment by id.
+	 * @param $proofId int
+	 * @return ProofAssignment
+	 */
+	function &getProofAssignment($proofId) {
+		$result = &$this->retrieve(
+			'SELECT p.*, u.first_name, u.last_name, u.email FROM proof_assignments p LEFT JOIN users u ON (p.proofreader_id = u.user_id) WHERE p.proof_id = ?',
+			$proofId
+			);
+		
+		if ($result->RecordCount() == 0) {
+			return null;
+		} else {
+			return $this->_returnProofAssignmentFromRow($result->GetRowAssoc(false));
+		}
+	}
+	
+	/**
+	 * Retrieve a proof assignment by article id.
+	 * @param $articleId int
+	 * @return ProofAssignment
+	 */
+	function &getProofAssignmentByArticleId($articleId) {
+		$result = &$this->retrieve(
+			'SELECT p.*, u.first_name, u.last_name, u.email FROM proof_assignments p LEFT JOIN users u ON (p.proofreader_id = u.user_id) WHERE p.article_id = ?',
+			$articleId
+			);
+		
+		if ($result->RecordCount() == 0) {
+			return null;
+		} else {
+			return $this->_returnProofAssignmentFromRow($result->GetRowAssoc(false));
+		}
+	}
+
+	/**
+	 * Internal function to return a proof assignment object from a row.
+	 * @param $row array
+	 * @return ProofAssignment
+	 */
+	function &_returnProofAssignmentFromRow(&$row) {
+		$proofAssignment = &new ProofAssignment();
+		$proofAssignment->setProofId($row['proof_id']);
+		$proofAssignment->setArticleId($row['article_id']);
+		$proofAssignment->setProofreaderId($row['proofreader_id']);
+		$proofAssignment->setDateSchedulingQueue($row['date_scheduling_queue']);
+
+		$proofAssignment->setAuthorComments($row['author_comments']);
+		$proofAssignment->setDateAuthorNotified($row['date_author_notified']);
+		$proofAssignment->setDateAuthorUnderway($row['date_author_underway']);
+		$proofAssignment->setDateAuthorCompleted($row['date_author_completed']);
+		$proofAssignment->setDateAuthorAcknowledged($row['date_author_acknowledged']);
+
+		$proofAssignment->setProofreaderComments($row['proofreader_comments']);
+		$proofAssignment->setDateProofreaderNotified($row['date_proofreader_notified']);
+		$proofAssignment->setDateProofreaderUnderway($row['date_proofreader_underway']);
+		$proofAssignment->setDateProofreaderCompleted($row['date_proofreader_completed']);
+		$proofAssignment->setDateProofreaderAcknowledged($row['date_proofreader_acknowledged']);
+
+		$proofAssignment->setLayoutEditorComments($row['layouteditor_comments']);
+		$proofAssignment->setDateLayoutEditorNotified($row['date_layouteditor_notified']);
+		$proofAssignment->setDateLayoutEditorUnderway($row['date_layouteditor_underway']);
+		$proofAssignment->setDateLayoutEditorCompleted($row['date_layouteditor_completed']);
+		$proofAssignment->setDateLayoutEditorAcknowledged($row['date_layouteditor_acknowledged']);
+
+		$proofAssignment->setProofreaderFirstName($row['first_name']);
+		$proofAssignment->setProofreaderLastName($row['last_name']);		
+		$proofAssignment->setProofreaderEmail($row['email']);
+
+		return $proofAssignment;
+	}
+	
+	/**
+	 * Insert a new ProofAssignment.
+	 * @param $proofAssignment ProofAssignment
+	 */	
+	function insertProofAssignment(&$proofAssignment) {
+		$this->update(
+			'INSERT INTO proof_assignments
+				(article_id, proofreader_id, date_scheduling_queue, author_comments, date_author_notified, date_author_underway, date_author_completed, date_author_acknowledged, proofreader_comments, date_proofreader_notified, date_proofreader_underway, date_proofreader_completed, date_proofreader_acknowledged, layouteditor_comments, date_layouteditor_notified, date_layouteditor_underway, date_layouteditor_completed, date_layouteditor_acknowledged)
+				VALUES
+				(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+			array(
+				$proofAssignment->getArticleId(),
+				$proofAssignment->getProofreaderId(),
+				$proofAssignment->getDateSchedulingQueue(),
+				$proofAssignment->getAuthorComments(),
+				$proofAssignment->getDateAuthorNotified(),
+				$proofAssignment->getDateAuthorUnderway(),
+				$proofAssignment->getDateAuthorCompleted(),
+				$proofAssignment->getDateAuthorAcknowledged(),
+				$proofAssignment->getProofreaderComments(),
+				$proofAssignment->getDateProofreaderNotified(),
+				$proofAssignment->getDateProofreaderUnderway(),
+				$proofAssignment->getDateProofreaderCompleted(),
+				$proofAssignment->getDateProofreaderAcknowledged(),
+				$proofAssignment->getLayoutEditorComments(),
+				$proofAssignment->getDateLayoutEditorNotified(),
+				$proofAssignment->getDateLayoutEditorUnderway(),
+				$proofAssignment->getDateLayoutEditorCompleted(),
+				$proofAssignment->getDateLayoutEditorAcknowledged()
+			)
+		);
+		
+		$proofAssignment->setProofId($this->getInsertProofId());
+	}
+	
+	/**
+	 * Update an existing proof assignment.
+	 * @param $proofAssignment ProofAssignment
+	 */
+	function updateProofAssignment(&$proofAssignment) {
+		return $this->update(
+			'UPDATE proof_assignments
+				SET	article_id = ?,
+					proofreader_id = ?,
+					date_scheduling_queue = ?,
+					author_comments = ?,
+					date_author_notified = ?,
+					date_author_underway = ?,
+					date_author_completed = ?,
+					date_author_acknowledged = ?,
+					proofreader_comments = ?,
+					date_proofreader_notified = ?,
+					date_proofreader_underway = ?,
+					date_proofreader_completed = ?,
+					date_proofreader_acknowledged = ?,
+					layouteditor_comments = ?,
+					date_layouteditor_notified = ?,
+					date_layouteditor_underway = ?,
+					date_layouteditor_completed = ?,
+					date_layouteditor_acknowledged = ?
+				WHERE proof_id = ?',
+			array(
+				$proofAssignment->getArticleId(),
+				$proofAssignment->getProofreaderId(),
+				$proofAssignment->getDateSchedulingQueue() ? str_replace("'",'',$proofAssignment->getDateSchedulingQueue()) : null,
+				$proofAssignment->getAuthorComments(),
+				$proofAssignment->getDateAuthorNotified() ? str_replace("'",'',$proofAssignment->getDateAuthorNotified()) : null,
+				$proofAssignment->getDateAuthorUnderway() ? str_replace("'",'',$proofAssignment->getDateAuthorUnderway()) : null,
+				$proofAssignment->getDateAuthorCompleted() ? str_replace("'",'',$proofAssignment->getDateAuthorCompleted()) : null,
+				$proofAssignment->getDateAuthorAcknowledged() ? str_replace("'",'',$proofAssignment->getDateAuthorAcknowledged()) : null,
+				$proofAssignment->getProofreaderComments(),
+				$proofAssignment->getDateProofreaderNotified() ? str_replace("'",'',$proofAssignment->getDateProofreaderNotified()) : null,
+				$proofAssignment->getDateProofreaderUnderway() ? str_replace("'",'',$proofAssignment->getDateProofreaderUnderway()) : null,
+				$proofAssignment->getDateProofreaderCompleted() ? str_replace("'",'',$proofAssignment->getDateProofreaderCompleted()) : null,
+				$proofAssignment->getDateProofreaderAcknowledged() ? str_replace("'",'',$proofAssignment->getDateProofreaderAcknowledged()) : null,
+				$proofAssignment->getLayoutEditorComments(),
+				$proofAssignment->getDateLayoutEditorNotified() ? str_replace("'",'',$proofAssignment->getDateLayouteditorNotified()) : null,
+				$proofAssignment->getDateLayoutEditorUnderway() ? str_replace("'",'',$proofAssignment->getDateLayouteditorUnderway()) : null,
+				$proofAssignment->getDateLayoutEditorCompleted() ? str_replace("'",'',$proofAssignment->getDateLayouteditorCompleted()) : null,
+				$proofAssignment->getDateLayoutEditorAcknowledged() ? str_replace("'",'',$proofAssignment->getDateLayouteditorAcknowledged()) : null,
+				$proofAssignment->getProofId()
+			)
+		);
+	}
+	
+	/**
+	 * Delete proof assignment.
+	 * @param $proofId int
+	 */
+	function deleteProofAssignmentById($proofId) {
+		return $this->update(
+			'DELETE FROM proof_assignments WHERE proof_id = ?',
+			$proofId
+		);
+	}
+	
+	/**
+	 * Get the ID of the last inserted proof assignment.
+	 * @return int
+	 */
+	function getInsertProofId() {
+		return $this->getInsertId('proof_assignments', 'proof_id');
+	}
+	
+}
+
+?>
