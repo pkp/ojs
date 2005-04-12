09176b7568346db8aaaa79eb2518093925d41fbf
jasonc <jasonc>
2005-04-12 00:46:31 +0000

    #1411# Single help directory cache.

--- classes/help/HelpTocDAO.inc.php
+++ classes/help/HelpTocDAO.inc.php
@@ -22,7 +22,7 @@ class HelpTocDAO extends XMLDAO {
 	function HelpTocDAO() {
 		parent::XMLDAO();
 	}
-	
+
 	/**
 	 * Retrieves a toc by its ID.
 	 * @param $tocId string
@@ -31,7 +31,7 @@ class HelpTocDAO extends XMLDAO {
 	function &getToc($tocId) {
 
 		$helpFile = sprintf('help/%s/%s.xml', Locale::getLocale(), $tocId);
-		$cacheFile = sprintf('help/%s/cache/%s.inc.php', Locale::getLocale(), str_replace('/','.',$tocId));
+		$cacheFile = sprintf('help/cache/%s.%s.inc.php', Locale::getLocale(), str_replace('/','.',$tocId));
 
 		// if available, load up cache of this table of contents otherwise load xml file
 		if (file_exists($cacheFile) && filemtime($helpFile) < filemtime($cacheFile)) {
@@ -43,7 +43,7 @@ class HelpTocDAO extends XMLDAO {
 			// check if data exists before saving it to cache
 			if ($data === false) {
 				return false;
-			}			
+			}
 
 			// Cache array
 			if ((file_exists($cacheFile) && is_writable($cacheFile)) || (!file_exists($cacheFile) && is_writable(dirname($cacheFile)))) {
@@ -51,25 +51,25 @@ class HelpTocDAO extends XMLDAO {
 				if (function_exists('var_export')) {
 					fwrite($fp, '<?php $data = ' . var_export($data, true) . '; ?>');
 				} else {
-					fwrite($fp, '<?php $data = ' . $this->custom_var_export($data, true) . '; ?>');				
+					fwrite($fp, '<?php $data = ' . $this->custom_var_export($data, true) . '; ?>');
 				}
 				fclose($fp);
-			}			
-		}	
-		
+			}
+		}
+
 		// check if data exists after loading
 		if (!is_array($data)) {
 			return false;
 		}
-		
+
 		$toc = &new HelpToc();
-		
+
 		$toc->setId($data['toc'][0]['attributes']['id']);
 		$toc->setTitle($data['toc'][0]['attributes']['title']);
 		if (isset($data['toc'][0]['attributes']['prev_topic'])) {
 			$toc->setPrevTopicId($data['toc'][0]['attributes']['prev_topic']);
 		}
-		
+
 		if (isset($data['topic'])) {
 			foreach ($data['topic'] as $topicData) {
 				$topic = &new HelpTopic();
@@ -84,9 +84,9 @@ class HelpTocDAO extends XMLDAO {
 				$toc->addBreadcrumb($breadcrumbData['attributes']['title'], $breadcrumbData['attributes']['url']);
 			}
 		}
-		
+
 		return $toc;
-	}	
+	}
 }
 
 ?>
--- classes/help/HelpTopicDAO.inc.php
+++ classes/help/HelpTopicDAO.inc.php
@@ -22,7 +22,7 @@ class HelpTopicDAO extends XMLDAO {
 	function HelpTopicDAO() {
 		parent::XMLDAO();
 	}
-	
+
 	/**
 	 * Retrieve a topic by its ID.
 	 * @param $topicId string
@@ -30,7 +30,7 @@ class HelpTopicDAO extends XMLDAO {
 	 */
 	function &getTopic($topicId) {
 		$helpFile = sprintf('help/%s/%s.xml', Locale::getLocale(), $topicId);
-		$cacheFile = sprintf('help/%s/cache/%s.inc.php', Locale::getLocale(), str_replace('/','.',$topicId));
+		$cacheFile = sprintf('help/cache/%s.%s.inc.php', Locale::getLocale(), str_replace('/','.',$topicId));
 
 		// if available, load up cache of this topic otherwise load xml file
 		if (file_exists($cacheFile) && filemtime($helpFile) < filemtime($cacheFile)) {
@@ -43,30 +43,30 @@ class HelpTopicDAO extends XMLDAO {
 			if ($data === false) {
 				return false;
 			}
-		
+
 			// Cache array
 			if ((file_exists($cacheFile) && is_writable($cacheFile)) || (!file_exists($cacheFile) && is_writable(dirname($cacheFile)))) {
 				$fp = fopen($cacheFile, 'w');
 				if (function_exists('var_export')) {
-					fwrite($fp, '<?php $data = ' . var_export($data, true) . '; ?>');				
+					fwrite($fp, '<?php $data = ' . var_export($data, true) . '; ?>');
 				} else {
 					fwrite($fp, '<?php $data = ' . $this->custom_var_export($data, true) . '; ?>');
 				}
 				fclose($fp);
-			}			
+			}
 		}
 
 		// check if data exists after loading
 		if (!is_array($data)) {
 			return false;
 		}
-		
+
 		$topic = &new HelpTopic();
-		
+
 		$topic->setId($data['topic'][0]['attributes']['id']);
 		$topic->setTitle($data['topic'][0]['attributes']['title']);
 		$topic->setTocId($data['topic'][0]['attributes']['toc']);
-		
+
 		if (isset($data['section'])) {
 			foreach ($data['section'] as $sectionData) {
 				$section = &new HelpTopicSection();
@@ -83,9 +83,9 @@ class HelpTopicDAO extends XMLDAO {
 			}
 		}
 
-		return $topic;		
+		return $topic;
 	}
-	
+
 	/**
 	 * Returns a set of topics matching a specified keyword.
 	 * @param $keyword string
@@ -103,15 +103,15 @@ class HelpTopicDAO extends XMLDAO {
 			}
 		}
 		closedir($dir);
-		
+
 		arsort($matchingTopics);
-		
+
 		$topics = array();
-		
+
 		foreach ($matchingTopics as $topicId => $numMatches) {
 			$topics[] = &$this->getTopic($topicId);
 		}
-		
+
 		return $topics;
 	}
 
@@ -119,9 +119,9 @@ class HelpTopicDAO extends XMLDAO {
 	 * Parses deeper into folders if subdirectories exists otherwise scans the topic xml files
 	 * @param $matchingTopics array stores topics that match the keyword
 	 * @param $keyword string
-	 * @param $dir string	 
+	 * @param $dir string
 	 * @modifies $matchingTopics array by reference by making appropriate calls to functions
-	 */	
+	 */
 	function searchDirectory(&$matchingTopics,$keyword,$dir) {
 		$currDir = opendir($dir);
 		while (($file = readdir($currDir)) !== false) {
@@ -140,9 +140,9 @@ class HelpTopicDAO extends XMLDAO {
 	 * @param $matchingTopics array stores topics that match the keyword
 	 * @param $keyword string
 	 * @param $dir string
-	 * @param $file string	 	 
+	 * @param $file string
 	 * @modifies $matchingTopics array by reference
-	 */		
+	 */
 	function scanTopic(&$matchingTopics,$keyword,$dir,$file) {
 		if (preg_match('/^\d{6,6}\.xml$/', $file)) {
 			$fileContents = String::regexp_replace('/(<!\[CDATA\[)|(\]\]>)|(<[^>]*>)/', '', join('', file("$dir/$file")));
@@ -152,7 +152,7 @@ class HelpTopicDAO extends XMLDAO {
 				$matchingTopics[$url[2] . '/' . str_replace('.xml', '', $file)] = $numMatches;
 			}
 		}
-	}	
+	}
 }
 
 ?>
