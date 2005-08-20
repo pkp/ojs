eb4f2c712521a04cde611db9b17b2b75317cf000
kevin <kevin>
2005-08-20 19:39:05 +0000

    #1772# Convert HTML entities. Modified Files: classes/search/SearchHTMLParser.inc.php

--- classes/search/SearchHTMLParser.inc.php
+++ classes/search/SearchHTMLParser.inc.php
@@ -17,8 +17,27 @@ import('search.SearchFileParser');
 
 class SearchHTMLParser extends SearchFileParser {
 
+	// From php.net/html_entity_decode
+	function unhtmlentities($string) {
+		// replace numeric entities
+		$string = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $string);
+		$string = preg_replace('~&#([0-9]+);~e', 'chr(\\1)', $string);
+		// replace literal entities
+		$trans_tbl = get_html_translation_table(HTML_ENTITIES);
+		$trans_tbl = array_flip($trans_tbl);
+		return strtr($string, $trans_tbl);
+	}
+
+
 	function doRead() {
-		return fgetss($this->fp, 4096);
+		$line = fgetss($this->fp, 4096);
+		$line = str_replace('&nbsp;', ' ', $line);
+		if (function_exists('html_entity_decode')) {
+			$line = html_entity_decode($line);
+		} else {
+			$line = $this->unhtmlentities($line);
+		}
+		return $line;
 	}
 	
 }
