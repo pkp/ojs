580ee6237308b0767baae5c8352d892178e6eda9
jalperin <jalperin>
2007-03-22 17:14:47 +0000

    New Plugin to add RSS aggregation to the CMS

---
+++ plugins/generic/cmsRSS/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+ * index.php
+ *
+ * Copyright (c) 2006 Gunther Eysenbach, Juan Pablo Alperin
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package plugins
+ *
+ * Wrapper for CMS plugin.
+ *
+ * $Id$
+ */
+ 
+require('CmsRssPlugin.inc.php'); 
+return new CmsRss(); 
+
+?> 
