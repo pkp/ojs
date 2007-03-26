832d59dadf70e4c2b0e45b8652fcf738ff0adbe8
mj <mj>
2007-03-26 16:36:35 +0000

    #2687# - added check to ensure we're handling a string (as per http://pkp.sfu.ca/support/forum/viewtopic.php?t=1756)

--- classes/form/Form.inc.php
+++ classes/form/Form.inc.php
@@ -78,19 +78,21 @@ class Form {
 	 * @param $value
 	 */
 	function setData($key, $value) {
-		// check for Windows-1252 encoding, and transliterate if necessary	
-		if ( function_exists('mb_check_encoding') ) {
-				import('core.Transcoder');
-				$trans =& new Transcoder('CP1252', 'UTF-8');
-				$value = $trans->trans($value);
-		} elseif ( function_exists('iconv') ) {
-			if (mb_check_encoding($value, "CP1252")) {
-				import('core.Transcoder');
-				$trans =& new Transcoder('CP1252', 'UTF-8');
-				$value = $trans->trans($value);
+
+		if (is_string($value)) {
+			// check for Windows-1252 encoding, and transliterate if necessary	
+			if ( function_exists('mb_check_encoding') ) {
+					import('core.Transcoder');
+					$trans =& new Transcoder('CP1252', 'UTF-8');
+					$value = $trans->trans($value);
+			} elseif ( function_exists('iconv') ) {
+				if (mb_check_encoding($value, "CP1252")) {
+					import('core.Transcoder');
+					$trans =& new Transcoder('CP1252', 'UTF-8');
+					$value = $trans->trans($value);
+				}
 			}
 		}
-
 		$this->_data[$key] = $value;
 	}
 	
