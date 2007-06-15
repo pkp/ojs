a7b5633911dadb6bbdf5008be91a8f6328f4f0e8
asmecher <asmecher>
2007-06-15 17:59:24 +0000

    #2916# POST form validator

---
+++ classes/form/validation/FormValidatorPost.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * FormValidatorPost.inc.php
+ *
+ * Copyright (c) 2003-2007 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package form.validation
+ *
+ * Form validation check to make sure the form is POSTed.
+ *
+ * $Id$
+ */
+
+import ('form.validation.FormValidator');
+
+class FormValidatorPost extends FormValidator {
+	/**
+	 * Constructor.
+	 * @see FormValidator::FormValidator()
+	 * @param message string the locale key to use (optional)
+	 */
+	function FormValidatorPost(&$form, $message = 'form.postRequired') {
+		parent::FormValidator($form, 'dummy', 'required', $message);
+	}
+	
+	/**
+	 * Check if field value is valid.
+	 * Value is valid if it is empty and optional or matches regular expression.
+	 * @return boolean
+	 */
+	function isValid() {
+		return Request::isPost();
+	}
+	
+}
+
+?>
