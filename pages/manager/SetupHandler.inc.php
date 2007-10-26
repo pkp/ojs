8c2b84a474cb519cd28d73433341cb507b2e22ec
asmecher <asmecher>
2007-10-26 16:52:22 +0000

    #2961# Localization fixes

--- pages/manager/SetupHandler.inc.php
+++ pages/manager/SetupHandler.inc.php
@@ -168,7 +168,7 @@ class SetupHandler extends ManagerHandler {
 					if (!isset($editData)) {
 						// Reorder checklist items
 						$checklist = $setupForm->getData('submissionChecklist');
-						if (is_array($checklist[$formLocale])) {
+						if (isset($checklist[$formLocale]) && is_array($checklist[$formLocale])) {
 							usort($checklist[$formLocale], create_function('$a,$b','return $a[\'order\'] == $b[\'order\'] ? 0 : ($a[\'order\'] < $b[\'order\'] ? -1 : 1);'));
 						}
 						$setupForm->setData('submissionChecklist', $checklist);
