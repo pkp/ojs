7b5b63f596a98df089d1ae19f3695abbd84043c5
mcrider <mcrider>
2008-10-28 22:23:05 +0000

    #3832# Papers report only lists submitter, not authors

--- plugins/reports/articles/ArticleReportPlugin.inc.php
+++ plugins/reports/articles/ArticleReportPlugin.inc.php
@@ -59,7 +59,9 @@ class ArticleReportPlugin extends ReportPlugin {
 		header('content-disposition: attachment; filename=report.csv');
 
 		$articleReportDao =& DAORegistry::getDAO('ArticleReportDAO');
-		list($articlesIterator, $decisionsIteratorsArray) = $articleReportDao->getArticleReport($journal->getJournalId());
+		list($articlesIterator, $authorsIterator, $decisionsIteratorsArray) = $articleReportDao->getArticleReport($journal->getJournalId());
+
+		$maxAuthors = $this->getMaxAuthorCount($authorsIterator);
 
 		$decisions = array();
 		foreach ($decisionsIteratorsArray as $decisionsIterator){
@@ -80,23 +82,28 @@ class ArticleReportPlugin extends ReportPlugin {
 		$columns = array(
 			'article_id' => Locale::translate('article.submissionId'),
 			'title' => Locale::translate('article.title'),
-			'abstract' => Locale::translate('article.abstract'),
-			'fname' => Locale::translate('user.firstName'),
-			'mname' => Locale::translate('user.middleName'),
-			'lname' => Locale::translate('user.lastName'),
-			'phone' => Locale::translate('user.phone'),
-			'fax' => Locale::translate('user.fax'),
-			'address' => Locale::translate('common.mailingAddress'),
-			'country' => Locale::translate('common.country'),
-			'affiliation' => Locale::translate('user.affiliation'),
-			'email' => Locale::translate('user.email'),
-			'url' => Locale::translate('user.url'),
-			'biography' => Locale::translate('user.biography'),
+			'abstract' => Locale::translate('article.abstract')
+		);
+			
+		for ($a = 1; $a <= $maxAuthors; $a++) {
+			$columns = array_merge($columns, array(
+				'fname' . $a => Locale::translate('user.firstName') . " (" . Locale::translate('user.role.author') . " $a)",
+				'mname' . $a => Locale::translate('user.middleName') . " (" . Locale::translate('user.role.author') . " $a)",
+				'lname' . $a => Locale::translate('user.lastName') . " (" . Locale::translate('user.role.author') . " $a)",
+				'country' . $a => Locale::translate('common.country') . " (" . Locale::translate('user.role.author') . " $a)",
+				'affiliation' . $a => Locale::translate('user.affiliation') . " (" . Locale::translate('user.role.author') . " $a)",
+				'email' . $a => Locale::translate('user.email') . " (" . Locale::translate('user.role.author') . " $a)",
+				'url' . $a => Locale::translate('user.url') . " (" . Locale::translate('user.role.author') . " $a)",
+				'biography' . $a => Locale::translate('user.biography') . " (" . Locale::translate('user.role.author') . " $a)"
+			));
+		}
+			
+		$columns = array_merge($columns, array(
 			'section_title' => Locale::translate('section.title'),
 			'language' => Locale::translate('common.language'),
 			'editor_decision' => Locale::translate('submission.editorDecision'),
 			'status' => Locale::translate('common.status')
-		);
+		));
 
 		$fp = fopen('php://output', 'wt');
 		String::fputcsv($fp, array_values($columns));
@@ -104,7 +111,10 @@ class ArticleReportPlugin extends ReportPlugin {
 		import('article.Article'); // Bring in getStatusMap function
 		$statusMap =& Article::getStatusMap();
 
+		$authorIndex = 0;
 		while ($row =& $articlesIterator->next()) {
+			$authors = $this->mergeAuthors($authorsIterator[$authorIndex]->toArray());
+
 			foreach ($columns as $index => $junk) switch ($index) {
 				case 'editor_decision':
 					if (isset($decisions[$row['article_id']])) {
@@ -116,16 +126,61 @@ class ArticleReportPlugin extends ReportPlugin {
 				case 'status':
 					$columns[$index] = Locale::translate($statusMap[$row[$index]]);
 					break;
+				case 'abstract':
+					$columns[$index] = strip_tags($row[$index]);
+					break;
 				default:
-					$columns[$index] = $row[$index];
+					if (isset($row[$index])) {
+						$columns[$index] = $row[$index];
+					} else if (isset($authors[$index])) {
+						$columns[$index] = $authors[$index];
+					} else $columns[$index] = '';
 					break;
 			}
 			String::fputcsv($fp, $columns);
 			unset($row);
+			$authorIndex++;
 		}
 		
 		fclose($fp);
 	}
+	
+	/**
+	 * Get the highest author count for any article (to determine how many columns to set)
+	 * @param $authorsIterator DBRowIterator
+	 * @return int
+	 */
+	function getMaxAuthorCount($authorsIterator) {
+		$maxAuthors = 0;
+		foreach ($authorsIterator as $authorIterator) {
+			$maxAuthors = $authorIterator->getCount() > $maxAuthors ? $authorIterator->getCount() : $maxAuthors;
+		}
+		return $maxAuthors;
+	}
+	
+	/**
+	 * Flatten an array of author information into one array and append author sequence to each key
+	 * @param $authors array
+	 * @return array
+	 */
+	function mergeAuthors($authors) {
+		$returner = array();
+		$seq = 0;
+		foreach($authors as $author) {
+			$seq++;
+			
+			$returner['fname' . $seq] = isset($author['fname']) ? $author['fname'] : '';
+			$returner['mname' . $seq] = isset($author['mname']) ? $author['mname'] : '';
+			$returner['lname' . $seq] = isset($author['lname']) ? $author['lname'] : '';
+			$returner['email' . $seq] = isset($author['email']) ? $author['email'] : '';
+			$returner['affiliation'] = isset($author['affiliation']) ? $author['affiliation'] : '';
+			$returner['country' . $seq] = isset($author['country']) ? $author['country'] : '';
+			$returner['url' . $seq] = isset($author['url']) ? $author['url'] : '';
+			$returner['biography' . $seq] = isset($author['biography']) ? $author['biography'] : '';
+		}
+		return $returner;
+	}
+
 }
 
 ?>
