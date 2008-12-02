7be7f14b14b30c2ee9de5bad117a2345f001766d
mcrider <mcrider>
2008-12-02 00:38:16 +0000

    #3822# Static methods not declared using the static keyword

--- classes/core/Request.inc.php
+++ classes/core/Request.inc.php
@@ -64,7 +64,7 @@ class Request extends PKPRequest {
 	 * Get the journal associated with the current request.
 	 * @return Journal
 	 */
-	static function &getJournal() {
+	function &getJournal() {
 		static $journal;
 
 		if (!isset($journal)) {
@@ -128,7 +128,7 @@ class Request extends PKPRequest {
 	 * @param $anchor string Optional name of anchor to add to URL
 	 * @param $escape boolean Whether or not to escape ampersands for this URL; default false.
 	 */
-	static function url($journalPath = null, $page = null, $op = null, $path = null, 
+	function url($journalPath = null, $page = null, $op = null, $path = null, 
 			$params = null, $anchor = null, $escape = false) {
 		return parent::url(array($journalPath), $page, $op, $path, $params, $anchor, $escape);
 	}
