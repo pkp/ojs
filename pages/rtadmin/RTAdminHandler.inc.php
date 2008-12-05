416067f14226f9cdf41f92af14fb77f3445acccf
asmecher <asmecher>
2008-12-05 16:56:33 +0000

    #1444# Locale fix

--- pages/rtadmin/RTAdminHandler.inc.php
+++ pages/rtadmin/RTAdminHandler.inc.php
@@ -253,7 +253,7 @@ class RTAdminHandler extends PKPHandler {
 	 */
 	function setupTemplate($subclass = false, $version = null, $context = null, $search = null) {
 		parent::setupTemplate();
-		Locale::requireComponents(array(LOCALE_COMPONENT_PKP_READER));
+		Locale::requireComponents(array(LOCALE_COMPONENT_PKP_READER, LOCALE_COMPONENT_OJS_MANAGER));
 		$templateMgr = &TemplateManager::getManager();
 
 		$pageHierarchy = array(array(Request::url(null, 'user'), 'navigation.user'), array(Request::url(null, 'manager'), 'manager.journalManagement'));
