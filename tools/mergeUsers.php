ef833a30add21d8901ef187e235eefcc3e88c30b
michael <michael>
2009-03-03 05:08:50 +0000

    #2277# Refactor merge users

--- tools/mergeUsers.php
+++ tools/mergeUsers.php
@@ -59,7 +59,6 @@ class mergeUsers extends CommandLineTool {
 	 * Execute the merge users command.
 	 */
 	function execute() {
-		$roleDao =& DAORegistry::getDAO('RoleDAO');
 		$userDao =& DAORegistry::getDAO('UserDAO');
 
 		$oldUser =& $userDao->getUserbyUsername($this->username2);
@@ -81,116 +80,8 @@ class mergeUsers extends CommandLineTool {
 		}
 
 		// Both user IDs are valid. Merge the accounts.
-		$articleDao =& DAORegistry::getDAO('ArticleDAO');
-		foreach ($articleDao->getArticlesByUserId($oldUserId) as $article) {
-			$article->setUserId($newUserId);
-			$articleDao->updateArticle($article);
-			unset($article);
-		}
-
-		$commentDao =& DAORegistry::getDAO('CommentDAO');
-		foreach ($commentDao->getCommentsByUserId($oldUserId) as $comment) {
-			$comment->setUserId($newUserId);
-			$commentDao->updateComment($comment);
-			unset($comment);
-		}
-
-		$articleNoteDao =& DAORegistry::getDAO('ArticleNoteDAO');
-		$articleNotes =& $articleNoteDao->getArticleNotesByUserId($oldUserId);
-		while ($articleNote =& $articleNotes->next()) {
-			$articleNote->setUserId($newUserId);
-			$articleNoteDao->updateArticleNote($articleNote);
-			unset($articleNote);
-		}
-
-		$editAssignmentDao =& DAORegistry::getDAO('EditAssignmentDAO');
-		$editAssignments =& $editAssignmentDao->getEditAssignmentsByUserId($oldUserId);
-		while ($editAssignment =& $editAssignments->next()) {
-			$editAssignment->setEditorId($newUserId);
-			$editAssignmentDao->updateEditAssignment($editAssignment);
-			unset($editAssignment);
-		}
-
-		$editorSubmissionDao =& DAORegistry::getDAO('EditorSubmissionDAO');
-		$editorSubmissionDao->transferEditorDecisions($oldUserId, $newUserId);
-
-		$reviewAssignmentDao =& DAORegistry::getDAO('ReviewAssignmentDAO');
-		foreach ($reviewAssignmentDao->getReviewAssignmentsByUserId($oldUserId) as $reviewAssignment) {
-			$reviewAssignment->setReviewerId($newUserId);
-			$reviewAssignmentDao->updateReviewAssignment($reviewAssignment);
-			unset($reviewAssignment);
-		}
-
-		$copyeditorSubmissionDao =& DAORegistry::getDAO('CopyeditorSubmissionDAO');
-		$copyeditorSubmissions =& $copyeditorSubmissionDao->getCopyeditorSubmissionsByCopyeditorId($oldUserId);
-		while ($copyeditorSubmission =& $copyeditorSubmissions->next()) {
-			$copyeditorSubmission->setCopyeditorId($newUserId);
-			$copyeditorSubmissionDao->updateCopyeditorSubmission($copyeditorSubmission);
-			unset($copyeditorSubmission);
-		}
-
-		$layoutEditorSubmissionDao =& DAORegistry::getDAO('LayoutEditorSubmissionDAO');
-		$layoutEditorSubmissions =& $layoutEditorSubmissionDao->getSubmissions($oldUserId);
-		while ($layoutEditorSubmission =& $layoutEditorSubmissions->next()) {
-			$layoutAssignment =& $layoutEditorSubmission->getLayoutAssignment();
-			$layoutAssignment->setEditorId($newUserId);
-			$layoutEditorSubmissionDao->updateSubmission($layoutEditorSubmission);
-			unset($layoutAssignment);
-			unset($layoutEditorSubmission);
-		}
-
-		$proofreaderSubmissionDao =& DAORegistry::getDAO('ProofreaderSubmissionDAO');
-		$proofreaderSubmissions =& $proofreaderSubmissionDao->getSubmissions($oldUserId);
-		while ($proofreaderSubmission =& $proofreaderSubmissions->next()) {
-			$proofAssignment =& $proofreaderSubmission->getProofAssignment();
-			$proofAssignment->setProofreaderId($newUserId);
-			$proofreaderSubmissionDao->updateSubmission($proofreaderSubmission);
-			unset($proofAssignment);
-			unset($proofreaderSubmission);
-		}
-
-		$articleEmailLogDao =& DAORegistry::getDAO('ArticleEmailLogDAO');
-		$articleEmailLogDao->transferArticleLogEntries($oldUserId, $newUserId);
-		$articleEventLogDao =& DAORegistry::getDAO('ArticleEventLogDAO');
-		$articleEventLogDao->transferArticleLogEntries($oldUserId, $newUserId);
-
-		$articleCommentDao =& DAORegistry::getDAO('ArticleCommentDAO');
-		foreach ($articleCommentDao->getArticleCommentsByUserId($oldUserId) as $articleComment) {
-			$articleComment->setAuthorId($newUserId);
-			$articleCommentDao->updateArticleComment($articleComment);
-			unset($articleComment);
-		}
-
-		$accessKeyDao =& DAORegistry::getDAO('AccessKeyDAO');
-		$accessKeyDao->transferAccessKeys($oldUserId, $newUserId);
-
-		// Delete the old user and associated info.
-		$sessionDao =& DAORegistry::getDAO('SessionDAO');
-		$sessionDao->deleteSessionsByUserId($oldUserId);
-		$subscriptionDao =& DAORegistry::getDAO('SubscriptionDAO');
-		$subscriptionDao->deleteSubscriptionsByUserId($oldUserId);
-		$temporaryFileDao =& DAORegistry::getDAO('TemporaryFileDAO');
-		$temporaryFileDao->deleteTemporaryFilesByUserId($oldUserId);
-		$notificationStatusDao =& DAORegistry::getDAO('NotificationStatusDAO');
-		$notificationStatusDao->deleteNotificationStatusByUserId($oldUserId);
-		$userSettingsDao =& DAORegistry::getDAO('UserSettingsDAO');
-		$userSettingsDao->deleteSettings($oldUserId);
-		$groupMembershipDao =& DAORegistry::getDAO('GroupMembershipDAO');
-		$groupMembershipDao->deleteMembershipByUserId($oldUserId);
-		$sectionEditorsDao =& DAORegistry::getDAO('SectionEditorsDAO');
-		$sectionEditorsDao->deleteEditorsByUserId($oldUserId);
-
-		// Transfer old user's roles
-		$roles =& $roleDao->getRolesByUserId($oldUserId);
-		foreach ($roles as $role) {
-			if (!$roleDao->roleExists($role->getJournalId(), $newUserId, $role->getRoleId())) {
-				$role->setUserId($newUserId);
-				$roleDao->insertRole($role);
-			}
-		}
-		$roleDao->deleteRoleByUserId($oldUserId);
-
-		$userDao->deleteUserById($oldUserId);
+		import('user.UserAction');
+		UserAction::mergeUsers($oldUserId, $newUserId);
 
 		printf("Merge completed: '%s' merged into '%s'.\n",
 			$this->username2,
