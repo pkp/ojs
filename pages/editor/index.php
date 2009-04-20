3ca8007d4d166f68bef6e8729d8df73dd25ad80d
jalperin <jalperin>
2009-04-20 23:06:57 +0000

    #4205# added SectionEditor page access to editor

--- pages/editor/index.php
+++ pages/editor/index.php
@@ -19,6 +19,151 @@
 
 switch ($op) {
 	//
+	// Submission Tracking
+	//
+	case 'enrollSearch':
+	case 'createReviewer':
+	case 'suggestUsername':
+	case 'enroll':
+	case 'submission':
+	case 'submissionRegrets':
+	case 'submissionReview':
+	case 'submissionEditing':
+	case 'submissionHistory':
+	case 'changeSection':
+	case 'recordDecision':
+	case 'selectReviewer':
+	case 'notifyReviewer':
+	case 'notifyAllReviewers':
+	case 'userProfile':
+	case 'clearReview':
+	case 'cancelReview':
+	case 'remindReviewer':
+	case 'thankReviewer':
+	case 'rateReviewer':
+	case 'confirmReviewForReviewer':
+	case 'uploadReviewForReviewer':
+	case 'enterReviewerRecommendation':
+	case 'makeReviewerFileViewable':
+	case 'setDueDate':
+	case 'viewMetadata':
+	case 'saveMetadata':
+	case 'removeArticleCoverPage':
+	case 'editorReview':
+	case 'selectCopyeditor':
+	case 'notifyCopyeditor':
+	case 'initiateCopyedit':
+	case 'thankCopyeditor':
+	case 'notifyAuthorCopyedit':
+	case 'thankAuthorCopyedit':
+	case 'notifyFinalCopyedit':
+	case 'thankFinalCopyedit':
+	case 'selectCopyeditRevisions':
+	case 'uploadReviewVersion':
+	case 'uploadCopyeditVersion':
+	case 'completeCopyedit':
+	case 'completeFinalCopyedit':
+	case 'addSuppFile':
+	case 'setSuppFileVisibility':
+	case 'editSuppFile':
+	case 'saveSuppFile':
+	case 'deleteSuppFile':
+	case 'deleteArticleFile':
+	case 'archiveSubmission':
+	case 'unsuitableSubmission':
+	case 'restoreToQueue':
+	case 'updateSection':
+	case 'updateCommentsStatus':
+	//
+	// Layout Editing
+	//
+	case 'deleteArticleImage':
+	case 'uploadLayoutFile':
+	case 'uploadLayoutVersion':
+	case 'assignLayoutEditor':
+	case 'notifyLayoutEditor':
+	case 'thankLayoutEditor':
+	case 'uploadGalley':
+	case 'editGalley':
+	case 'saveGalley':
+	case 'orderGalley':
+	case 'deleteGalley':
+	case 'proofGalley':
+	case 'proofGalleyTop':
+	case 'proofGalleyFile':
+	case 'uploadSuppFile':
+	case 'orderSuppFile':
+	//
+	// Submission History
+	//
+	case 'submissionEventLog':
+	case 'submissionEventLogType':
+	case 'clearSubmissionEventLog':
+	case 'submissionEmailLog':
+	case 'submissionEmailLogType':
+	case 'clearSubmissionEmailLog':
+	case 'addSubmissionNote':
+	case 'removeSubmissionNote':
+	case 'updateSubmissionNote':
+	case 'clearAllSubmissionNotes':
+	case 'submissionNotes':
+	//
+	// Misc.
+	//
+	case 'downloadFile':
+	case 'viewFile':
+	// Submission Review Form
+	case 'clearReviewForm':
+	case 'selectReviewForm':
+	case 'previewReviewForm':
+	case 'viewReviewFormResponse':
+	// Proof Assignment Functions
+	case 'selectProofreader':
+	case 'notifyAuthorProofreader':
+	case 'thankAuthorProofreader':
+	case 'editorInitiateProofreader':
+	case 'editorCompleteProofreader':
+	case 'notifyProofreader':
+	case 'thankProofreader':
+	case 'editorInitiateLayoutEditor':
+	case 'editorCompleteLayoutEditor':
+	case 'notifyLayoutEditorProofreader':
+	case 'thankLayoutEditorProofreader':
+	//
+	// Scheduling functions
+	//
+	case 'scheduleForPublication':
+	 //
+	 // Payments
+	 //
+	 case 'waiveSubmissionFee':
+	 case 'waiveFastTrackFee':
+	 case 'waivePublicationFee':
+		define('HANDLER_CLASS', 'SubmissionEditHandler');
+		import('pages.sectionEditor.SubmissionEditHandler');
+		break;
+	//
+	// Submission Comments
+	//
+	case 'viewPeerReviewComments':
+	case 'postPeerReviewComment':
+	case 'viewEditorDecisionComments':
+	case 'blindCcReviewsToReviewers':
+	case 'postEditorDecisionComment':
+	case 'viewCopyeditComments':
+	case 'postCopyeditComment':
+	case 'emailEditorDecisionComment':
+	case 'viewLayoutComments':
+	case 'postLayoutComment':
+	case 'viewProofreadComments':
+	case 'postProofreadComment':
+	case 'editComment':
+	case 'saveComment':
+	case 'deleteComment':
+		define('HANDLER_CLASS', 'SubmissionCommentsHandler');
+		import('pages.sectionEditor.SubmissionCommentsHandler');
+		break;
+	//
 	// Issue
 	//
 	case 'futureIssues':
