8f66e509c2b924f352e258b5c2a60bdccdfe94e2
jalperin <jalperin>
2009-04-20 18:51:55 +0000

    #4205# fixed parameter count for validatorCustom

--- classes/handler/Handler.inc.php
+++ classes/handler/Handler.inc.php
@@ -24,7 +24,7 @@ class Handler extends PKPHandler{
 		$journal =& Request::getJournal();
 		$page = Request::getRequestedPage();
 		if ( $journal && $journal->getSetting('restrictSiteAccess')) { 
-			$this->addCheck(new HandlerValidatorCustom(&$this, create_function('$page', 'if (!Validation::isLoggedIn() && !in_array($page, Handler::getLoginExemptions())) return false; else return true;'), array($page)));
+			$this->addCheck(new HandlerValidatorCustom($this, null, null, null, create_function('$page', 'if (!Validation::isLoggedIn() && !in_array($page, Handler::getLoginExemptions())) return false; else return true;'), array($page)));
 		}
 	}
 }
