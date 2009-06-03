1125d11bda0b448fbb60382cdf883ebc540f0256
asmecher <asmecher>
2009-06-03 21:07:40 +0000

    #3694# Abstracted user settings

--- classes/user/UserSettingsDAO.inc.php
+++ classes/user/UserSettingsDAO.inc.php
@@ -16,7 +16,9 @@
 // $Id$
 
 
-class UserSettingsDAO extends DAO {
+import('user.PKPUserSettingsDAO');
+
+class UserSettingsDAO extends PKPUserSettingsDAO {
 	/**
 	 * Retrieve a user setting value.
 	 * @param $userId int
@@ -25,19 +27,7 @@ class UserSettingsDAO extends DAO {
 	 * @return mixed
 	 */
 	function &getSetting($userId, $name, $journalId = null) {
-		$result =& $this->retrieve(
-			'SELECT setting_value, setting_type FROM user_settings WHERE user_id = ? AND setting_name = ? AND journal_id = ?',
-			array((int) $userId, $name, (int) $journalId)
-		);
-
-		if ($result->RecordCount() != 0) {
-			$row =& $result->getRowAssoc(false);
-			$returner = $this->convertFromDB($row['setting_value'], $row['setting_type']);
-		} else {
-			$returner = null;
-		}
-
-		return $returner;
+		return parent::getSetting($userId, $name, ASSOC_TYPE_JOURNAL, $journalId);
 	}
 
 	/**
@@ -49,16 +39,7 @@ class UserSettingsDAO extends DAO {
 	 * @return DAOResultFactory matching Users
 	 */
 	function &getUsersBySetting($name, $value, $type = null, $journalId = null) {
-		$userDao =& DAORegistry::getDAO('UserDAO');
-
-		$value = $this->convertToDB($value, $type);
-		$result =& $this->retrieve(
-			'SELECT u.* FROM users u, user_settings s WHERE u.user_id = s.user_id AND s.setting_name = ? AND s.setting_value = ? AND s.journal_id = ?',
-			array($name, $value, (int) $journalId)
-		);
-
-		$returner = new DAOResultFactory($result, $userDao, '_returnUserFromRow');
-		return $returner;
+		return parent::getUsersBySetting($name, $value, $type, ASSOC_TYPE_JOURNAL, $journalId);
 	}
 
 	/**
@@ -68,23 +49,7 @@ class UserSettingsDAO extends DAO {
 	 * @return array 
 	 */
 	function &getSettingsByJournal($userId, $journalId = null) {
-		$userSettings = array();
-
-		$result =& $this->retrieve(
-			'SELECT setting_name, setting_value, setting_type FROM user_settings WHERE user_id = ? and journal_id = ?',
-			array((int) $userId, (int) $journalId)
-		);
-
-		while (!$result->EOF) {
-			$row =& $result->getRowAssoc(false);
-			$value = $this->convertFromDB($row['setting_value'], $row['setting_type']);
-			$userSettings[$row['setting_name']] = $value;
-			$result->MoveNext();
-		}
-		$result->Close();
-		unset($result);
-
-		return $userSettings;
+		return parent::getSettingsByAssoc($userId, ASSOC_TYPE_JOURNAL, $journalId);
 	}
 
 	/**
@@ -96,34 +61,7 @@ class UserSettingsDAO extends DAO {
 	 * @param $journalId int
 	 */
 	function updateSetting($userId, $name, $value, $type = null, $journalId = null) {
-		$result = $this->retrieve(
-			'SELECT COUNT(*) FROM user_settings WHERE user_id = ? AND setting_name = ? AND journal_id = ?',
-			array($userId, $name, (int) $journalId)
-		);
-
-		$value = $this->convertToDB($value, $type);
-		if ($result->fields[0] == 0) {
-			$returner = $this->update(
-				'INSERT INTO user_settings
-					(user_id, setting_name, journal_id, setting_value, setting_type)
-					VALUES
-					(?, ?, ?, ?, ?)',
-				array((int) $userId, $name, (int) $journalId, $value, $type)
-			);
-		} else {
-			$returner = $this->update(
-				'UPDATE user_settings SET
-					setting_value = ?,
-					setting_type = ?
-					WHERE user_id = ? AND setting_name = ? AND journal_id = ?',
-				array($value, $type, (int) $userId, $name, (int) $journalId)
-			);
-		}
-
-		$result->Close();
-		unset($result);
-
-		return $returner;
+		return parent::updateSetting($userId, $name, $value, $type, ASSOC_TYPE_JOURNAL, $journalId);
 	}
 
 	/**
@@ -133,20 +71,7 @@ class UserSettingsDAO extends DAO {
 	 * @param $journalId int
 	 */
 	function deleteSetting($userId, $name, $journalId = null) {
-		return $this->update(
-			'DELETE FROM user_settings WHERE user_id = ? AND setting_name = ? AND journal_id = ?',
-			array((int) $userId, $name, (int) $journalId)
-		);
-	}
-
-	/**
-	 * Delete all settings for a user.
-	 * @param $userId int
-	 */
-	function deleteSettings($userId) {
-		return $this->update(
-			'DELETE FROM user_settings WHERE user_id = ?', $userId
-		);
+		return parent::deleteSetting($userId, $name, ASSOC_TYPE_JOURNAL, $journalId);
 	}
 }
 
