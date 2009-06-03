f76454f8a95e658a9f9ad80c306201ee9508e764
mj <mj>
2009-06-03 19:19:37 +0000

    #4407# Calls to String::utf8Clean or String::utf8_bad_strip can hang indefinitely on valid UTF8 content

--- classes/core/String.inc.php
+++ classes/core/String.inc.php
@@ -363,16 +363,9 @@ class String {
 	 * @return boolean
 	 */
 	function isUTF8 ($str) {
-		// From http://w3.org/International/questions/qa-forms-utf-8.html
-		return preg_match('%(?:
-				[\xC2-\xDF][\x80-\xBF]								# non-overlong 2-byte
-				|\xE0[\xA0-\xBF][\x80-\xBF]					# excluding overlongs
-				|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}		# straight 3-byte
-				|\xED[\x80-\x9F][\x80-\xBF]					# excluding surrogates
-				|\xF0[\x90-\xBF][\x80-\xBF]{2}				# planes 1-3
-				|[\xF1-\xF3][\x80-\xBF]{3}						# planes 4-15
-				|\xF4[\x80-\x8F][\x80-\xBF]{2}				# plane 16
-				)+%xs', $str);
+		// From the phputf8 project:  http://phputf8.sourceforge.net/
+		if ( strlen($str) == 0 ) return true;
+		return (preg_match('/^.{1}/us',$str,$ar) == 1);
 	}
 
 	/**
