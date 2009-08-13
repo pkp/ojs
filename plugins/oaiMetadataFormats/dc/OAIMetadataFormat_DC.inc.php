a2a3ba0365f346d5bf50037b765472c9ba6e2c42
asmecher <asmecher>
2009-08-13 16:46:40 +0000

    #4610# Fixed language info

--- plugins/oaiMetadataFormats/dc/OAIMetadataFormat_DC.inc.php
+++ plugins/oaiMetadataFormats/dc/OAIMetadataFormat_DC.inc.php
@@ -130,6 +130,7 @@ class OAIMetadataFormat_DC extends OAIMetadataFormat {
 
 		$response = '';
 		foreach ($value as $key => $v) {
+			$key = str_replace('_', '-', $key);
 			if (!$multilingual) $response .= "\t<dc:$name>" . OAIUtils::prepOutput($v) . "</dc:$name>\n";
 			else {
 				if (is_array($v)) {
