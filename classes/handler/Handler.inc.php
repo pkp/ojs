124cea68ab8484702f82c32e289f251ccd8b603f
jerico.dev <jerico.dev@gmail.com>
2010-07-06 21:35:27 -0300

    #5540 Implement validation (authorization) for OMP - move restricted site access to PKPHandler

--- classes/handler/Handler.inc.php
+++ classes/handler/Handler.inc.php
@@ -20,12 +20,6 @@ import('classes.handler.validation.HandlerValidatorSubmissionComment');
 class Handler extends PKPHandler {
 	function Handler() {
 		parent::PKPHandler();
-
-		$journal =& Request::getJournal();
-		$page = Request::getRequestedPage();
-		if ( $journal && $journal->getSetting('restrictSiteAccess')) {
-			$this->addCheck(new HandlerValidatorCustom($this, true, null, null, create_function('$page', 'if (!Validation::isLoggedIn() && !in_array($page, Handler::getLoginExemptions())) return false; else return true;'), array($page)));
-		}
 	}
 }
 
