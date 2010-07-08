3900b4c2c28f160ff28d20ed3a5cbb2ec20e234b
Alec Smecher <alec@smecher.bc.ca>
2010-07-08 10:23:26 -0700

    *5558* Fixed missing DAO get

--- classes/manager/form/UserManagementForm.inc.php
+++ classes/manager/form/UserManagementForm.inc.php
@@ -140,6 +140,7 @@ class UserManagementForm extends Form {
 	 * Initialize form data from current user profile.
 	 */
 	function initData(&$args, &$request) {
+		$interestDao =& DAORegistry::getDAO('InterestDAO');
 		if (isset($this->userId)) {
 			$userDao =& DAORegistry::getDAO('UserDAO');
 			$user =& $userDao->getUser($this->userId);
