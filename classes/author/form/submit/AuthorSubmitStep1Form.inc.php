1fc8d7cd0cd7fe6893ef57091b8c0ebdf254e787
Alec Smecher <alec@smecher.bc.ca>
2010-08-31 09:58:04 -0700

    *5855* Fixed language field default

--- classes/author/form/submit/AuthorSubmitStep1Form.inc.php
+++ classes/author/form/submit/AuthorSubmitStep1Form.inc.php
@@ -154,7 +154,7 @@ class AuthorSubmitStep1Form extends AuthorSubmitForm {
 			$this->article->setSectionId($this->getData('sectionId'));
 			$this->article->stampStatusModified();
 			$this->article->setSubmissionProgress($this->step + 1);
-			$this->article->setLanguage(String::substr($article->getLocale(), 0, 2));
+			$this->article->setLanguage(String::substr($this->article->getLocale(), 0, 2));
 			$this->article->setCommentsToEditor($this->getData('commentsToEditor'));
 
 			// Set user to initial author
