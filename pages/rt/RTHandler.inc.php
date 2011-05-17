53d2a7269bb7118787489c635ca733dd7ef0e6c8
michael-pkp <michael.pkp@gmail.com>
2011-05-17 15:47:20 -0700

    *6634* RT "Print this article" button doesn't work with custom galley identifiers

--- pages/rt/RTHandler.inc.php
+++ pages/rt/RTHandler.inc.php
@@ -209,7 +209,7 @@ class RTHandler extends ArticleHandler {
 	function printerFriendly($args, &$request) {
 		$router =& $request->getRouter();
 		$articleId = isset($args[0]) ? $args[0] : 0;
-		$galleyId = isset($args[1]) ? (int) $args[1] : 0;
+		$galleyId = isset($args[1]) ? $args[1] : 0;
 
 		$this->validate($request, $articleId, $galleyId);
 		$journal =& $router->getContext($request);
@@ -226,7 +226,11 @@ class RTHandler extends ArticleHandler {
 		}
 
 		$articleGalleyDao =& DAORegistry::getDAO('ArticleGalleyDAO');
-		$galley =& $articleGalleyDao->getGalley($galleyId, $article->getId());
+		if ($journal->getSetting('enablePublicGalleyId')) {
+			$galley =& $articleGalleyDao->getGalleyByBestGalleyId($galleyId, $article->getId());
+		} else {
+			$galley =& $articleGalleyDao->getGalley($galleyId, $article->getId());
+		}
 
 		$sectionDao =& DAORegistry::getDAO('SectionDAO');
 		$section =& $sectionDao->getSection($article->getSectionId(), $journal->getJournalId(), true);
