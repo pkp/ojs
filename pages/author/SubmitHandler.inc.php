3d5e0ef8d00e31ae76b16da816e00cbcb9f6098e
Matthew Crider <mattcrider@gmail.com>
2011-08-03 13:07:30 -0700

    *6802* Fix editor list construction for new article notifications

--- pages/author/SubmitHandler.inc.php
+++ pages/author/SubmitHandler.inc.php
@@ -163,9 +163,9 @@ class SubmitHandler extends AuthorHandler {
 					$article =& $articleDao->getArticle($articleId);
 					$roleDao =& DAORegistry::getDAO('RoleDAO');
 					$notificationUsers = array();
-					$editors = $roleDao->getUsersByRoleId(ROLE_ID_EDITOR);
-					$notifyUsers = $editors->toArray();
-					while ($editor =& $editors->next()) {
+					$editors = $roleDao->getUsersByRoleId(ROLE_ID_EDITOR, $journal->getId());
+					while (!$editors->eof()) {
+						$editor =& $editors->next();
 						$url = $request->url(null, 'editor', 'submission', $articleId);
 						$notificationManager->createNotification(
 							$editor->getId(), 'notification.type.articleSubmitted',
