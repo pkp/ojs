60005c272da2c2fb8c0fee67639db1280f5dca68
Alec Smecher <alec@smecher.bc.ca>
2012-03-01 08:56:57 -0800

    *7188* Add CURL error information to paypal dump

--- plugins/paymethod/paypal/PayPalPlugin.inc.php
+++ plugins/paymethod/paypal/PayPalPlugin.inc.php
@@ -175,6 +175,7 @@ class PayPalPlugin extends PaymethodPlugin {
 				curl_setopt($ch, CURLOPT_HTTPHEADER, Array('Content-Type: application/x-www-form-urlencoded', 'Content-Length: ' . strlen($req)));
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
 				$ret = curl_exec ($ch);
+				$curlError = curl_error($ch);
 				curl_close ($ch);
 
 				// Check the confirmation response and handle as necessary.
@@ -286,7 +287,7 @@ class PayPalPlugin extends PaymethodPlugin {
 					$mail->assignParams(array(
 						'journalName' => $journal->getLocalizedTitle(),
 						'postInfo' => print_r($_POST, true),
-						'additionalInfo' => "Confirmation return: $ret",
+						'additionalInfo' => "Confirmation return: $ret\nCURL error: $curlError",
 						'serverVars' => print_r($_SERVER, true)
 					));
 					$mail->send();
