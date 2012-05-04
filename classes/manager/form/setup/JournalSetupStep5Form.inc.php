6146c3b041ebeed1566b36f2df440c0eba0a1f32
michael-pkp <michael.pkp@gmail.com>
2012-05-04 16:30:23 -0700

    *6495* Favicon doesn't display in IE (move mime type detection to setup upload)

--- classes/manager/form/setup/JournalSetupStep5Form.inc.php
+++ classes/manager/form/setup/JournalSetupStep5Form.inc.php
@@ -149,6 +149,7 @@ class JournalSetupStep5Form extends JournalSetupForm {
 					'uploadName' => $uploadName,
 					'width' => $width,
 					'height' => $height,
+					'mimeType' => $fileManager->getUploadedFileType($settingName),
 					'dateUploaded' => Core::getCurrentDate()
 				);
 
