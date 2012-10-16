49a774a5a280baf568bf966fa7814a5f8f91081d
Alec Smecher <alec@smecher.bc.ca>
2012-10-16 11:45:13 -0700

    *7961* Prep OJS for grid-based plugin management

---
+++ controllers/grid/settings/plugins/SettingsPluginGridHandler.inc.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @file controllers/grid/settings/plugins/SettingsPluginGridHandler.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SettingsPluginGridHandler
+ * @ingroup controllers_grid_settings_plugins
+ *
+ * @brief Handle plugin grid requests.
+ */
+
+import('lib.pkp.classes.controllers.grid.plugins.PluginGridHandler');
+
+class SettingsPluginGridHandler extends PluginGridHandler {
+	/**
+	 * Constructor
+	 */
+	function SettingsPluginGridHandler() {
+		$roles = array(ROLE_ID_SITE_ADMIN, ROLE_ID_JOURNAL_MANAGER);
+		$this->addRoleAssignment($roles, array('plugin'));
+		parent::PluginGridHandler($roles);
+	}
+
+
+	//
+	// Extended methods from PluginGridHandler
+	//
+	/**
+	 * @see PluginGridHandler::loadData()
+	 */
+	function getCategoryData($categoryDataElement, $filter) {
+		$plugins = parent::getCategoryData($categoryDataElement, $filter);
+
+		$journalDao =& DAORegistry::getDAO('JournalDAO');
+		$journals =& $journalDao->getJournals();
+		$singleJournal = false;
+		if ($journals->getCount() == 1) {
+			$singleJournal = true;
+		}
+
+		$userRoles = $this->getAuthorizedContextObject(ASSOC_TYPE_USER_ROLES);
+
+		$showSitePlugins = false;
+		if (in_array(ROLE_ID_SITE_ADMIN, $userRoles)) {
+			$showSitePlugins = true;
+		}
+
+		if ($showSitePlugins) {
+			return $plugins;
+		} else {
+			$contextLevelPlugins = array();
+			foreach ($plugins as $plugin) {
+				if (!$plugin->isSitePlugin()) {
+					$contextLevelPlugins[$plugin->getName()] = $plugin;
+				}
+				unset($plugin);
+			}
+			return $contextLevelPlugins;
+		}
+	}
+
+	//
+	// Overriden template methods.
+	//
+	/**
+	 * @see CategoryGridHandler::getCategoryRowInstance()
+	 */
+	function getRowInstance() {
+		$userRoles = $this->getAuthorizedContextObject(ASSOC_TYPE_USER_ROLES);
+
+		import('controllers.grid.plugins.PluginGridRow');
+		return new PluginGridRow($userRoles, CONTEXT_JOURNAL);
+	}
+
+	/**
+	 * @see GridHandler::authorize()
+	 */
+	function authorize($request, $args, $roleAssignments) {
+		$category = $request->getUserVar('category');
+		$pluginName = $request->getUserVar('plugin');
+		$verb = $request->getUserVar('verb');
+
+		if ($category && $pluginName) {
+			import('classes.security.authorization.OjsPluginAccessPolicy');
+			if ($verb) {
+				$accessMode = ACCESS_MODE_MANAGE;
+			} else {
+				$accessMode = ACCESS_MODE_ADMIN;
+			}
+
+			$this->addPolicy(new OjsPluginAccessPolicy($request, $args, $roleAssignments, $accessMode));
+		}
+
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+}
+
+?>
