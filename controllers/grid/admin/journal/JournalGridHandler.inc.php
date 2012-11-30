9e1f8ca5af977c936faa946e5e550b853fad2ca6
Alec Smecher <alec@smecher.bc.ca>
2012-11-29 16:20:19 -0800

    *8046* Port context grid to OJS

---
+++ controllers/grid/admin/journal/JournalGridHandler.inc.php
@@ -0,0 +1,195 @@
+<?php
+
+/**
+ * @file controllers/grid/admin/journal/JournalGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class JournalGridHandler
+ * @ingroup controllers_grid_admin_journal
+ *
+ * @brief Handle journal grid requests.
+ */
+
+import('lib.pkp.controllers.grid.admin.context.ContextGridHandler');
+
+import('controllers.grid.admin.journal.JournalGridRow');
+import('controllers.grid.admin.journal.form.JournalSiteSettingsForm');
+
+class JournalGridHandler extends ContextGridHandler {
+	/**
+	 * Constructor
+	 */
+	function JournalGridHandler() {
+		parent::ContextGridHandler();
+	}
+
+
+	//
+	// Implement template methods from PKPHandler.
+	//
+	/**
+	 * @see PKPHandler::initialize()
+	 */
+	function initialize(&$request) {
+		// Load user-related translations.
+		AppLocale::requireComponents(
+			LOCALE_COMPONENT_OJS_ADMIN,
+			LOCALE_COMPONENT_OJS_MANAGER,
+			LOCALE_COMPONENT_APPLICATION_COMMON
+		);
+
+		parent::initialize($request);
+
+		// Basic grid configuration.
+		$this->setTitle('journal.journals');
+	}
+
+
+	//
+	// Implement methods from GridHandler.
+	//
+	/**
+	 * @see GridHandler::getRowInstance()
+	 * @return UserGridRow
+	 */
+	function &getRowInstance() {
+		$row = new JournalGridRow();
+		return $row;
+	}
+
+	/**
+	 * @see GridHandler::loadData()
+	 * @param $request PKPRequest
+	 * @return array Grid data.
+	 */
+	function loadData(&$request) {
+		// Get all journals.
+		$journalDao =& DAORegistry::getDAO('JournalDAO');
+		$journals = $journalDao->getAll();
+
+		return $journals->toAssociativeArray();
+	}
+
+	/**
+	 * @see lib/pkp/classes/controllers/grid/GridHandler::setDataElementSequence()
+	 */
+	function setDataElementSequence(&$request, $rowId, &$journal, $newSequence) {
+		$journalDao = DAORegistry::getDAO('JournalDAO'); /* @var $journalDao JournalDAO */
+		$journal->setSequence($newSequence);
+		$journalDao->updateObject($journal);
+	}
+
+
+	//
+	// Public grid actions.
+	//
+	/**
+	 * Edit an existing journal.
+	 * @param $args array
+	 * @param $request PKPRequest
+	 * @return string Serialized JSON object
+	 */
+	function editContext($args, &$request) {
+
+		// Identify the journal Id.
+		$journalId = $request->getUserVar('rowId');
+
+		// Form handling.
+		$settingsForm = new JournalSiteSettingsForm(!isset($journalId) || empty($journalId) ? null : $journalId);
+		$settingsForm->initData();
+		$json = new JSONMessage(true, $settingsForm->fetch($args, $request));
+
+		return $json->getString();
+	}
+
+	/**
+	 * Update an existing journal.
+	 * @param $args array
+	 * @param $request PKPRequest
+	 * @return string Serialized JSON object
+	 */
+	function updateContext($args, $request) {
+		// Identify the journal Id.
+		$journalId = $request->getUserVar('contextId');
+
+		// Form handling.
+		$settingsForm = new JournalSiteSettingsForm($journalId);
+		$settingsForm->readInputData();
+
+		if ($settingsForm->validate()) {
+			PluginRegistry::loadCategory('blocks');
+
+			$settingsForm->execute($request);
+
+			// Create the notification.
+			$notificationMgr = new NotificationManager();
+			$user =& $request->getUser();
+			$notificationMgr->createTrivialNotification($user->getId());
+
+			return DAO::getDataChangedEvent($journalId);
+		}
+
+		$json = new JSONMessage(false);
+		return $json->getString();
+	}
+
+	/**
+	 * Delete a journal.
+	 * @param $args array
+	 * @param $request PKPRequest
+	 * @return string Serialized JSON object
+	 */
+	function deleteContext($args, &$request) {
+		// Identify the current context.
+		$context =& $request->getContext();
+
+		// Identify the journal Id.
+		$journalId = $request->getUserVar('rowId');
+		$journalDao =& DAORegistry::getDAO('JournalDAO');
+		$journal =& $journalDao->getById($journalId);
+
+		if ($journal) {
+			$journalDao->deleteById($journalId);
+
+			// Delete journal file tree
+			// FIXME move this somewhere better.
+			import('lib.pkp.classes.file.FileManager');
+			$fileManager = new FileManager($journalId);
+			$journalPath = Config::getVar('files', 'files_dir') . '/journals/' . $journalId;
+			$fileManager->rmtree($journalPath);
+
+			import('classes.file.PublicFileManager');
+			$publicFileManager = new PublicFileManager();
+			$publicFileManager->rmtree($publicFileManager->getJournalFilesPath($journalId));
+
+			return DAO::getDataChangedEvent($journalId);
+		}
+
+		$json = new JSONMessage(false);
+		return $json->getString();
+	}
+
+
+	//
+	// Private helper methods.
+	//
+	/**
+	 * Get the "add context" locale key
+	 * @return string
+	 */
+	protected function _getAddContextKey() {
+		return 'admin.journals.create';
+	}
+
+	/**
+	 * Get the context name locale key
+	 * @return string
+	 */
+	protected function _getContextNameKey() {
+		return 'manager.setup.journalTitle';
+	}
+}
+
+?>
---
+++ controllers/grid/admin/journal/form/JournalSiteSettingsForm.inc.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * @file controllers/grid/admin/journal/form/JournalSiteSettingsForm.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class JournalSiteSettingsForm
+ * @ingroup controllers_grid_admin_journal_form
+ *
+ * @brief Form for site administrator to edit basic journal settings.
+ */
+
+import('lib.pkp.controllers.grid.admin.context.form.ContextSiteSettingsForm');
+
+class JournalSiteSettingsForm extends ContextSiteSettingsForm {
+	/**
+	 * Constructor.
+	 * @param $contextId omit for a new journal
+	 */
+	function JournalSiteSettingsForm($contextId = null) {
+		parent::ContextSiteSettingsForm('admin/journalSettings.tpl', $contextId);
+
+		// Validation checks for this form
+		$this->addCheck(new FormValidatorLocale($this, 'name', 'required', 'admin.journals.form.titleRequired'));
+		$this->addCheck(new FormValidator($this, 'path', 'required', 'admin.journals.form.pathRequired'));
+		$this->addCheck(new FormValidatorAlphaNum($this, 'path', 'required', 'admin.journals.form.pathAlphaNumeric'));
+		$this->addCheck(new FormValidatorCustom($this, 'path', 'required', 'admin.journals.form.pathExists', create_function('$path,$form,$journalDao', 'return !$journalDao->existsByPath($path) || ($form->getData(\'oldPath\') != null && $form->getData(\'oldPath\') == $path);'), array(&$this, DAORegistry::getDAO('JournalDAO'))));
+	}
+
+	/**
+	 * Initialize form data from current settings.
+	 */
+	function initData() {
+		if (isset($this->contextId)) {
+			$journalDao =& DAORegistry::getDAO('JournalDAO');
+			$journal =& $journalDao->getById($this->contextId);
+
+			parent::initData($journal);
+		} else {
+			parent::initData();
+		}
+	}
+
+	/**
+	 * Assign form data to user-submitted data.
+	 */
+	function readInputData() {
+		parent::readInputData();
+
+		if ($this->contextId) {
+			$journalDao =& DAORegistry::getDAO('JournalDAO');
+			$journal =& $journalDao->getById($this->contextId);
+			if ($journal) $this->setData('oldPath', $journal->getPath());
+		}
+	}
+
+	/**
+	 * Save journal settings.
+	 * @param $request PKPRequest
+	 */
+	function execute($request) {
+		$journalDao =& DAORegistry::getDAO('JournalDAO');
+
+		if (isset($this->contextId)) {
+			$journal =& $journalDao->getById($this->contextId);
+		}
+
+		if (!isset($journal)) {
+			$journal = $journalDao->newDataObject();
+		}
+
+		$journal->setPath($this->getData('path'));
+		$journal->setEnabled($this->getData('enabled'));
+
+		if ($journal->getId() != null) {
+			$isNewJournal = false;
+			$journalDao->updateObject($journal);
+			$section = null;
+		} else {
+			$isNewJournal = true;
+			$site =& Request::getSite();
+
+			// Give it a default primary locale
+			$journal->setPrimaryLocale ($site->getPrimaryLocale());
+
+			$journalId = $journalDao->insertObject($journal);
+			$journalDao->resequence();
+
+			// Make the site administrator the journal manager of newly created journals
+			$sessionManager =& SessionManager::getManager();
+			$userSession =& $sessionManager->getUserSession();
+			if ($userSession->getUserId() != null && $userSession->getUserId() != 0 && !empty($journalId)) {
+				$role = new Role();
+				$role->setJournalId($journalId);
+				$role->setUserId($userSession->getUserId());
+				$role->setRoleId(ROLE_ID_JOURNAL_MANAGER);
+
+				$roleDao =& DAORegistry::getDAO('RoleDAO');
+				$roleDao->insertRole($role);
+			}
+
+			// Make the file directories for the journal
+			import('lib.pkp.classes.file.FileManager');
+			$fileManager = new FileManager();
+			$fileManager->mkdir(Config::getVar('files', 'files_dir') . '/journals/' . $journalId);
+			$fileManager->mkdir(Config::getVar('files', 'files_dir'). '/journals/' . $journalId . '/articles');
+			$fileManager->mkdir(Config::getVar('files', 'files_dir'). '/journals/' . $journalId . '/issues');
+			$fileManager->mkdir(Config::getVar('files', 'public_files_dir') . '/journals/' . $journalId);
+
+			// Install default journal settings
+			$journalSettingsDao =& DAORegistry::getDAO('JournalSettingsDAO');
+			$names = $this->getData('name');
+			AppLocale::requireComponents(LOCALE_COMPONENT_OJS_DEFAULT, LOCALE_COMPONENT_APPLICATION_COMMON);
+			$journalSettingsDao->installSettings($journalId, 'registry/journalSettings.xml', array(
+				'indexUrl' => Request::getIndexUrl(),
+				'journalPath' => $this->getData('path'),
+				'primaryLocale' => $site->getPrimaryLocale(),
+				'journalName' => $names[$site->getPrimaryLocale()]
+			));
+
+			// Install the default RT versions.
+			import('classes.rt.ojs.JournalRTAdmin');
+			$journalRtAdmin = new JournalRTAdmin($journalId);
+			$journalRtAdmin->restoreVersions(false);
+
+			// Create a default "Articles" section
+			$sectionDao =& DAORegistry::getDAO('SectionDAO');
+			$section = new Section();
+			$section->setJournalId($journal->getId());
+			$section->setTitle(__('section.default.title'), $journal->getPrimaryLocale());
+			$section->setAbbrev(__('section.default.abbrev'), $journal->getPrimaryLocale());
+			$section->setMetaIndexed(true);
+			$section->setMetaReviewed(true);
+			$section->setPolicy(__('section.default.policy'), $journal->getPrimaryLocale());
+			$section->setEditorRestricted(false);
+			$section->setHideTitle(false);
+			$sectionDao->insertSection($section);
+		}
+		$journal->updateSetting('name', $this->getData('name'), 'string', true);
+		$journal->updateSetting('description', $this->getData('description'), 'string', true);
+
+		// Make sure all plugins are loaded for settings preload
+		PluginRegistry::loadAllPlugins();
+
+		HookRegistry::call('JournalSiteSettingsForm::execute', array(&$this, &$journal, &$section, &$isNewJournal));
+	}
+}
+
+?>
