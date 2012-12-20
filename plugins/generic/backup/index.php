86460b45ec717418249f8b44ce089bde953c31d5
Alec Smecher <alec@smecher.bc.ca>
2012-12-19 16:24:26 -0800

    *4846* Add backup plugin

---
+++ plugins/generic/backup/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @defgroup plugins_generic_backup
+ */
+ 
+/**
+ * @file plugins/generic/back/up/index.php
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @brief Wrapper for backup plugin.
+ *
+ * @ingroup plugins_generic_backup
+ */
+
+require_once('BackupPlugin.inc.php');
+
+return new BackupPlugin();
+
+?> 
