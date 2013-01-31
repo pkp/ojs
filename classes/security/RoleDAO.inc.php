143ca1a11b84dd9a0768a9b443f38c097ed32d5d
Jason Nugent <jason.nugent@gmail.com>
2013-01-31 16:22:37 -0400

    *8095* port getRoleNames method to new format, update locale keys

--- classes/security/RoleDAO.inc.php
+++ classes/security/RoleDAO.inc.php
@@ -418,13 +418,36 @@ class RoleDAO extends PKPRoleDAO {
 	}
 
 	/**
-	 * Get role names
-	 * @param $roleId int ROLE_ID_...
-	 * @param $plural boolean
+	 * Get a mapping of role keys and i18n key names.
+	 * @param boolean $contextOnly If false, also returns site-level roles (Site admin)
+	 * @param array $roleIds Only return role names of these IDs
 	 * @return array
 	 */
-	static function getRoleNames($roleId, $plural = false) {
-		return array(self::getRoleName($roleId, $plural));
+	static function getRoleNames($contextOnly = false, $roleIds = null) {
+		$parentRoleNames = parent::getRoleNames($contextOnly);
+
+		$journalRoleNames = array(
+			ROLE_ID_MANAGER => 'user.role.manager',
+			ROLE_ID_EDITOR => 'user.role.editor',
+			ROLE_ID_GUEST_EDITOR => 'user.role.seriesEditor',
+			ROLE_ID_ASSISTANT => 'user.role.journalAssistant',
+			ROLE_ID_SECTION_EDITOR => 'user.role.sectionEditor',
+			ROLE_ID_COPYEDITOR => 'user.role.copyeditor',
+			ROLE_ID_PROOFREADER => 'user.role.proofreader',
+			ROLE_ID_LAYOUT_EDITOR => 'user.role.layoutEditor',
+			ROLE_ID_SUBSCRIPTION_MANAGER => 'user.role.subscriptionManager',
+		);
+		$roleNames = $parentRoleNames + $journalRoleNames;
+
+		if(!empty($roleIds)) {
+			$returner = array();
+			foreach($roleIds as $roleId) {
+				if(isset($roleNames[$roleId])) $returner[$roleId] = $roleNames[$roleId];
+			}
+			return $returner;
+		} else {
+			return $roleNames;
+		}
 	}
 
 	/**
