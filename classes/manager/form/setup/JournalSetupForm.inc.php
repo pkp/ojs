689101f81b1259129d21613b85fc5d21bb2b75e1
Barbara Hui <barbara.hui@ucop.edu>
2013-02-22 13:04:05 -0800

    Removed error_log debugging line.

--- classes/manager/form/setup/JournalSetupForm.inc.php
+++ classes/manager/form/setup/JournalSetupForm.inc.php
@@ -73,7 +73,6 @@ class JournalSetupForm extends Form {
 
 		foreach ($this->_data as $name => $value) {
 			if (isset($this->settings[$name])) {
-				error_log("name: " . $name);
 				$isLocalized = in_array($name, $this->getLocaleFieldNames());
 				$settingsDao->updateSetting(
 					$journal->getId(),
