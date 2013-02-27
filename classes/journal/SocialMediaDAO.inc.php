70f8aafa82ea35345a15d34f511c20b1254bfbcc
Alec Smecher <alec@smecher.bc.ca>
2013-02-27 15:11:51 -0800

    *8087* Add navigation settings to OJS

---
+++ classes/journal/SocialMediaDAO.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file classes/journal/SocialMediaDAO.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SocialMediaDAO
+ * @ingroup journal
+ * @see SocialMedia
+ *
+ * @brief Operations for retrieving and modifying SocialMedia objects.
+ */
+
+import ('lib.pkp.classes.context.PKPSocialMediaDAO');
+import ('classes.journal.SocialMedia');
+
+class SocialMediaDAO extends PKPSocialMediaDAO {
+	/**
+	 * Constructor
+	 */
+	function SocialMediaDAO() {
+		parent::PKPSocialMediaDAO();
+	}
+
+	/**
+	 * Construct a new data object corresponding to this DAO.
+	 * @return SocialMedia
+	 */
+	function newDataObject() {
+		return new SocialMedia();
+	}
+}
+
+?>
