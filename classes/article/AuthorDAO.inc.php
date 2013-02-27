a96cb554f8032593581da9d196c52baf8a02fb7f
bozana <bozana.bokan@fu-berlin.de>
2013-02-27 16:33:09 +0100

    fixed browse by author

--- classes/article/AuthorDAO.inc.php
+++ classes/article/AuthorDAO.inc.php
@@ -115,11 +115,9 @@ class AuthorDAO extends PKPAuthorDAO {
 				aa.first_name,
 				aa.middle_name,
 				aa.last_name,
-				SUBSTRING(asl.setting_value FROM 1 FOR 255) AS affiliation_l,
-				asl.locale,
-				SUBSTRING(aspl.setting_value FROM 1 FOR 255) AS affiliation_pl,
-				aspl.locale AS primary_locale,
-				aa.country
+				CASE WHEN asl.setting_value = \'\' THEN NULL ELSE SUBSTRING(asl.setting_value FROM 1 FOR 255) END AS affiliation_l,
+				CASE WHEN aspl.setting_value = \'\' THEN NULL ELSE SUBSTRING(aspl.setting_value FROM 1 FOR 255) END AS affiliation_pl,
+				CASE WHEN aa.country = \'\' THEN NULL ELSE aa.country END
 			FROM	authors aa
 				LEFT JOIN author_settings aspl ON (aa.author_id = aspl.author_id AND aspl.setting_name = ? AND aspl.locale = ?)
 				LEFT JOIN author_settings asl ON (aa.author_id = asl.author_id AND asl.setting_name = ? AND asl.locale = ?)
