143dac8aa6bce0fa949068bc16d02eb2c17a3202
Alec Smecher <alec@smecher.bc.ca>
2013-03-14 15:41:41 -0700

    Conventions

--- classes/file/TemporaryFileManager.inc.php
+++ classes/file/TemporaryFileManager.inc.php
@@ -54,7 +54,7 @@ class TemporaryFileManager extends PKPTemporaryFileManager {
 			$temporaryFile->setOriginalFileName($articleFile->getOriginalFileName());
 			$temporaryFile->setDateUploaded(Core::getCurrentDate());
 
-			$temporaryFileDao->insertTemporaryFile($temporaryFile);
+			$temporaryFileDao->insertObject($temporaryFile);
 
 			return $temporaryFile;
 
