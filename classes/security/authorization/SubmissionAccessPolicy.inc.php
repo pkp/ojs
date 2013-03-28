871bbf313852a0e89031abfa428b7b1228b705b8
Jason Nugent <jason.nugent@gmail.com>
2013-03-28 13:34:49 -0300

    *8148* new security policies

---
+++ classes/security/authorization/SubmissionAccessPolicy.inc.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @file classes/security/authorization/SubmissionAccessPolicy.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SubmissionAccessPolicy
+ * @ingroup security_authorization
+ *
+ * @brief Class to control (write) access to submissions and (read) access to
+ * submission details in OJS.
+ */
+
+import('lib.pkp.classes.security.authorization.PKPSubmissionAccessPolicy');
+
+class SubmissionAccessPolicy extends PKPSubmissionAccessPolicy {
+	/**
+	 * Constructor
+	 * @param $request PKPRequest
+	 * @param $args array request parameters
+	 * @param $roleAssignments array
+	 * @param $submissionParameterName string the request parameter we
+	 *  expect the submission id in.
+	 */
+	function SubmissionAccessPolicy(&$request, $args, $roleAssignments, $submissionParameterName = 'submissionId') {
+		parent::PKPSubmissionAccessPolicy($request, $args, $roleAssignments, $submissionParameterName);
+
+		$submissionAccessPolicy = $this->_baseSubmissionAccessPolicy;
+
+		//
+		// Series editor role
+		//
+		if (isset($roleAssignments[ROLE_ID_SUB_EDITOR])) {
+			// 1) Series editors can access all operations on submissions ...
+			$sectionEditorSubmissionAccessPolicy = new PolicySet(COMBINING_DENY_OVERRIDES);
+			$sectionEditorSubmissionAccessPolicy->addPolicy(new RoleBasedHandlerOperationPolicy($request, ROLE_ID_SUB_EDITOR, $roleAssignments[ROLE_ID_SUB_EDITOR]));
+
+			// 2) ... but only if the requested submission is part of their series.
+			import('classes.security.authorization.internal.SectionAssignmentPolicy');
+			$sectionEditorSubmissionAccessPolicy->addPolicy(new SectionAssignmentPolicy($request));
+			$submissionAccessPolicy->addPolicy($sectionEditorSubmissionAccessPolicy);
+		}
+
+		$this->addPolicy($submissionAccessPolicy);
+	}
+}
+
+?>
