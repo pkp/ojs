ab93ebc4b3f49352d8b52dbd19e34c1d1a21444b
Bruno Beghelli <bruno.beghelli@gmail.com>
2013-04-16 01:33:42 -0300

    *8015* Introduced plugin that implements a hook to provide usage event

---
+++ plugins/generic/usageEvent/UsageEventPlugin.inc.php
@@ -0,0 +1,346 @@
+<?php
+
+/**
+ * @file plugins/generic/usageEvent/UsageEventPlugin.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class UsageEventPlugin
+ * @ingroup plugins_generic_usageEvent
+ *
+ * @brief Provide usage event to other statistics plugins.
+ */
+
+
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+// Our own and OA-S classification types.
+define('USAGE_EVENT_PLUGIN_CLASSIFICATION_BOT', 'bot');
+define('USAGE_EVENT_PLUGIN_CLASSIFICATION_ADMIN', 'administrative');
+
+class UsageEventPlugin extends GenericPlugin {
+
+	//
+	// Implement methods from PKPPlugin.
+	//
+	/**
+	* @see LazyLoadPlugin::register()
+	*/
+	function register($category, $path) {
+		$success = parent::register($category, $path);
+
+		if ($success) {
+			// Register callbacks.
+			HookRegistry::register('TemplateManager::display', array($this, 'getUsageEvent'));
+			HookRegistry::register('ArticleHandler::viewFile', array($this, 'getUsageEvent'));
+			HookRegistry::register('ArticleHandler::downloadFile', array($this, 'getUsageEvent'));
+			HookRegistry::register('ArticleHandler::downloadSuppFile', array($this, 'getUsageEvent'));
+			HookRegistry::register('IssueHandler::viewFile', array($this, 'getUsageEvent'));
+			HookRegistry::register('FileManager::downloadFileFinished', array($this, 'getUsageEvent'));
+		}
+
+		return $success;
+	}
+
+	/**
+	 * @see LazyLoadPlugin::getName()
+	 */
+	function getName() {
+		return 'usageEvent';
+	}
+
+	/**
+	 * @see PKPPlugin::getDisplayName()
+	 */
+	function getDisplayName() {
+		return __('plugins.generic.usageEvent.displayName');
+	}
+
+	/**
+	 * @see PKPPlugin::getDescription()
+	 */
+	function getDescription() {
+		return __('plugins.generic.usageEvent.description');
+	}
+
+	/**
+	 * @see LazyLoadPlugin::getEnabled()
+	 */
+	function getEnabled() {
+		return true;
+	}
+
+	/**
+	* @see PKPPlugin::isSitePlugin()
+	*/
+	function isSitePlugin() {
+		return true;
+	}
+
+
+	//
+	// Public methods.
+	//
+	/**
+	 * Get the unique site id.
+	 * @return mixed string or null
+	 */
+	function getUniqueSiteId() {
+		return $this->getSetting(0, 'uniqueSiteId');
+	}
+
+
+	//
+	// Hook implementations.
+	//
+	/**
+	 * Get usage event and pass it to the registered plugins, if any.
+	 */
+	function getUsageEvent($hookName, $args) {
+		// Check if we have a registration to receive the usage event.
+		$hooks = HookRegistry::getHooks();
+		if (array_key_exists('UsageEventPlugin::getUsageEvent', $hooks)) {
+
+			$usageEvent = $this->_buildUsageEvent($hookName, $args);
+
+			// If we have a real usage event, then let registered callbacks receive it.
+			if ($usageEvent) {
+				HookRegistry::call('UsageEventPlugin::getUsageEvent', array_merge(array($hookName, $usageEvent), $args));
+			}
+		}
+		return false;
+	}
+
+
+	//
+	// Private helper methods.
+	//
+	/**
+	 * Build an usage event.
+	 * @param $hookName string
+	 * @param $args array
+	 * @return array
+	 */
+	private function _buildUsageEvent($hookName, $args) {
+		$request = $this->getRequest();
+		$router = $request->getRouter(); /* @var $router PageRouter */
+		$templateMgr = $args[0]; /* @var $templateMgr TemplateManager */
+
+		// Check whether we are in journal context.
+		$journal = $router->getContext($request);
+		if (!$journal) return false;
+
+		// Prepare request information.
+		$downloadSuccess = false;
+		switch ($hookName) {
+
+			// Article abstract, HTML galley and remote galley.
+			case 'TemplateManager::display':
+				// We are only interested in access to the article abstract/galley view page.
+				$page = $router->getRequestedPage($request);
+				$op = $router->getRequestedOp($request);
+				if ($page != 'article' || !($op == 'view' || $op == 'articleView')) return false;
+
+				$galley = $templateMgr->get_template_vars('galley'); /* @var $galley ArticleGalley */
+				$article = $templateMgr->get_template_vars('article');
+				if ($galley) {
+					if ($galley->isHTMLGalley() || $galley->getRemoteURL()) {
+						$pubObject = $galley;
+						$assocType = ASSOC_TYPE_GALLEY;
+						$canonicalUrlParams = array($article->getBestArticleId(), $pubObject->getBestGalleyId($journal));
+						$idParams = array('a' . $article->getId(), 'g' . $pubObject->getId());
+					} else {
+						// This is an access to an intermediary galley page which we
+						// do not count.
+						return false;
+					}
+				} else {
+					$pubObject = $article;
+					$assocType = ASSOC_TYPE_ARTICLE;
+					$canonicalUrlParams = array($pubObject->getBestArticleId($journal));
+					$idParams = array('a' . $pubObject->getId());
+				}
+				// The article and HTML/remote galley pages do not download anything.
+				$downloadSuccess = true;
+				$canonicalUrlOp = 'view';
+				break;
+
+				// Article galley (except for HTML and remote galley).
+			case 'ArticleHandler::viewFile':
+			case 'ArticleHandler::downloadFile':
+				$pubObject = $args[1];
+				$assocType = ASSOC_TYPE_GALLEY;
+				$canonicalUrlOp = 'download';
+				$article = $args[0];
+				$canonicalUrlParams = array($article->getBestArticleId(), $pubObject->getBestGalleyId($journal));
+				$idParams = array('a' . $article->getId(), 'g' . $pubObject->getId());
+				break;
+
+				// Supplementary file.
+			case 'ArticleHandler::downloadSuppFile':
+				$pubObject = $args[1];
+				$assocType = ASSOC_TYPE_SUPP_FILE;
+				$canonicalUrlOp = 'downloadSuppFile';
+				$article = $args[0];
+				$canonicalUrlParams = array($article->getBestArticleId(), $pubObject->getBestSuppFileId($journal));
+				$idParams = array('a' . $article->getId(), 's' . $pubObject->getId());
+				break;
+
+				// Issue galley.
+			case 'IssueHandler::viewFile':
+				$pubObject = $args[1];
+				$assocType = ASSOC_TYPE_ISSUE_GALLEY;
+				$canonicalUrlOp = 'download';
+				$issue = $args[0];
+				$canonicalUrlParams = array($issue->getBestIssueId(), $pubObject->getBestGalleyId($journal));
+				$idParams = array('i' . $issue->getId(), 'ig' . $pubObject->getId());
+				break;
+
+			default:
+				// Why are we called from an unknown hook?
+				assert(false);
+		}
+
+		// Timestamp.
+		$time = Core::getCurrentDate();
+
+		// Actual document size, MIME type.
+		if ($assocType == ASSOC_TYPE_ARTICLE) {
+			// Article abstract.
+			$docSize = 0;
+			$mimeType = 'text/html';
+		} else {
+			// Files.
+			$docSize = (int)$pubObject->getFileSize();
+			$mimeType = $pubObject->getFileType();
+		}
+
+		// Canonical URL.
+		if ($assocType == ASSOC_TYPE_ISSUE_GALLEY) {
+			$canonicalUrlPage = 'issue';
+		} else {
+			$canonicalUrlPage = 'article';
+		}
+		$canonicalUrl = $router->url(
+			$request, null, $canonicalUrlPage, $canonicalUrlOp, $canonicalUrlParams
+		);
+
+		// Public identifiers.
+		// 1) A unique OJS-internal ID that will help us to easily attribute
+		//    statistics to a specific publication object.
+		array_unshift($idParams, 'j' . $journal->getId());
+		$siteId = $this->getUniqueSiteId();
+		if (empty($siteId)) {
+			// Create a globally unique, persistent site ID
+			// so that we can uniquely identify publication
+			// objects from this site, even if the URL or any
+			// other externally influenced information changes.
+			$siteId = uniqid();
+			$this->updateSetting(0, 'uniqueSiteId', $siteId);
+		}
+		array_unshift($idParams, $siteId);
+		$ojsId = 'ojs:' . implode('-', $idParams);
+		$identifiers = array('other::ojs' => $ojsId);
+
+		// 2) Standardized public identifiers, e.g. DOI, URN, etc.
+		if (!is_a($pubObject, 'IssueGalley')) {
+			$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $journal->getId());
+			if (is_array($pubIdPlugins)) {
+				foreach ($pubIdPlugins as $pubIdPlugin) {
+					if (!$pubIdPlugin->getEnabled()) continue;
+					$pubId = $pubIdPlugin->getPubId($pubObject);
+					if ($pubId) {
+						$identifiers[$pubIdPlugin->getPubIdType()] = $pubId;
+					}
+				}
+			}
+		}
+
+		// Service URI.
+		$serviceUri = $router->url($request, $journal->getPath());
+
+		// IP and Host.
+		$ip = $request->getRemoteAddr();
+		$host = null;
+		if (isset($_SERVER['REMOTE_HOST'])) {
+			// We do NOT actively look up the remote host to
+			// avoid the performance penalty. We only set the remote
+			// host if we get it "for free".
+			$host = $_SERVER['REMOTE_HOST'];
+		}
+
+		// HTTP user agent.
+		$userAgent = $request->getUserAgent();
+
+		// HTTP referrer.
+		$referrer = (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null);
+
+		// User and roles.
+		$user = $request->getUser();
+		$roles = array();
+		if ($user) {
+			$roleDao = DAORegistry::getDAO('RoleDAO');
+			$rolesByContext = $roleDao->getByUserIdGroupedByContext($user->getId());
+			foreach (array(CONTEXT_SITE, $journal->getId()) as $context) {
+				if(isset($rolesByContext[$context])) {
+					foreach ($rolesByContext[$context] as $role) {
+						$roles[] = $role->getRoleId();
+					}
+				}
+			}
+		}
+
+		// Try a simple classification of the request.
+		$classification = null;
+		if (!empty($roles)) {
+			// Access by editors, authors, etc.
+			$internalRoles = array_diff($roles, array(ROLE_ID_READER));
+			if (!empty($internalRoles)) {
+				$classification = USAGE_EVENT_PLUGIN_CLASSIFICATION_ADMIN;
+			}
+		}
+		if ($request->isBot()) {
+			// The bot classification overwrites other classifications.
+			$classification = USAGE_EVENT_PLUGIN_CLASSIFICATION_BOT;
+		}
+		// TODO: Classify LOCKSS or similar as 'internal' access.
+
+		/*
+		 * Comparison of our event log format with Apache log parameters...
+		*
+		* 1) default parameters:
+		* %h: remote hostname or IP => $ip, $host
+		* %l: remote logname (identd) => not supported, see $user, $roles instead
+		* %u: remote user => not supported, see $user, $roles instead
+		* %t: request time => $time
+		* %r: query => derived objects: $pubObject, $assocType, $canonicalUrl, $identifiers, $serviceUri, $classification
+		* %s: status => not supported (always 200 in our case)
+		* %b: response size => $docSize
+		*
+		* 2) other common parameters
+		* %O: bytes sent => not supported (cannot be reliably determined from within PHP)
+		* %X: connection status => $downloadSuccess (not reliable!)
+		* %{ContentType}o: => $mimeType
+		* %{User-agent}i: => $userAgent
+		* %{Referer}i: => $referrer
+		*
+		* Several items, e.g. time etc., may differ from what Apache
+		* would actually log. But the differences do not matter for our use
+		* cases.
+		*/
+
+		// Collect all information into an array.
+		$usageEvent = compact(
+			'time', 'pubObject', 'assocType', 'canonicalUrl', 'mimeType',
+			'identifiers', 'docSize', 'downloadSuccess', 'serviceUri',
+			'ip', 'host', 'user', 'roles', 'userAgent', 'referrer',
+			'classification'
+		);
+
+		return $usageEvent;
+	}
+
+}
+
+?>
