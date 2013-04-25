bbbd9d698efba95937b888d7f9a0c4f2b7e21370
Alec Smecher <alec@smecher.bc.ca>
2013-04-25 09:26:27 -0700

    *8209* Use article publication date for DOAJ export rather than issue

--- plugins/importexport/doaj/DOAJExportDom.inc.php
+++ plugins/importexport/doaj/DOAJExportDom.inc.php
@@ -67,7 +67,7 @@ class DOAJExportDom {
 		XMLCustomWriter::createChildWithText($doc, $root, 'eissn', $journal->getSetting('onlineIssn'), false);
 
 		/* --- Article's publication date, volume, issue, DOI --- */
-		XMLCustomWriter::createChildWithText($doc, $root, 'publicationDate', DOAJExportDom::formatDate($issue->getDatePublished()), false);
+		XMLCustomWriter::createChildWithText($doc, $root, 'publicationDate', DOAJExportDom::formatDate($article->getDatePublished()), false);
 
 		XMLCustomWriter::createChildWithText($doc, $root, 'volume',  $issue->getVolume(), false);
 
