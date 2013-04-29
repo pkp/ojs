cbc9880463cbda49d8175d550411cbdff27b0d82
Alec Smecher <alec@smecher.bc.ca>
2013-04-29 16:10:26 -0700

    *8212* Index wrapper

---
+++ pages/workflow/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @defgroup pages_workflow
+ */
+
+/**
+ * @file pages/workflow/index.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup pages_workflow
+ * @brief Handle requests for workflow functions.
+ *
+ */
+
+switch ($op) {
+	case 'access':
+	case 'submission':
+	case 'externalReview':
+	case 'editorial':
+	case 'production':
+	case 'editorDecisionActions':
+	case 'submissionProgressBar':
+		define('HANDLER_CLASS', 'WorkflowHandler');
+		import('pages.workflow.WorkflowHandler');
+		break;
+}
+
+?>
