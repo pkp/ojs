666ccf66dcdb2244aba86bfbce902ad464cb9ccc
Jason Nugent <jason.nugent@gmail.com>
2013-05-02 15:00:48 -0300

    *8212* move logEvent up

--- classes/log/ArticleLog.inc.php
+++ classes/log/ArticleLog.inc.php
@@ -1,54 +1,20 @@
 <?php
 
 /**
- * @file classes/log/MonographLog.inc.php
+ * @file classes/log/ArticleLog.inc.php
  *
  * Copyright (c) 2003-2013 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
- * @class MonographLog
+ * @class ArticleLog
  * @ingroup log
  *
- * @brief Static class for adding / accessing monograph log entries.
+ * @brief Static class for adding / accessing submission log entries.
  */
 
 import('lib.pkp.classes.log.PkpLog');
 
 class ArticleLog extends PkpLog {
-	/**
-	 * Add a new event log entry with the specified parameters
-	 * @param $request object
-	 * @param $submission object
-	 * @param $eventType int
-	 * @param $messageKey string
-	 * @param $params array optional
-	 * @return object ArticleLogEntry iff the event was logged
-	 */
-	function logEvent($request, $submission, $eventType, $messageKey, $params = array()) {
-		// Create a new entry object
-		$articleEventLogDao = DAORegistry::getDAO('SubmissionEventLogDAO');
-		$entry = $articleEventLogDao->newDataObject();
-
-		// Set implicit parts of the log entry
-		$entry->setDateLogged(Core::getCurrentDate());
-		$entry->setIPAddress($request->getRemoteAddr());
-
-		$user = $request->getUser();
-		if ($user) $entry->setUserId($user->getId());
-
-		$entry->setAssocType(ASSOC_TYPE_SUBMISSION);
-		$entry->setAssocId($submission->getId());
-
-		// Set explicit parts of the log entry
-		$entry->setEventType($eventType);
-		$entry->setMessage($messageKey);
-		$entry->setParams($params);
-		$entry->setIsTranslated(0); // Legacy for other apps. All messages use locale keys.
-
-		// Insert the resulting object
-		$articleEventLogDao->insertObject($entry);
-		return $entry;
-	}
 }
 
 ?>
