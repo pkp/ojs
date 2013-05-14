7cd6af6770b4edf71368dba1751262588f671499
Alec Smecher <alec@smecher.bc.ca>
2013-05-14 12:00:31 -0700

    *8212* Review components into OJS

---
+++ controllers/grid/users/reviewer/ReviewerGridHandler.inc.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file controllers/grid/users/reviewer/ReviewerGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ReviewerGridHandler
+ * @ingroup controllers_grid_users_reviewer
+ *
+ * @brief Handle reviewer grid requests.
+ */
+
+import('lib.pkp.classes.controllers.grid.users.reviewer.PKPReviewerGridHandler');
+
+class ReviewerGridHandler extends PKPReviewerGridHandler {
+	/**
+	 * Constructor
+	 */
+	function ReviewerGridHandler() {
+		parent::PKPReviewerGridHandler();
+	}
+
+
+	//
+	// Overridden methods from PKPHandler
+	//
+	/**
+	 * @see PKPHandler::authorize()
+	 * @param $request PKPRequest
+	 * @param $args array
+	 * @param $roleAssignments array
+	 */
+	function authorize($request, &$args, $roleAssignments) {
+		$stageId = $request->getUserVar('stageId'); // This is being validated in WorkflowStageAccessPolicy
+
+		// Not all actions need a stageId. Some work off the reviewAssignment which has the type and round.
+		$this->_stageId = (int)$stageId;
+
+		// Get the stage access policy
+		import('classes.security.authorization.WorkflowStageAccessPolicy');
+		$ompWorkflowStageAccessPolicy = new WorkflowStageAccessPolicy($request, $args, $roleAssignments, 'submissionId', $stageId);
+
+		// Add policy to ensure there is a review round id.
+		import('lib.pkp.classes.security.authorization.internal.ReviewRoundRequiredPolicy');
+		$ompWorkflowStageAccessPolicy->addPolicy(new ReviewRoundRequiredPolicy($request, $args, 'reviewRoundId', $this->_getReviewRoundOps()));
+
+		// Add policy to ensure there is a review assignment for certain operations.
+		import('lib.pkp.classes.security.authorization.internal.ReviewAssignmentRequiredPolicy');
+		$ompWorkflowStageAccessPolicy->addPolicy(new ReviewAssignmentRequiredPolicy($request, $args, 'reviewAssignmentId', $this->_getReviewAssignmentOps()));
+		$this->addPolicy($ompWorkflowStageAccessPolicy);
+
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+}
+
+?>
---
+++ controllers/tab/workflow/ReviewRoundTabHandler.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file controllers/tab/workflow/ReviewRoundTabHandler.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ReviewRoundTabHandler
+ * @ingroup controllers_tab_workflow
+ *
+ * @brief Handle AJAX operations for review round tabs on review stages workflow pages.
+ */
+
+import('classes.handler.Handler');
+
+// Import the base class.
+import('lib.pkp.classes.controllers.tab.workflow.PKPReviewRoundTabHandler');
+
+class ReviewRoundTabHandler extends PKPReviewRoundTabHandler {
+
+	/**
+	 * Constructor
+	 */
+	function ReviewRoundTabHandler() {
+		parent::PKPReviewRoundTabHandler();
+		$this->addRoleAssignment(
+			array(ROLE_ID_SUB_EDITOR, ROLE_ID_MANAGER, ROLE_ID_ASSISTANT),
+			array('externalReviewRound')
+		);
+	}
+
+
+	//
+	// Extended methods from Handler
+	//
+	/**
+	 * @see PKPHandler::authorize()
+	 */
+	function authorize($request, &$args, $roleAssignments) {
+		$stageId = (int) $request->getUserVar('stageId'); // This is validated in WorkflowStageAccessPolicy.
+
+		import('classes.security.authorization.WorkflowStageAccessPolicy');
+		$this->addPolicy(new WorkflowStageAccessPolicy($request, $args, $roleAssignments, 'submissionId', $stageId));
+
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+}
+
+?>
