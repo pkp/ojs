d0c8650a52e43a3b0a0823284394a2770c0db106
Alec Smecher <alec@smecher.bc.ca>
2013-05-28 10:02:08 -0700

    *8212* Forgotten commits

---
+++ classes/security/authorization/OjsAuthorDashboardAccessPolicy.inc.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file classes/security/authorization/OjsAuthorDashboardAccessPolicy.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class OjsAuthorDashboardAccessPolicy
+ * @ingroup security_authorization
+ *
+ * @brief Class to control access to OMP author dashboard.
+ */
+
+import('lib.pkp.classes.security.authorization.internal.ContextPolicy');
+import('lib.pkp.classes.security.authorization.PolicySet');
+
+class OjsAuthorDashboardAccessPolicy extends ContextPolicy {
+	/**
+	 * Constructor
+	 * @param $request PKPRequest
+	 * @param $args array request arguments
+	 * @param $roleAssignments array
+	 */
+	function OjsAuthorDashboardAccessPolicy($request, &$args, $roleAssignments) {
+		parent::ContextPolicy($request);
+
+		$authorDashboardPolicy = new PolicySet(COMBINING_DENY_OVERRIDES);
+
+		// AuthorDashboard requires a valid monograph in request.
+		import('classes.security.authorization.SubmissionAccessPolicy');
+		$authorDashboardPolicy->addPolicy(new SubmissionAccessPolicy($request, $args, $roleAssignments), true);
+
+		// Check if the user has an stage assignment with the monograph in request.
+		// Any workflow stage assignment is suficient to access the author dashboard.
+		import('classes.security.authorization.internal.UserAccessibleWorkflowStageRequiredPolicy');
+		$authorDashboardPolicy->addPolicy(new UserAccessibleWorkflowStageRequiredPolicy($request));
+
+		$this->addPolicy($authorDashboardPolicy);
+	}
+}
+
+?>
---
+++ controllers/modals/submissionMetadata/AuthorSubmissionMetadataHandler.inc.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file controllers/modals/submissionMetadata/AuthorSubmissionMetadataHandler.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AuthorSubmissionMetadataHandler
+ * @ingroup controllers_modals_submissionMetadata
+ *
+ * @brief Display submission metadata to authors.
+ */
+
+import('lib.pkp.classes.controllers.modals.submissionMetadata.SubmissionMetadataHandler');
+
+// import JSON class for use with all AJAX requests
+import('lib.pkp.classes.core.JSONMessage');
+
+class AuthorSubmissionMetadataHandler extends SubmissionMetadataHandler {
+	/**
+	 * Constructor.
+	 */
+	function AuthorSubmissionMetadataHandler() {
+		parent::SubmissionMetadataHandler();
+		$this->addRoleAssignment(array(ROLE_ID_AUTHOR), array('fetch', 'saveForm'));
+	}
+
+	//
+	// Implement template methods from PKPHandler.
+	//
+	/**
+	 * @see PKPHandler::authorize()
+	 * @param $request PKPRequest
+	 * @param $args array
+	 * @param $roleAssignments array
+	 */
+	function authorize($request, &$args, $roleAssignments) {
+		import('classes.security.authorization.SubmissionAccessPolicy');
+		$this->addPolicy(new SubmissionAccessPolicy($request, $args, $roleAssignments));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	/**
+	 * Get an instance of the metadata form to be used by this handler.
+	 * @param $submissionId int
+	 * @return Form
+	 */
+	function getFormInstance($submissionId, $stageId = null, $params = null) {
+		import('controllers.modals.submissionMetadata.form.IssueEntrySubmissionReviewForm');
+		return new IssueEntrySubmissionReviewForm($submissionId, $stageId, $params);
+	}
+}
+
+?>
