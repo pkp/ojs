fc3b935a8f67f5bcacffee086e049a97a342de33
Bruno Beghelli <bruno.beghelli@gmail.com>
2013-06-10 12:23:59 -0300

    *8254* Backport usage events plugin

---
+++ plugins/generic/usageEvent/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @defgroup plugins_generic_usageEvent
+ */
+
+/**
+ * @file plugins/generic/usageEvent/index.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_generic_usageEvent
+ * @brief Wrapper for usage event plugin.
+ *
+ */
+require_once('UsageEventPlugin.inc.php');
+
+return new UsageEventPlugin();
+
+?>
