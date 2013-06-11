071472a4d703df82f233cda60a17bfefcb9918a2
Alec Smecher <alec@smecher.bc.ca>
2013-06-11 11:41:54 -0700

    *8250* Fix section sequence casting

--- classes/journal/SectionDAO.inc.php
+++ classes/journal/SectionDAO.inc.php
@@ -207,7 +207,7 @@ class SectionDAO extends DAO {
 			array(
 				(int)$section->getJournalId(),
 				(int)$section->getReviewFormId(),
-				$section->getSequence() == null ? 0 : $section->getSequence(),
+				(float) $section->getSequence(),
 				$section->getMetaIndexed() ? 1 : 0,
 				$section->getMetaReviewed() ? 1 : 0,
 				$section->getAbstractsNotRequired() ? 1 : 0,
@@ -247,7 +247,7 @@ class SectionDAO extends DAO {
 				WHERE section_id = ?',
 			array(
 				(int)$section->getReviewFormId(),
-				$this->nullOrInt($section->getSequence()),
+				(float) $section->getSequence(),
 				(int)$section->getMetaIndexed(),
 				(int)$section->getMetaReviewed(),
 				(int)$section->getAbstractsNotRequired(),
