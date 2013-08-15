10d173ebf51322e93e68c9b0a4645b2a7a0af3ac
Alec Smecher <alec@smecher.bc.ca>
2013-08-15 09:35:51 -0700

    *8396

--- pages/manager/FilesHandler.inc.php
+++ pages/manager/FilesHandler.inc.php
@@ -161,7 +161,7 @@ class FilesHandler extends ManagerHandler {
 	}
 
 	function _fileNameFilter($var) {
-		return (!empty($var) && $var != '..' && $var != '.');
+		return (!empty($var) && $var != '..' && $var != '.' && strpos($var, '/')===false);
 	}
 
 	function _cleanFileName($var) {
