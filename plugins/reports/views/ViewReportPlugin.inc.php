88623fc00eedc51d5aaf3aff0ec58cffd78bc6bd
Bruno Beghelli <bruno.beghelli@gmail.com>
2013-08-17 23:07:14 -0500

    *8254* Adapted view report plugin to use metrics structure

--- plugins/reports/views/ViewReportPlugin.inc.php
+++ plugins/reports/views/ViewReportPlugin.inc.php
@@ -15,6 +15,8 @@
 
 import('classes.plugins.ReportPlugin');
 
+define('OJS_METRIC_TYPE_LEGACY_DEFAULT', 'ojs::legacyDefault');
+
 class ViewReportPlugin extends ReportPlugin {
 	/**
 	 * Called as a plugin is registered to the registry
@@ -48,13 +50,18 @@ class ViewReportPlugin extends ReportPlugin {
 	function display(&$args) {
 		$journal =& Request::getJournal();
 
+		$issueDao =& DAORegistry::getDAO('IssueDAO');
+		$publishedArticleDao =& DAORegistry::getDAO('PublishedArticleDAO');
+		$galleyDao =& DAORegistry::getDAO('ArticleGalleyDAO'); /* @var $galleyDao ArticleGalleyDAO */
+		$metricsDao =& DAORegistry::getDAO('MetricsDAO'); /* @var $metricsDao MetricsDAO */
+
 		$columns = array(
-			__('plugins.reports.views.articleId'),
-			__('plugins.reports.views.articleTitle'),
-			__('issue.issue'),
-			__('plugins.reports.views.datePublished'),
-			__('plugins.reports.views.abstractViews'),
-			__('plugins.reports.views.galleyViews'),
+		__('plugins.reports.views.articleId'),
+		__('plugins.reports.views.articleTitle'),
+		__('issue.issue'),
+		__('plugins.reports.views.datePublished'),
+		__('plugins.reports.views.abstractViews'),
+		__('plugins.reports.views.galleyViews'),
 		);
 		$galleyLabels = array();
 		$galleyViews = array();
@@ -64,50 +71,92 @@ class ViewReportPlugin extends ReportPlugin {
 		$issueDatesPublished = array();
 		$articleTitles = array();
 		$articleIssueIdentificationMap = array();
+		$firstTime = true;
+		$result = array();
 
-		$issueDao =& DAORegistry::getDAO('IssueDAO');
-		$publishedArticleDao =& DAORegistry::getDAO('PublishedArticleDAO');
+		import('classes.db.DBResultRange');
+		$dbResultRange = new DBResultRange(STATISTICS_MAX_ROWS);
+		$page = 3;
 
-		$publishedArticles =& $publishedArticleDao->getPublishedArticlesByJournalId($journal->getJournalId());
-		while ($publishedArticle =& $publishedArticles->next()) {
-			$articleId = $publishedArticle->getId();
-			$issueId = $publishedArticle->getIssueId();
-			$articleTitles[$articleId] = $publishedArticle->getArticleTitle();
+		$request =& Application::getRequest();
+		if ($request->getUserVar('metricType') === OJS_METRIC_TYPE_COUNTER) {
+			$metricType = OJS_METRIC_TYPE_COUNTER;
+		} else {
+			$metricType = OJS_METRIC_TYPE_LEGACY_DEFAULT;
+		}
 
-			// Store the abstract view count
-			$abstractViewCounts[$articleId] = $publishedArticle->getViews();
-			// Make sure we get the issue identification
-			$articleIssueIdentificationMap[$articleId] = $issueId;
-			if (!isset($issueIdentifications[$issueId])) {
-				$issue =& $issueDao->getIssueById($issueId);
-				$issueIdentifications[$issueId] = $issue->getIssueIdentification();
-				$issueDatesPublished[$issueId] = $issue->getDatePublished();
-				unset($issue);
-			}
+		while (true) {
+			$dbResultRange->setPage($page);
+			$result = $metricsDao->getMetrics($metricType,
+				array(STATISTICS_DIMENSION_ASSOC_ID),
+				array(STATISTICS_DIMENSION_ASSOC_TYPE => ASSOC_TYPE_ARTICLE, STATISTICS_DIMENSION_CONTEXT_ID => $journal->getId()),
+				array(),
+				$dbResultRange);
+			$page++;
 
-			// For each galley, store the label and the count
-			$galleys =& $publishedArticle->getGalleys();
-			$galleyViews[$articleId] = array();
-			$galleyViewTotals[$articleId] = 0;
-			foreach ($galleys as $galley) {
-				$label = $galley->getGalleyLabel();
-				$i = array_search($label, $galleyLabels);
-				if ($i === false) {
-					$i = count($galleyLabels);
-					$galleyLabels[] = $label;
+			foreach ($result as $record) {
+				$articleId = $record[STATISTICS_DIMENSION_ASSOC_ID];
+				$publishedArticle =& $publishedArticleDao->getPublishedArticleByArticleId($articleId, null, true);
+				if (!is_a($publishedArticle, 'PublishedArticle')) {
+					continue;
+				}
+				$issueId = $publishedArticle->getIssueId();
+				if (!$issueId) {
+					continue;
+				}
+				$articleTitles[$articleId] = $publishedArticle->getArticleTitle();
+
+				// Store the abstract view count, making
+				// sure both metric types will be counted.
+				if (isset($abstractViewCounts[$articleId])) {
+					$abstractViewCounts[$articleId] += $record[STATISTICS_METRIC];
+				} else {
+					$abstractViewCounts[$articleId] = $record[STATISTICS_METRIC];
+				}
+				// Make sure we get the issue identification
+				$articleIssueIdentificationMap[$articleId] = $issueId;
+				if (!isset($issueIdentifications[$issueId])) {
+					$issue =& $issueDao->getIssueById($issueId);
+					$issueIdentifications[$issueId] = $issue->getIssueIdentification();
+					$issueDatesPublished[$issueId] = $issue->getDatePublished();
+					unset($issue);
 				}
 
-				// Make sure the array is the same size as in previous iterations
-				//  so that we insert values into the right location
-				$galleyViews[$articleId] = array_pad($galleyViews[$articleId], count($galleyLabels), '');
+				// For each galley, store the label and the count
+				$galleysResult =& $metricsDao->getMetrics($metricType,
+					array(STATISTICS_DIMENSION_ASSOC_ID),
+					array(STATISTICS_DIMENSION_ASSOC_TYPE => ASSOC_TYPE_GALLEY, STATISTICS_DIMENSION_SUBMISSION_ID => $articleId));
+				$galleyViews[$articleId] = array();
+				$galleyViewTotals[$articleId] = 0;
+				foreach ($galleysResult as $galleyRecord) {
+					$galleyId = $galleyRecord[STATISTICS_DIMENSION_ASSOC_ID];
+					$galley =& $galleyDao->getGalley($galleyId);
+					$label = $galley->getGalleyLabel();
+					$i = array_search($label, $galleyLabels);
+					if ($i === false) {
+						$i = count($galleyLabels);
+						$galleyLabels[] = $label;
+					}
 
-				$views = $galley->getViews();
-				$galleyViews[$articleId][$i] = $views;
-				$galleyViewTotals[$articleId] += $views;
+					// Make sure the array is the same size as in previous iterations
+					//  so that we insert values into the right location
+					if (count($galleyViews[$articleId]) !== count($galleyLabels)) {
+						$galleyViews[$articleId] = array_pad($galleyViews[$articleId], count($galleyLabels), '');
+					}
+
+					$views = $galleyRecord[STATISTICS_METRIC];
+					// Make sure both metric types will be counted.
+					$galleyViews[$articleId][$i] += $views;
+
+					$galleyViewTotals[$articleId] += $views;
+				}
+
+				// Clean up
+				unset($publishedArticle, $galleys);
 			}
 
-			// Clean up
-			unset($publishedArticle, $galleys);
+			$firstTime = false;
+			if (count($result) < STATISTICS_MAX_ROWS) break;
 		}
 
 		header('content-type: text/comma-separated-values');
