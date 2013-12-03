fe781ea1b61345883e6a75ca39c790cb270d3ff0
Bruno Beghelli <bruno.beghelli@gmail.com>
2013-12-03 15:42:39 -0200

    *8254* Removed php 5 code

--- plugins/generic/usageStats/UsageStatsLoader.inc.php
+++ plugins/generic/usageStats/UsageStatsLoader.inc.php
@@ -129,11 +129,11 @@ class UsageStatsLoader extends FileLoader {
 	/**
 	 * @see FileLoader::processFile()
 	 */
-	function processFile($filePath) {
+	function processFile($filePath, &$errorMsg) {
 		$fhandle = fopen($filePath, 'r');
 		$geoTool = $this->_geoLocationTool;
 		if (!$fhandle) {
-			throw new Exception(__('plugins.generic.usageStats.openFileFailed', array('file' => $filePath)));
+			$errorMsg = __('plugins.generic.usageStats.openFileFailed', array('file' => $filePath));
 		}
 
 		$loadId = basename($filePath);
@@ -153,8 +153,8 @@ class UsageStatsLoader extends FileLoader {
 			if ($line == '') continue;
 			$entryData = $this->_getDataFromLogEntry($line);
 			if (!$this->_isLogEntryValid($entryData, $lineNumber)) {
-				throw new Exception(__('plugins.generic.usageStats.invalidLogEntry',
-					array('file' => $filePath, 'lineNumber' => $lineNumber)));
+				$errorMsg = __('plugins.generic.usageStats.invalidLogEntry',
+					array('file' => $filePath, 'lineNumber' => $lineNumber));
 			}
 
 			// Avoid internal apache requests.
