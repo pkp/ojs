a839da46937acf7bde62fc4ea35baac0e173e743
axfelix <axfelix@gmail.com>
2014-04-01 10:50:10 -0700

    added dataverse plugin to ojs-stable-2_4

---
+++ plugins/generic/dataverse/classes/DataverseStudy.inc.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * @file plugins/generic/dataverse/classes/DataverseStudy.inc.php
+ *
+ * Copyright (c) 2003-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class DataverseStudy
+ * @ingroup plugins_generic_dataverse
+ *
+ * @brief Basic class describing a Dataverse study
+ */
+
+class DataverseStudy extends DataObject {
+  
+  function DataverseStudy() {
+    parent::DataObject();
+  }
+  
+	//
+	// Get/set methods
+	//
+
+	/**
+	 * Get the ID of the study.
+	 * @return int
+	 */
+	function getId() {
+		return $this->getData('studyId');
+	}
+
+	/**
+	 * Set the ID of the study.
+	 * @param $studyId int
+	 */
+	function setId($studyId) {
+		return $this->setData('studyId', $studyId);
+	}
+
+  /**
+	 * Get the study's submission ID 
+	 * @return int
+	 */
+	function getSubmissionId() {
+		return $this->getData('submissionId');
+	}
+
+	/**
+	 * Set the study's submission ID
+	 * @param $submissionId int
+	 */
+	function setSubmissionId($submissionId) {
+		return $this->setData('submissionId', $submissionId);
+	}
+  
+	/**
+	 * Get study edit URI.
+	 * @return string
+	 */
+	function getEditUri() {
+		return $this->getData('editUri');
+	}
+
+	/**
+	 * Set study edit URI
+	 * @param $editUri string
+	 */
+	function setEditUri($editUri) {
+		return $this->setData('editUri', $editUri);
+	}  
+  
+	/**
+	 * Get study's edit media URI.
+	 * @return string
+	 */
+	function getEditMediaUri() {
+		return $this->getData('editMediaUri');
+	}
+
+	/**
+	 * Set study's edit media URI
+	 * @param $editMediaUri string
+	 */
+	function setEditMediaUri($editMediaUri) {
+		return $this->setData('editMediaUri', $editMediaUri);
+	}  
+
+  /**
+	 * Get study's statement URI.
+	 * @return string
+	 */
+	function getStatementUri() {
+		return $this->getData('statementUri');
+	}
+
+	/**
+	 * Set study's statement URI
+	 * @param $statementUri string
+	 */
+	function setStatementUri($statementUri) {
+		return $this->setData('statementUri', $statementUri);
+	} 
+  
+  /**
+   * Get study persistent URI
+   * @return string
+   */
+  function getPersistentUri() {
+    return $this->getData('persistentUri');
+  }
+  
+  /**
+   * Set study persistent Uri
+   * @param string $persistentUri
+   */
+  function setPersistentUri($persistentUri) {
+    $this->setData('persistentUri', $persistentUri);
+  }
+  
+  /**
+   * Get data citation
+   * @param boolean $markup add HTML to link persistent URI
+   */
+  function getDataCitation($markup = true) {
+    $dataCitation = $this->getData('dataCitation');
+    if ($markup) {
+      // Add markup to link persistent URI.
+      $dataCitation = str_replace($this->getPersistentUri(), '<a href="'. $this->getPersistentUri() .'">'. $this->getPersistentUri() .'</a>', $dataCitation);
+    }
+    return $dataCitation;
+  }
+  
+  /**
+   * Set data citation
+   * @param string $dataCitation
+   */
+  function setDataCitation($dataCitation) {
+    $this->setData('dataCitation', $dataCitation);
+  }
+
+}
+
+?>
---
+++ plugins/generic/dataverse/templates/dataverseAuthForm.tpl
@@ -0,0 +1,59 @@
+{**
+* plugins/generic/dataverse/dataverseAuthForm.tpl
+*
+* Copyright (c) 2003-2014 John Willinsky
+* Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+*
+* Connect to Dataverse Network
+*
+*}
+{strip}
+  {assign var="pageTitle" value="plugins.generic.dataverse.displayName"}
+  {include file="common/header.tpl"}
+{/strip}
+
+<ul class="menu">
+  <li class="current"><a href="{plugin_url path="connect"}">{translate key="plugins.generic.dataverse.settings.connect"}</a></li>
+  <li><a href="{plugin_url path="select"}">{translate key="plugins.generic.dataverse.settings.selectDataverse"}</a></li>
+  <li><a href="{plugin_url path="settings"}">{translate key="plugins.generic.dataverse.settings"}</a></li>
+</ul>
+
+<div style="margin: 1em 0;">
+
+  <form method="post" action="{plugin_url path="connect"}" id="dvnForm">
+    {include file="common/formErrors.tpl"}
+
+    <table width="100%" class="data">
+      <tr valign="top">
+        <td class="label">{fieldLabel name="dvnUri" required="true" key="plugins.generic.dataverse.settings.dvnUri"}</td>
+        <td class="value"><input type="text" name="dvnUri" id="dvnUri" value="{$dvnUri|escape}" size="40" maxlength="90" class="textField"/></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>{translate key="plugins.generic.dataverse.settings.dvnUriDescription"}</td>
+      </tr>
+      <tr valign="top">
+        <td class="label">{fieldLabel name="username" required="true" key="user.username"}</td>
+        <td class="value"><input type="text" name="username" id="username" value="{$username|escape}" size="20" maxlength="90" class="textField" /></td>
+      </tr>      
+      <tr>
+        <td>&nbsp;</td>
+        <td>{translate key="plugins.generic.dataverse.settings.usernameDescription"}</td>
+      </tr>
+      <tr valign="top">
+        <td class="label">{fieldLabel name="password" required="true" key="user.password"}</td>
+        <td class="value">
+          <input type="password" name="password" id="password" value="{$password|escape}" size="20" maxlength="90" class="textField"/>
+        </td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>{translate key="plugins.generic.dataverse.settings.passwordDescription"}</td>
+      </tr>
+    </table>
+    <input type="submit" class="button defaultButton" name="save" value="{translate key="common.saveAndContinue"}"  /> 
+    <input type="button" class="button" value="{translate key="common.cancel"}" onclick="document.location='{plugin_url path=""}';"/>
+  </form>
+  <p><span class="formRequired">{translate key="common.requiredField"}</span></p>
+</div>
+{include file="common/footer.tpl"}
