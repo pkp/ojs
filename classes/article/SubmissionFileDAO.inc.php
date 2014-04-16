94278e2771982a135498887960b026cc8c9ae642
Alec Smecher <alec@smecher.bc.ca>
2014-04-16 13:21:54 -0700

    *6902* Remove unnecessary subclass extensions

--- classes/article/SubmissionFileDAO.inc.php
+++ classes/article/SubmissionFileDAO.inc.php
@@ -34,13 +34,6 @@ class SubmissionFileDAO extends PKPSubmissionFileDAO {
 	// Implement protected template methods from PKPSubmissionFileDAO
 	//
 	/**
-	 * @see PKPSubmissionFileDAO::getSubmissionEntityName()
-	 */
-	function getSubmissionEntityName() {
-		return 'submission';
-	}
-
-	/**
 	 * @see PKPSubmissionFileDAO::getDelegateClassNames()
 	 */
 	function getDelegateClassNames() {
@@ -79,76 +72,17 @@ class SubmissionFileDAO extends PKPSubmissionFileDAO {
 
 
 	//
-	// Override methods from PKPSubmissionFileDAO
-	// FIXME *6902* Move this code to PKPSubmissionFileDAO after the review round
-	// refactoring is ported to other applications.
-	//
-	/**
-	 * @see PKPSubmissionFileDAO::deleteAllRevisionsByReviewRound()
-	 */
-	function deleteAllRevisionsByReviewRound($reviewRoundId) {
-		// Remove currently assigned review files.
-		return $this->update('DELETE FROM review_round_files
-						WHERE review_round_id = ?',
-		array((int)$reviewRoundId));
-	}
-
-	/**
-	 * @see PKPSubmissionFileDAO::assignRevisionToReviewRound()
-	 */
-	function assignRevisionToReviewRound($fileId, $revision, $reviewRound) {
-		if (!is_numeric($fileId) || !is_numeric($revision)) fatalError('Invalid file!');
-		return $this->update('INSERT INTO review_round_files
-						('.$this->getSubmissionEntityName().'_id, review_round_id, stage_id, file_id, revision)
-						VALUES (?, ?, ?, ?, ?)',
-		array((int)$reviewRound->getSubmissionId(), (int)$reviewRound->getId(), (int)$reviewRound->getStageId(), (int)$fileId, (int)$revision));
-	}
-
-	/**
-	 * @see PKPSubmissionFileDAO::getRevisionsByReviewRound()
-	 */
-	function getRevisionsByReviewRound($reviewRound, $fileStage = null,
-			$uploaderUserId = null, $uploaderUserGroupId = null) {
-		if (!is_a($reviewRound, 'ReviewRound')) {
-			return null;
-		}
-		return $this->_getInternally($reviewRound->getSubmissionId(),
-			$fileStage, null, null, null, null, null,
-			$uploaderUserId, $uploaderUserGroupId, null, $reviewRound->getId()
-		);
-	}
-
-	/**
-	 * @see PKPSubmissionFileDAO::getLatestNewRevisionsByReviewRound()
-	 */
-	function getLatestNewRevisionsByReviewRound($reviewRound, $fileStage = null) {
-		if (!is_a($reviewRound, 'ReviewRound')) {
-			$emptyArray = array();
-			return $emptyArray;
-		}
-		return $this->_getInternally($reviewRound->getSubmissionId(),
-			$fileStage, null, null, null, null, $reviewRound->getStageId(),
-			null, null, null, $reviewRound->getId(), true
-		);
-	}
-
-	//
 	// Protected helper methods
 	//
 	/**
 	 * @see PKPSubmissionFileDAO::fromRow()
 	 */
 	function fromRow($row) {
-			// Identify the appropriate file implementation for the
-		// given row.
 		if (isset($row['artwork_file_id']) && is_numeric($row['artwork_file_id'])) {
-			$fileImplementation = 'ArtworkFile';
+			return parent::fromRow($row, 'ArtworkFile');
 		} else {
-			$fileImplementation = 'ArticleFile';
+			return parent::fromRow($row, 'ArticleFile');
 		}
-
-		// Let the superclass instantiate the file.
-		return parent::fromRow($row, $fileImplementation);
 	}
 }
 
