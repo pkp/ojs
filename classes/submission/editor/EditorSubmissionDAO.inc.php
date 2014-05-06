a4e9cfd4d06eff27568e4c343a2ddcf934a608ad
Antonio Jesús Sánchez Padial <antonio.sanchez@inia.es>
2014-05-06 11:08:12 +0200

    Copyright update.

--- classes/submission/editor/EditorSubmissionDAO.inc.php
+++ classes/submission/editor/EditorSubmissionDAO.inc.php
@@ -3,6 +3,7 @@
 /**
  * @file classes/editor/EditorSubmissionDAO.inc.php
  *
+ * Copyright (c) 2014 Instituto Nacional de Investigacion y Tecnologia Agraria y Alimentaria
  * Copyright (c) 2013-2014 Simon Fraser University Library
  * Copyright (c) 2003-2014 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
@@ -209,8 +210,22 @@ class EditorSubmissionDAO extends DAO {
 
 		if (!empty($search)) switch ($searchField) {
 			case SUBMISSION_FIELD_ID:
-				$params[] = (int) $search;
-				$searchSql = ' AND a.article_id = ?';
+				switch ($searchMatch) {
+					case 'is':
+						$params[] = (int) $search;
+						$searchSql = ' AND a.article_id = ?';
+						break;
+					case 'contains':
+						$search = '%' . $search . '%';
+						$params[] = $search;
+						$searchSql = ' AND CONCAT(a.article_id) LIKE ?';
+						break;
+					case 'startsWith':
+						$search = $search . '%';
+						$params[] = $search;
+						$searchSql = 'AND CONCAT(a.article_id) LIKE ?';
+						break;
+				}
 				break;
 			case SUBMISSION_FIELD_TITLE:
 				if ($searchMatch === 'is') {
