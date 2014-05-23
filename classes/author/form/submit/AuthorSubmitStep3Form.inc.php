06c577191a4ce5b957386a9bd088972acb5676c8
Jason Nugent <jason.nugent@gmail.com>
2014-05-23 09:49:58 -0300

    *6341* Fixed word count

--- classes/author/form/submit/AuthorSubmitStep3Form.inc.php
+++ classes/author/form/submit/AuthorSubmitStep3Form.inc.php
@@ -40,7 +40,7 @@ class AuthorSubmitStep3Form extends AuthorSubmitForm {
 		$section = $sectionDao->getSection($article->getSectionId());
 		$abstractWordCount = $section->getAbstractWordCount();
 		if (isset($abstractWordCount) && $abstractWordCount > 0) {
-			$this->addCheck(new FormValidatorCustom($this, 'abstract', 'required', 'author.submit.form.wordCountAlert', create_function('$abstract, $wordCount', 'foreach ($abstract as $localizedAbstract) {return count(explode(" ",$localizedAbstract)) < $wordCount; }'), array($abstractWordCount)));
+			$this->addCheck(new FormValidatorCustom($this, 'abstract', 'required', 'author.submit.form.wordCountAlert', create_function('$abstract, $wordCount', 'foreach ($abstract as $localizedAbstract) {return count(explode(" ",strip_tags($localizedAbstract))) < $wordCount; }'), array($abstractWordCount)));
 		}
 
 	}
@@ -219,7 +219,7 @@ class AuthorSubmitStep3Form extends AuthorSubmitForm {
 				$author->setSequence($authors[$i]['seq']);
 
 				HookRegistry::call('Author::Form::Submit::AuthorSubmitStep3Form::Execute', array(&$author, &$authors[$i]));
-				
+
 				if ($isExistingAuthor) {
 					$authorDao->updateAuthor($author);
 				} else {
