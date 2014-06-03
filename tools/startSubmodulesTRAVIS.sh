05e1aeabe1d8976e2bad6f53822658847d4b72bc
Bruno Beghelli <bruno.beghelli@gmail.com>
2014-06-02 22:30:58 -0300

    *8786* Fixed submodules tool

--- tools/startSubmodulesTRAVIS.sh
+++ tools/startSubmodulesTRAVIS.sh
@@ -4,15 +4,14 @@ strLength=${#libModuleHash}
 if [ \( -n "$libModuleHash" \) -a \( "$strLength" -eq 40 \) ]; then
 	gitUser=$(cat .git/config | grep -A2 "remote \"origin\"" | grep "url" | cut -f2 -d":" | cut -f4 -d"/")
 	branch=$(git branch | grep -A1 "*" | grep "^ " | tr -d " ")
-	if [ \( "$gitUser" = 'ojs' \) -o \( "$gitUser" = 'omp' \) -o \( "$gitUser" = 'ocs' \) ]; then
-		git submodule update --init --recursive
-	else  
+	if [ "$gitUser" != 'pkp' ]; then
 		git reset --hard HEAD^
 		git submodule update --init --recursive
 		cd lib/pkp
+		echo "Updating pkp-lib with code from $gitUser repository, $branch branch."
 		git remote add "$gitUser" git://github.com/"$gitUser"/pkp-lib
 		git pull "$gitUser" "$branch"
+		exit 1
 	fi
-else
-	git submodule update --init --recursive
 fi
+git submodule update --init --recursive
