a3f3a49ac0a88e2c1885dc4300c8ad46c9f285fd
Alec Smecher <alec@smecher.bc.ca>
2014-06-03 17:24:30 -0700

    *8787* Introduce framework for data build tests

---
+++ tests/build/user-extensions.js
@@ -0,0 +1,18 @@
+/**
+ * @file tests/build/user-extensions.js
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Used to permit configuration of the Selenium remote control with parameters
+ * from the local environment.
+ */
+
+var dbtype='MySQL';
+var dbhost='localhost';
+var dbname='ojs-ci';
+var dbusername='ojs-ci';
+var dbpassword='ojs-ci';
+var filesdir='/home/asmecher/git/ojs/files';
+var dummyfilepath='/home/asmecher/Data/dummy.pdf';
