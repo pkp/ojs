fb6b6cc533dc66a3c5a21d0b5b937aebdc00d350
Jason Nugent <jason.nugent@gmail.com>
2014-06-16 14:57:13 -0300

    *8710* add index op, refactor for WorkflowStageDAO

--- pages/workflow/WorkflowHandler.inc.php
+++ pages/workflow/WorkflowHandler.inc.php
@@ -28,7 +28,7 @@ class WorkflowHandler extends PKPWorkflowHandler {
 		$this->addRoleAssignment(
 			array(ROLE_ID_SUB_EDITOR, ROLE_ID_MANAGER, ROLE_ID_ASSISTANT),
 			array(
-				'access', 'submission',
+				'access', 'index', 'submission',
 				'editorDecisionActions', // Submission & review
 				'externalReview', // review
 				'editorial',
