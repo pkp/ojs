d1051c658f791221e8602f446fcc0fa994c58471
Alec Smecher <alec@smecher.bc.ca>
2014-07-30 10:26:13 -0700

    *8787* Finish ddiouf test

--- tests/data/60-content/DdioufSubmissionTest.php
+++ tests/data/60-content/DdioufSubmissionTest.php
@@ -29,11 +29,25 @@ class DdioufSubmissionTest extends ContentBaseTestCase {
 			'roles' => array('Author'),
 		));
 
+		$title = 'Genetic transformation of forest trees';
 		$this->createSubmission(array(
-			'title' => 'Genetic transformation of forest trees',
+			'title' => $title,
 			'abstract' => 'In this review, the recent progress on genetic transformation of forest trees were discussed. Its described also, different applications of genetic engineering for improving forest trees or understanding the mechanisms governing genes expression in woody plants.',
 		));
 
+		$this->logOut();
+		$this->findSubmissionAsEditor('dbarnes', null, $title);
+		$this->assignParticipant('Section editor', 'David Buskins');
+		$this->sendToReview();
+		$this->assignReviewer('phudson', 'Paul Hudson');
+		$this->assignReviewer('agallego', 'Adela Gallego');
+		$this->recordEditorialDecision('Accept Submission');
+		$this->waitForText('css=a.editorial.stageId4 > div.stageState', 'Initiated');
+		$this->assignParticipant('Copyeditor', 'Maria Fritz');
+		$this->recordEditorialDecision('Send To Production');
+		$this->waitForText('css=a.production.stageId5 > div.stageState', 'Initiated');
+		$this->assignParticipant('Layout Editor', 'Graham Cox');
+		$this->assignParticipant('Proofreader', 'Catherine Turner');
 		$this->logOut();
 	}
 }
