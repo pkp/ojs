a2397a025c547e906d6e3d800a65e86fbd1f44d4
Alec Smecher <alec@smecher.bc.ca>
2014-08-12 11:03:41 -0700

    *8787* Abstract certain tests for sharing

--- tests/data/10-InstallationTest.php
+++ tests/data/10-InstallationTest.php
@@ -13,44 +13,14 @@
  * @brief Data build suite: Install the system.
  */
 
-import('lib.pkp.tests.WebTestCase');
+import('lib.pkp.tests.data.PKPInstallationTest');
 
-class InstallationTest extends WebTestCase {
+class InstallationTest extends PKPInstallationTest {
 	/**
-	 * Install OJS. Requires configuration items to be specified in
-	 * environment variables -- see getenv(...) calls below.
+	 * Get a piece of text by which to recognize the installation form.
+	 * @return string
 	 */
-	function testInstallation() {
-		$this->open(self::$baseUrl);
-		$this->assertTextPresent('OJS Installation');
-
-		// Administrator
-		$this->waitForElementPresent('css=[id^=adminUsername-]');
-		$this->type('css=[id^=adminUsername-]', 'admin');
-		$this->type('css=[id^=adminPassword-]', 'admin');
-		$this->type('css=[id^=adminPassword2-]', 'admin');
-		$this->type('css=[id^=adminEmail-]', 'pkpadmin@mailinator.com');
-
-		// Database
-		$this->select('id=databaseDriver', 'label=' . getenv('DBTYPE'));
-		$this->type('css=[id^=databaseHost-]', getenv('DBHOST'));
-		$this->type('css=[id^=databasePassword-]', getenv('DBPASSWORD'));
-		$this->type('css=[id^=databaseUsername-]', getenv('DBUSERNAME'));
-		$this->type('css=[id^=databaseName-]', getenv('DBNAME'));
-		$this->click('id=createDatabase');
-
-		// Locale
-		$this->click('id=additionalLocales-en_US');
-		$this->click('id=additionalLocales-fr_CA');
-		$this->select('id=connectionCharset', 'label=Unicode (UTF-8)');
-		$this->select('id=databaseCharset', 'label=Unicode (UTF-8)');
-
-		// Files
-		$this->type('css=[id^=filesDir-]', getenv('FILESDIR'));
-
-		// Execute
-		$this->click('css=[id^=submitFormButton-]');
-		$this->waitForElementPresent('link=Login');
-		$this->waitJQuery();
+	protected function _getInstallerText() {
+		return 'OJS Installation';
 	}
 }
--- tests/data/40-CreateUsersTest.php
+++ tests/data/40-CreateUsersTest.php
@@ -13,55 +13,9 @@
  * @brief Data build suite: Create test users
  */
 
-import('lib.pkp.tests.WebTestCase');
+import('lib.pkp.tests.data.PKPCreateUsersTest');
 
-class CreateUsersTest extends WebTestCase {
-	/**
-	 * Create a user account.
-	 * @param $data array
-	 */
-	protected function createUser($data) {
-		// Come up with sensible defaults for data not supplied
-		$username = $data['username'];
-		$data = array_merge(array(
-			'email' => $username . '@mailinator.com',
-			'password' => $username . $username,
-			'password2' => $username . $username,
-			'roles' => array()
-		), $data);
-
-		$this->open(self::$baseUrl);
-		$this->waitForElementPresent('link=Users & Roles');
-		$this->click('link=Users & Roles');
-		$this->waitForElementPresent('css=[id^=component-grid-settings-user-usergrid-addUser-button-]');
-		$this->click('css=[id^=component-grid-settings-user-usergrid-addUser-button-]');
-		$this->waitForElementPresent('css=[id^=firstName-]');
-
-		// Fill in user data
-		$this->type('css=[id^=firstName-]', $data['firstName']);
-		$this->type('css=[id^=lastName-]', $data['lastName']);
-		$this->type('css=[id^=username-]', $username);
-		$this->type('css=[id^=email-]', $data['email']);
-		$this->type('css=[id^=password-]', $data['password']);
-		$this->type('css=[id^=password2-]', $data['password2']);
-		if (isset($data['country'])) $this->select('id=country', $data['country']);
-		if (isset($data['affiliation'])) $this->type('css=[id^=affiliation-]', $data['affiliation']);
-		$this->click('//span[text()=\'OK\']/..');
-		$this->waitJQuery();
-
-		// Roles
-		foreach ($data['roles'] as $role) {
-			$this->waitForElementPresent('css=[id^=component-listbuilder-users-userusergrouplistbuilder-addItem-button-]');
-			$this->clickAt('css=[id^=component-listbuilder-users-userusergrouplistbuilder-addItem-button-]', '10,10');
-			$this->waitForElementPresent('//select[@name=\'newRowId[name]\']//option[text()=\'' . $role . '\']');
-			$this->select('name=newRowId[name]', $role);
-			$this->waitJQuery();
-		}
-
-		$this->click('//span[text()=\'Save\']/..');
-		$this->waitJQuery();
-	}
-
+class CreateUsersTest extends PKPCreateUsersTest {
 	/**
 	 * Create a user
 	 */
