c13f2d149978b430981bef7d7cfead32e2025376
Alec Smecher <alec@smecher.bc.ca>
2014-09-09 17:19:15 -0700

    *8787* Publish initial issue and schedule intended submissions

--- tests/data/60-content/BvemerSubmissionTest.php
+++ tests/data/60-content/BvemerSubmissionTest.php
@@ -29,10 +29,31 @@ class BvemerSubmissionTest extends ContentBaseTestCase {
 			'roles' => array('Author'),
 		));
 
+		$title = 'A Review of Information Systems and Corporate Memory: design for staff turn-over';
 		$this->createSubmission(array(
 			'section' => 'Reviews',
-			'title' => 'A Review of Information Systems and Corporate Memory: design for staff turn-over',
+			'title' => $title,
 		));
+		$this->logOut();
+
+		$this->findSubmissionAsEditor('dbarnes', null, $title);
+
+		// Go to review; accept submission
+		$this->clickAndWait('link=Review');
+		$this->recordEditorialDecision('Accept Submission');
+
+		// Go to editing
+		$this->clickAndWait('link=Editing');
+
+		// Upload a galley file
+		$this->click('id=layoutFileTypeGalley');
+		$this->attachFile('name=layoutFile', "file://" . getenv('DUMMYFILE'));
+		$this->clickAndWait('//input[@name=\'layoutFile\']/..//input[@value=\'Upload\']');
+		$this->clickAndWait('//input[@value=\'Save\']');
+
+		// Schedule for an issue
+		$this->select('id=issueId', 'label=Vol 1, No 1 (2014)');
+		$this->clickAndWait('//div[@id=\'scheduling\']//input[@value=\'Record\']');
 
 		$this->logOut();
 	}
