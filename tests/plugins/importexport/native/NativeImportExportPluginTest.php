908e128ea5da90d7c1403eb7a9de563c68d9edb6
Alec Smecher <alec@smecher.bc.ca>
2014-09-10 17:03:23 -0700

    *8740* Add permissions import/export to native import/export plugin

--- tests/plugins/importexport/native/NativeImportExportPluginTest.php
+++ tests/plugins/importexport/native/NativeImportExportPluginTest.php
@@ -14,9 +14,6 @@
  * @brief Test class for the NativeImportExportPlugin class
  */
 
-
-require_mock_env('env2'); // Required for mock app locale.
-
 import('lib.pkp.tests.DatabaseTestCase');
 import('lib.pkp.classes.core.PKPRouter');
 import('classes.article.Article');
@@ -108,13 +105,94 @@ class NativeImportExportPluginTest extends DatabaseTestCase {
 			'{$dummyFileName}' => basename($dummyFile),
 		);
 
-		$this->assertEquals($generatedXml, str_replace(
+		$this->assertEquals(
+			str_replace(
+				array_keys($params),
+				array_values($params),
+				trim(file_get_contents(dirname(__FILE__) . '/expectedExport.xml'))
+			),
+			$generatedXml
+		);
+
+		unlink($filename); // Clean up temporary file
+	}
+
+	/**
+	 * @covers NativeImportExportPlugin
+	 * @covers SolrSearchRequest
+	 */
+	public function testImport() {
+		$dummyFile = getenv('DUMMYFILE');
+		$params = array(
+			'{$embedContents}' => base64_encode(file_get_contents($dummyFile)),
+			'{$currentDate}' => date('Y-m-d'),
+			'{$dummyFileName}' => basename($dummyFile),
+		);
+
+		$parser = new XMLParser();
+                $doc =& $parser->parseText(str_replace(
 			array_keys($params),
 			array_values($params),
 			trim(file_get_contents(dirname(__FILE__) . '/expectedExport.xml'))
 		));
-		
-		unlink($filename); // Clean up temporary file
+
+		// Get a journal
+		$journalDao =& DAORegistry::getDAO('JournalDAO');
+		$journal =& $journalDao->getJournalByPath('publicknowledge');
+		$this->assertTrue(is_a($journal, 'Journal'));
+
+		// Alter the journal configuration in order to prevent automatically-assigned
+		// permissions from looking the same as the import ones
+		$journal->updateSetting('copyrightHolderType', 'other', 'string');
+		$journal->updateSetting('copyrightHolderOther', array('en_US' => 'Tucker Fitzgerald'), 'string', true);
+		$journal->updateSetting('copyrightYearBasis', 'issue', 'string'); // Later we'll set the issue pub date
+		$journal->updateSetting('licenseUrl', 'http://www.apache.org/licenses/LICENSE-2.0', 'string');
+
+		// Get an article
+		$publishedArticleDao =& DAORegistry::getDAO('PublishedArticleDAO');
+		$articles =& $publishedArticleDao->getBySetting('title', 'A Review of Information Systems and Corporate Memory: design for staff turn-over', $journal->getId());
+		$this->assertTrue(count($articles)==1);
+		$article = array_shift($articles);
+		$this->assertTrue(is_a($article, 'Article'));
+
+		// Get issue
+		$issueDao =& DAORegistry::getDAO('IssueDAO');
+		$issue =& $issueDao->getIssueById($article->getIssueId());
+		$this->assertTrue(is_a($issue, 'Issue'));
+		// Set the issue publication date somewhere strange to properly test
+		// copyright date on import.
+		$issue->setDatePublished('1980-10-05');
+		$issueDao->updateIssue($issue);
+
+		// Get section
+		$sectionDao =& DAORegistry::getDAO('SectionDAO');
+		$section =& $sectionDao->getSection($article->getSectionId());
+		$this->assertTrue(is_a($section, 'Section'));
+
+		// Get user
+		$userDao =& DAORegistry::getDAO('UserDAO');
+		$user =& $userDao->getByUsername('admin');
+
+		// Run the import
+		$this->plugin->import('NativeImportDom');
+		$result = NativeImportDom::importArticle($journal, $doc, $issue, $section, $newArticle, $errors, $user, true);
+		$this->assertEquals(1, $result);
+
+		// Check the resulting import
+		$this->assertEquals('en_US', $newArticle->getLocale());
+		$this->assertEquals($section->getId(), $newArticle->getSectionId());
+		$this->assertEquals($journal->getId(), $newArticle->getJournalId());
+		$this->assertEquals($user->getId(), $newArticle->getUserId());
+		$this->assertEquals(STATUS_PUBLISHED, $newArticle->getStatus());
+		$this->assertEquals(array('en_US' => 'A Review of Information Systems and Corporate Memory: design for staff turn-over'), $newArticle->getTitle(null));
+		$this->assertEquals(array('en_US' => 'information technology;knowledge preservation'), $newArticle->getSubject(null));
+		$this->assertEquals('en', $newArticle->getLanguage());
+		$this->assertFalse($article->getId() == $newArticle->getId());
+
+		// Check permissions
+		$this->assertEquals('2014', $newArticle->getCopyrightYear());
+		$this->assertEquals(array('en_US' => 'Brian Vemer'), $newArticle->getCopyrightHolder(null));
+		$this->assertEquals('http://creativecommons.org/licenses/by-nc-nd/4.0', $newArticle->getLicenseURL());
 	}
 }
 
