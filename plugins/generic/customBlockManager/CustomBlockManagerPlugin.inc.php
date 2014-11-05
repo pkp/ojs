344e0d032d7767a58961f607c921df6de90a7e70
Michael Thessel <michael.thessel@gmx.de>
2014-11-04 15:26:58 -1000

    *8999* Fixed settings table update bug

--- plugins/generic/customBlockManager/CustomBlockManagerPlugin.inc.php
+++ plugins/generic/customBlockManager/CustomBlockManagerPlugin.inc.php
@@ -26,7 +26,7 @@ class CustomBlockManagerPlugin extends GenericPlugin {
 	function register($category, $path) {
 		if (parent::register($category, $path)) {
 			if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
-			if ( $this->getEnabled() ) {
+			if ($this->getEnabled()) {
 				HookRegistry::register('PluginRegistry::loadCategory', array(&$this, 'callbackLoadCategory'));
 			}
 			return true;
@@ -44,35 +44,23 @@ class CustomBlockManagerPlugin extends GenericPlugin {
 	function callbackLoadCategory($hookName, $args) {
 		$category =& $args[0];
 		$plugins =& $args[1];
-		switch ($category) {
-			case 'blocks':
-				$this->import('CustomBlockPlugin');
 
-				$journal =& Request::getJournal();
-				if (!$journal) return false;
+		if ($category != 'blocks') return false;
 
-				$blocks = $this->getSetting($journal->getId(), 'blocks');
-				if (!is_array($blocks)) break;
-				$i=0;
-				foreach ($blocks as $block) {
-					$blockPlugin = new CustomBlockPlugin($block, $this->getName());
+		$this->import('CustomBlockPlugin');
 
-					// default the block to being enabled
-					if ($blockPlugin->getEnabled() !== false) {
-						$blockPlugin->setEnabled(true);
-					}
-					// default the block to the right sidebar
-					if (!is_numeric($blockPlugin->getBlockContext())) {
-						$blockPlugin->setBlockContext(BLOCK_CONTEXT_RIGHT_SIDEBAR);
-					}
-					$plugins[$blockPlugin->getSeq()][$blockPlugin->getPluginPath() . $i] =& $blockPlugin;
+		$journal =& Request::getJournal();
+		if (!$journal) return false;
 
-					$i++;
-					unset($blockPlugin);
-				}
-				break;
+		$blocks = $this->getSetting($journal->getId(), 'blocks');
+		if (!is_array($blocks)) return false;
+
+		$i = 0;
+		foreach ($blocks as $block) {
+			$blockPlugin = new CustomBlockPlugin($block, $this->getName());
+			$plugins[$blockPlugin->getSeq()][$blockPlugin->getPluginPath() . $i++] =& $blockPlugin;
+			unset($blockPlugin);
 		}
-		return false;
 	}
 
 	/**
@@ -91,76 +79,95 @@ class CustomBlockManagerPlugin extends GenericPlugin {
 	 */
 	function manage($verb, $args, &$message) {
 		if (!parent::manage($verb, $args, $message)) return false;
-		switch ($verb) {
-			case 'settings':
-				$journal =& Request::getJournal();
 
-				$templateMgr =& TemplateManager::getManager();
-				$templateMgr->register_function('plugin_url', array(&$this, 'smartyPluginUrl'));
-
-				$pageCrumbs = array(
-					array(
-						Request::url(null, 'user'),
-						'navigation.user'
-					),
-					array(
-						Request::url(null, 'manager'),
-						'user.role.manager'
-					),
-					array(
-						Request::url(null, 'manager', 'plugins'),
-						__('manager.plugins'),
-						true
-					)
-				);
-				$templateMgr->assign('pageHierarchy', $pageCrumbs);
-
-				$this->import('SettingsForm');
-				$form = new SettingsForm($this, $journal->getId());
-				$form->readInputData();
-
-				if (Request::getUserVar('addBlock')) {
-					// Add a block
-					$editData = true;
-					$blocks = $form->getData('blocks');
-					array_push($blocks, '');
-					$form->_data['blocks'] = $blocks;
-
-				} else if (($delBlock = Request::getUserVar('delBlock')) && count($delBlock) == 1) {
-					// Delete an block
-					$editData = true;
-					list($delBlock) = array_keys($delBlock);
-					$delBlock = (int) $delBlock;
-					$blocks = $form->getData('blocks');
-					if (isset($blocks[$delBlock]) && !empty($blocks[$delBlock])) {
-						$deletedBlocks = explode(':', $form->getData('deletedBlocks'));
-						array_push($deletedBlocks, $blocks[$delBlock]);
-						$form->setData('deletedBlocks', join(':', $deletedBlocks));
-					}
-					array_splice($blocks, $delBlock, 1);
-					$form->_data['blocks'] = $blocks;
-
-				} else if ( Request::getUserVar('save') ) {
-					$editData = true;
-					$form->execute();
-				} else {
-					$form->initData();
-				}
-
-				if ( !isset($editData) && $form->validate()) {
-					$form->execute();
-					$form->display();
-					exit;
-				} else {
-					$form->display();
-					exit;
-				}
-				return true;
-			default:
-				// Unknown management verb
-				assert(false);
-				return false;
+		if ($verb != 'settings') {
+			assert(false);
+			return false;
 		}
+
+		$this->import('CustomBlockPlugin');
+
+		$journal =& Request::getJournal();
+
+		$templateMgr =& TemplateManager::getManager();
+		$templateMgr->register_function('plugin_url', array(&$this, 'smartyPluginUrl'));
+
+		$pageCrumbs = array(
+			array(
+				Request::url(null, 'user'),
+				'navigation.user'
+			),
+			array(
+				Request::url(null, 'manager'),
+				'user.role.manager'
+			),
+			array(
+				Request::url(null, 'manager', 'plugins'),
+				__('manager.plugins'),
+				true
+			)
+		);
+		$templateMgr->assign('pageHierarchy', $pageCrumbs);
+
+		$this->import('SettingsForm');
+		$form = new SettingsForm($this, $journal->getId());
+		$form->readInputData();
+
+		if (Request::getUserVar('addBlock')) {
+			// Add a block
+			$editData = true;
+			$blocks = $form->getData('blocks');
+			array_push($blocks, '');
+			$form->_data['blocks'] = $blocks;
+
+		} else if (($delBlock = Request::getUserVar('delBlock')) && count($delBlock) == 1) {
+			// Delete an block
+			$editData = true;
+			list($delBlock) = array_keys($delBlock);
+			$delBlock = (int) $delBlock;
+			$blocks = $form->getData('blocks');
+			if (isset($blocks[$delBlock]) && !empty($blocks[$delBlock])) {
+				$deletedBlocks = explode(':', $form->getData('deletedBlocks'));
+				array_push($deletedBlocks, $blocks[$delBlock]);
+				$form->setData('deletedBlocks', join(':', $deletedBlocks));
+			}
+			array_splice($blocks, $delBlock, 1);
+			$form->_data['blocks'] = $blocks;
+
+		} else if (Request::getUserVar('save')) {
+			$editData = true;
+			$form->execute();
+
+			// Enable the block plugin and place it in the right sidebar
+			if ($form->validate()) {
+				foreach ($form->getData('blocks') as $block) {
+					$blockPlugin = new CustomBlockPlugin($block, $this->getName());
+
+					// Default the block to being enabled
+					if (!is_bool($blockPlugin->getEnabled())) {
+						$blockPlugin->setEnabled(true);
+					}
+
+					// Default the block to the right sidebar
+					if (!is_numeric($blockPlugin->getBlockContext())) {
+						$blockPlugin->setBlockContext(BLOCK_CONTEXT_RIGHT_SIDEBAR);
+					}
+				}
+			}
+
+		} else {
+			$form->initData();
+		}
+
+		if (!isset($editData) && $form->validate()) {
+			$form->execute();
+			$form->display();
+			exit;
+		} else {
+			$form->display();
+			exit;
+		}
+		return true;
 	}
 }
 
