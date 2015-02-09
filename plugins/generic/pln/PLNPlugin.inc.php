444f3926625ecba5e5a7d0ddafc4ea1a280b8737
Clinton Graham <ctgraham@pitt.edu>
2015-02-09 12:57:24 -0500

    Move UUID generation function from PLN plugin to PKP Library (String).  ##ulsdevteam/add-uuid-string-creation##

--- plugins/generic/pln/PLNPlugin.inc.php
+++ plugins/generic/pln/PLNPlugin.inc.php
@@ -658,16 +658,7 @@ class PLNPlugin extends GenericPlugin {
 	 * @return string
 	 */
 	function newUUID() {
-		
-		mt_srand((double)microtime()*10000);
-		$charid = strtoupper(md5(uniqid(rand(), true)));
-		$hyphen = '-';
-		$uuid = substr($charid, 0, 8).$hyphen
-				.substr($charid, 8, 4).$hyphen
-				.substr($charid,12, 4).$hyphen
-				.substr($charid,16, 4).$hyphen
-				.substr($charid,20,12);
-		return $uuid;
+		return String::generateUUID();
 	}
 	
 
