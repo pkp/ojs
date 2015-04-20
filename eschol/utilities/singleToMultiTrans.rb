3c660b11d889b7589ad6f4a92181c7f764c4b20c
eschol role account <eschol@pub-submit-dev.escholarship.org>
2015-04-20 15:02:28 -0700

    Fixed block sizes on database restore.

--- eschol/utilities/singleToMultiTrans.rb
+++ eschol/utilities/singleToMultiTrans.rb
@@ -3,8 +3,31 @@
 # Translate an SQL dump with multiple lines for each table to a single line per table,
 # for vastly greater restore speed.
 
-open(ARGV[1], "r", "UTF-8") do |io|
+prevCmd = nil
+count = 0
+File.open(ARGV[0], "r:UTF-8") do |io|
   io.each { |line|
-    puts line
+    if line =~ /^(INSERT INTO [^ ]+ VALUES )(.+);$/
+      cmd, val = $1, $2
+      if prevCmd
+        if cmd == prevCmd && count < 500
+          count += 1
+          STDOUT.write(",\n" + val)
+        else
+          STDOUT.write(";\n" + cmd + val)
+          count = 0
+        end
+      else
+        STDOUT.write(cmd + val)
+      end
+      prevCmd = cmd
+    else
+      if prevCmd
+        STDOUT.write(";\n")
+        prevCmd = nil
+        count = 0
+      end
+      puts line
+    end
   }
 end
