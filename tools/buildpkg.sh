b8c26d5dc866f50fbf080090e8bc9fad35620935
Alec Smecher <alec@smecher.bc.ca>
2015-09-11 10:44:36 -0700

    pkp/pkp-lib#737 Remove patch-based upgrade toolset/docs

--- tools/buildpkg.sh
+++ tools/buildpkg.sh
@@ -22,7 +22,6 @@ fi
 
 VERSION=$1
 TAG=$2
-PATCHDIR=${3-}
 PREFIX=ojs
 BUILD=$PREFIX-$VERSION
 TMPDIR=`mktemp -d $PREFIX.XXXXXX` || exit 1
@@ -117,21 +116,6 @@ cd ..
 echo -n "Creating archive $BUILD.tar.gz ... "
 tar -zhcf ../$BUILD.tar.gz $BUILD
 echo "Done"
-
-if [ ! -z "$PATCHDIR" ]; then
-	echo "Creating patches in $BUILD.patch ..."
-	[ -e "../${BUILD}.patch" ] || mkdir "../$BUILD.patch"
-	for FILE in $PATCHDIR/*; do
-		OLDBUILD=$(basename $FILE)
-		OLDVERSION=${OLDBUILD/$PREFIX-/}
-		OLDVERSION=${OLDVERSION/.tar.gz/}
-		echo -n "Creating patch against ${OLDVERSION} ... "
-		tar -zxf $FILE
-		diff -urN $PREFIX-$OLDVERSION $BUILD | gzip -c > ../${BUILD}.patch/$PREFIX-${OLDVERSION}_to_${VERSION}.patch.gz
-		echo "Done"
-	done
-	echo "Done"
-fi
 
 cd ..
 
