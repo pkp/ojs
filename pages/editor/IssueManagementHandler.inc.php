345f1f0e4e7477ee6e9e3e7f56f6eb38214aca35
Lepidus Tecnologia <lepidus@lepidus.com.br>
2015-11-19 16:18:47 -0200

    pkp/pkp-lib#919 Fix notify "All published authors"

--- pages/editor/IssueManagementHandler.inc.php
+++ pages/editor/IssueManagementHandler.inc.php
@@ -1082,7 +1082,7 @@ class IssueManagementHandler extends EditorHandler {
 			$emails = array();
 			while ($recipients && !$recipients->eof()) {
 				$recipient =& $recipients->next();
-				if ($recipient->getDisabled()) continue;
+				if ($recipient instanceof User && $recipient->getDisabled()) continue;
 				if (!isset($emails[$recipient->getEmail()])) {
 					if (preg_match(ValidatorEmail::getRegexp(), $recipient->getEmail())) {
 						$email->addRecipient($recipient->getEmail(), $recipient->getFullName());
@@ -1097,6 +1097,7 @@ class IssueManagementHandler extends EditorHandler {
 			if($request->getUserVar('sendToMailList')) {
 				$mailList = $notificationMailListDao->getMailList($journal->getId());
 				foreach ($mailList as $mailListRecipient) {
+					if ($recipient instanceof User && $recipient->getDisabled()) continue;
 					$email->addRecipient($mailListRecipient);
 				}
 			}
