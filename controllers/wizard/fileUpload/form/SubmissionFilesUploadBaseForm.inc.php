a5c690f59f1fb55cd424ca8a2a619c794690ef8f
Alec Smecher <alec@smecher.bc.ca>
2015-12-15 13:37:23 -0800

    Fix reference use

--- controllers/wizard/fileUpload/form/SubmissionFilesUploadBaseForm.inc.php
+++ controllers/wizard/fileUpload/form/SubmissionFilesUploadBaseForm.inc.php
@@ -42,8 +42,7 @@ class SubmissionFilesUploadBaseForm extends PKPSubmissionFilesUploadBaseForm {
 	 * For now, this simply uses the parent method.  Having this allows for different sets of submission files
 	 * depending on stageId or fileStage.  See e.g. this class in the OMP codebase as an example.
 	 */
-
-	function &getSubmissionFiles() {
+	function getSubmissionFiles() {
 		if (is_null($this->_submissionFiles)) {
 			$this->_submissionFiles =& parent::getSubmissionFiles();
 		}
