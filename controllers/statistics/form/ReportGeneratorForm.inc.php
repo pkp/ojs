1f4b3ec3feb672337d7edc4f26de973f2319c2ee
Bruno Beghelli <bruno.beghelli@gmail.com>
2016-01-08 13:35:22 -0200

    pkp/pkp-lib#1005: introduce report generator form app class

---
+++ controllers/statistics/form/ReportGeneratorForm.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file controllers/statistics/form/ReportGeneratorForm.inc.php
+ *
+ * Copyright (c) 2013-2015 Simon Fraser University Library
+ * Copyright (c) 2003-2015 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ReportGeneratorForm
+ * @ingroup controllers_statistics_form
+ * @see Form
+ *
+ * @brief Form to generate custom statistics reports.
+ */
+
+import('lib.pkp.controllers.statistics.form.PKPReportGeneratorForm');
+
+class ReportGeneratorForm extends PKPReportGeneratorForm {
+
+	/**
+	 * Constructor
+	 */
+	function ReportGeneratorForm($columns, $objects, $fileTypes, $metricType, $defaultReportTemplates, $reportTemplateIndex = null) {
+		parent::PKPReportGeneratorForm($columns, $objects, $fileTypes, $metricType, $defaultReportTemplates, $reportTemplateIndex);
+	}
+
+	/**
+	 * @see PKPReportGeneratorForm::getFileAssocTypes()
+	 */
+	function getFileAssocTypes() {
+		return array(ASSOC_TYPE_SUBMISSION_FILE, ASSOC_TYPE_ISSUE_GALLEY);
+	}
+}
+
+?>
