79a5f0111982cdf9dbb8050026171ad40c02da41
Stefan Weil <sw@weilnetz.de>
2016-02-13 16:32:37 +0100

    Fix number of parameters when calling function getBySubmissionId
    It only takes one parameter. This issue was reported by PhpStorm.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- tests/functional/plugins/importexport/FunctionalDoiExportTestCase.inc.php
+++ tests/functional/plugins/importexport/FunctionalDoiExportTestCase.inc.php
@@ -432,7 +432,7 @@ class FunctionalDoiExportTest extends FunctionalImportExportBaseTestCase {
 		$article->setData('doiSuffix', '');
 		$articleDao->updateObject($article); // Do not use PublishedArticleDAO::updatePublishedArticle() for this, otherwise the ADODB cache flush there may cause a permission error.
 		$galleyDao = DAORegistry::getDAO('ArticleGalleyDAO'); /* @var $galleyDao ArticleGalleyDAO */
-		$galleys = $galleyDao->getBySubmissionId(1, 1);
+		$galleys = $galleyDao->getBySubmissionId(1);
 		while ($galley = $galleys->next()) {
 			$galley->setData('doiSuffix', '');
 			$galleyDao->updateObject($galley);
