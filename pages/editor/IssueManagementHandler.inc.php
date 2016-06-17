5cbd8b9ce4c576d0a8076fc81d1530567c55e09a
Lisa Schiff <lisa.schiff@ucop.edu>
2016-06-17 15:57:23 -0700

    fixed hash name reference

--- pages/editor/IssueManagementHandler.inc.php
+++ pages/editor/IssueManagementHandler.inc.php
@@ -812,7 +812,7 @@ class IssueManagementHandler extends EditorHandler {
 					 $journalPath = $journal->getPath();
 					 //needs to be updated with each journal using DOIs
 					 $owner = array("uciem_westjem"=>'cdllib', "cjpp"=>'ucblibrary', "irows_cliodynamics"=>'ucr-jclio', "bling_proceedings"=>' ucb-bls');
-					 error_log("Campus Owner: $campusOwner[$journalPath]");
+					 error_log("Campus Based Owner: $owner[$journalPath]");
 					 $ezidIdentifier = 'https://ezid.cdlib.org/id/doi:' . $articleDOI;
 					 error_log("EZID IDENTIFIER $ezidIdentifier");
                      //now pass this to EZID:
