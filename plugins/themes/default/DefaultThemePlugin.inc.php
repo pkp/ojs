1088849a6e4dda67f8891842cdb2c4f7c59695f8
Nate Wright <natew@notthisway.com>
2016-07-25 11:06:40 +0100

    pkp/pkp-lib#1526 Fix reviewer interest autocomplete on frontend

--- plugins/themes/default/DefaultThemePlugin.inc.php
+++ plugins/themes/default/DefaultThemePlugin.inc.php
@@ -54,18 +54,20 @@ class DefaultThemePlugin extends ThemePlugin {
 
 		// Load jQuery from a CDN or, if CDNs are disabled, from a local copy.
 		$min = Config::getVar('general', 'enable_minified') ? '.min' : '';
+		$request = Application::getRequest();
 		if (Config::getVar('general', 'enable_cdn')) {
 			$jquery = '//ajax.googleapis.com/ajax/libs/jquery/' . CDN_JQUERY_VERSION . '/jquery' . $min . '.js';
 			$jqueryUI = '//ajax.googleapis.com/ajax/libs/jqueryui/' . CDN_JQUERY_UI_VERSION . '/jquery-ui' . $min . '.js';
 		} else {
 			// Use OJS's built-in jQuery files
-			$request = Application::getRequest();
 			$jquery = $request->getBaseUrl() . '/lib/pkp/lib/vendor/components/jquery/jquery' . $min . '.js';
 			$jqueryUI = $request->getBaseUrl() . '/lib/pkp/lib/vendor/components/jqueryui/jquery-ui' . $min . '.js';
 		}
 		// Use an empty `baseUrl` argument to prevent the theme from looking for
 		// the files within the theme directory
 		$this->addScript('jQuery', $jquery, array('baseUrl' => ''));
+		$this->addScript('jQueryUI', $jqueryUI, array('baseUrl' => ''));
+		$this->addScript('jQueryTagIt', $request->getBaseUrl() . '/lib/pkp/js/lib/jquery/plugins/jquery.tag-it.js', array('baseUrl' => ''));
 
 		// Load custom JavaScript for this theme
 		$this->addScript('default', 'js/main.js');
