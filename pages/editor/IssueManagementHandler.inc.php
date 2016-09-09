f1e6f0e7a5f7c96d21979b46b283490800a64db9
Lisa Schiff <lschiff@ucop.edu>
2016-09-09 14:05:49 -0700

    Added DOIs for two more journals

--- pages/editor/IssueManagementHandler.inc.php
+++ pages/editor/IssueManagementHandler.inc.php
@@ -811,7 +811,7 @@ class IssueManagementHandler extends EditorHandler {
 					 $articleDOI = str_replace("/","%2F", $articleDOI);
 					 $journalPath = $journal->getPath();
 					 //needs to be updated with each journal using DOIs
-					 $owner = array("ao4elt4"=>'ucolick',"uciem_westjem"=>'cdllib', "cjpp"=>'ucb-eschol', "irows_cliodynamics"=>'ucr-jclio', "bling_proceedings"=>' ucb-bls');
+					 $owner = array("ao4elt4"=>'ucolick',"cjpp"=>'ucb-eschol', "irows_cliodynamics"=>'ucr-jclio', "bling_proceedings"=>'ucb-bls', "biogeographia"=>'ucm_mndawson', "fb"=>'ucm_mndawson');
 					 error_log("Campus Based Owner: $owner[$journalPath]");
 					 $ezidIdentifier = 'https://ezid.cdlib.org/id/doi:' . $articleDOI;
 					 error_log("EZID IDENTIFIER $ezidIdentifier");
