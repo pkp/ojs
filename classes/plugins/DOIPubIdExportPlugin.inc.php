e23b49807bbfabaeacb602d60af40abfad5d122b
Bozana Bokan <bozana.bokan@posteo.net>
2016-09-22 11:41:49 +0200

    pkp/pkp-lib#1601 typo fix

--- classes/plugins/DOIPubIdExportPlugin.inc.php
+++ classes/plugins/DOIPubIdExportPlugin.inc.php
@@ -334,7 +334,7 @@ abstract class DOIPubIdExportPlugin extends PubObjectsExportPlugin {
 	}
 
 	/**
-	 * Get article galleys with a DOI asigned  from gallley IDs.
+	 * Get article galleys with a DOI assigned from gallley IDs.
 	 * @param $galleyIds array
 	 * @param $context Context
 	 * @return array
