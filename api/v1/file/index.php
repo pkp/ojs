4862e02c7afeb2ebec5e22358659ceedfe2e8003
Alec Smecher <alec@smecher.bc.ca>
2016-10-03 06:30:05 -0700

    pkp/pkp-lib#1503 Code headers added

--- api/v1/file/index.php
+++ api/v1/file/index.php
@@ -1,5 +1,20 @@
 <?php 
 
-import('api.v1.file.FileHandler');
+/**
+ * @defgroup api_v1_file File API requests
+ */
 
-return  FileHandler::init();
+/**
+ * @file api/v1/file/index.php
+ *
+ * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2003-2016 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup api_v1_file
+ * @brief Handle requests for file API functions.
+ *
+ */
+
+import('api.v1.file.FileHandler');
+return FileHandler::init();
