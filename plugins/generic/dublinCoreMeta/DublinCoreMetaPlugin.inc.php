e283a665536c84e4c3601bc69c97e948b46e7aaa
Alec Smecher <alec@smecher.bc.ca>
2016-10-17 14:52:14 -0700

    pkp/pkp-lib#1815 Fix empty array bugs for pub ID plugin loops

--- plugins/generic/dublinCoreMeta/DublinCoreMetaPlugin.inc.php
+++ plugins/generic/dublinCoreMeta/DublinCoreMetaPlugin.inc.php
@@ -96,7 +96,7 @@ class DublinCoreMetaPlugin extends GenericPlugin {
 			$templateMgr->addHeader('dublinCorePages', '<meta name="DC.Identifier.pageNumber" content="' . htmlspecialchars($pages) . '"/>');
 		}
 
-		foreach($templateMgr->get_template_vars('pubIdPlugins') as $pubIdPlugin) {
+		foreach((array) $templateMgr->get_template_vars('pubIdPlugins') as $pubIdPlugin) {
 			if ($pubId = $article->getStoredPubId($pubIdPlugin->getPubIdType())) {
 				$templateMgr->addHeader('dublinCorePubId' . $pubIdPlugin->getPubIdDisplayType(), '<meta name="DC.Identifier.' . htmlspecialchars($pubIdPlugin->getPubIdDisplayType()) . '" content="' . htmlspecialchars($pubId) . '"/>');
 			}
