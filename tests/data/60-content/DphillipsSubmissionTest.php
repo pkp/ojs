2226d1a306dba96299a3b1bd2966a4329e61328e
Alec Smecher <alec@smecher.bc.ca>
2016-10-31 18:15:47 -0700

    Fix "smart" apostrophe

--- tests/data/60-content/DphillipsSubmissionTest.php
+++ tests/data/60-content/DphillipsSubmissionTest.php
@@ -28,7 +28,7 @@ class DphillipsSubmissionTest extends ContentBaseTestCase {
 			'country' => 'Canada',
 		));
 
-		$title = 'Investigating the Shared Background Required for Argument: A Critique of Fogelin’s Thesis on Deep Disagreement';
+		$title = 'Investigating the Shared Background Required for Argument: A Critique of Fogelin\'s Thesis on Deep Disagreement';
 		$this->createSubmission(array(
 			'title' => $title,
 			'abstract' => 'Robert Fogelin claims that interlocutors must share a framework of background beliefs and commitments in order to fruitfully pursue argument. I refute Fogelin’s claim by investigating more thoroughly the shared background required for productive argument. I find that this background consists not in any common beliefs regarding the topic at hand, but rather in certain shared pro-cedural commitments and competencies. I suggest that Fogelin and his supporters mistakenly view shared beliefs as part of the required background for productive argument because these procedural com-mitments become more difficult to uphold when people’s beliefs diverge widely regarding the topic at hand.',
