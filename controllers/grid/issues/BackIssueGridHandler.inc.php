caf804b986212b3ee4441ff5cd0d1da7f1fca3c1
PKP <pkp@mailinator.com>
2016-11-02 16:10:54 -0700

    pkp/pkp-lib#1951 Add support for custom issue ordering

--- controllers/grid/issues/BackIssueGridHandler.inc.php
+++ controllers/grid/issues/BackIssueGridHandler.inc.php
@@ -21,6 +21,10 @@ class BackIssueGridHandler extends IssueGridHandler {
 	 */
 	function BackIssueGridHandler() {
 		parent::IssueGridHandler();
+		$this->addRoleAssignment(
+			array(ROLE_ID_MANAGER),
+			array('saveSequence')
+		);
 	}
 
 
@@ -52,6 +56,34 @@ class BackIssueGridHandler extends IssueGridHandler {
 				$issueGridCellProvider
 			)
 		);
+	}
+
+	/**
+	 * @copydoc GridHandler::setDataElementSequence()
+	 */
+	function setDataElementSequence($request, $rowId, $gridDataElement, $newSequence) {
+		$issueDao = DAORegistry::getDAO('IssueDAO');
+		$issueDao->moveCustomIssueOrder($gridDataElement->getJournalId(), $gridDataElement->getId(), $newSequence);
+	}
+
+	/**
+	 * @copydoc GridHandler::getDataElementSequence()
+	 */
+	function getDataElementSequence($gridDataElement) {
+		$issueDao = DAORegistry::getDAO('IssueDAO');
+		$customOrder = $issueDao->getCustomIssueOrder($gridDataElement->getJournalId(), $gridDataElement->getId());
+		if ($customOrder !== null) return $customOrder;
+
+		if ($gridDataElement->getCurrent()) return 0;
+		return $gridDataElement->getDatePublished();
+	}
+
+	/**
+	 * @copydoc GridHandler::addFeatures()
+	 */
+	function initFeatures($request, $args) {
+		import('lib.pkp.classes.controllers.grid.feature.OrderGridItemsFeature');
+		return array(new OrderGridItemsFeature());
 	}
 
 	/**
