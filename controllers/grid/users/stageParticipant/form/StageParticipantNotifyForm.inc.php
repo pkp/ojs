70fea77830753c1eafc039ecc50e094ce2dbe317
Alec Smecher <alec@smecher.bc.ca>
2016-11-30 15:24:53 -0800

    pkp/pkp-lib#2053 Add editor assign template to submission stage

--- controllers/grid/users/stageParticipant/form/StageParticipantNotifyForm.inc.php
+++ controllers/grid/users/stageParticipant/form/StageParticipantNotifyForm.inc.php
@@ -30,9 +30,8 @@ class StageParticipantNotifyForm extends PKPStageParticipantNotifyForm {
 	 * @return array
 	 */
 	protected function _getStageTemplates() {
-
 		return array(
-			WORKFLOW_STAGE_ID_SUBMISSION => array(),
+			WORKFLOW_STAGE_ID_SUBMISSION => array('EDITOR_ASSIGN'),
 			WORKFLOW_STAGE_ID_EXTERNAL_REVIEW => array('EDITOR_ASSIGN'),
 			WORKFLOW_STAGE_ID_EDITING => array('COPYEDIT_REQUEST'),
 			WORKFLOW_STAGE_ID_PRODUCTION => array('LAYOUT_REQUEST', 'LAYOUT_COMPLETE', 'INDEX_REQUEST', 'INDEX_COMPLETE', 'EDITOR_ASSIGN')
