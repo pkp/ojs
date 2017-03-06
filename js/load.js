3b636fb64df1f02ad9b187e17229ad2d8720f3c7
Nate Wright <natew@notthisway.com>
2017-03-06 14:44:25 +0000

    pkp/pkp-lib#2163 Add event bus and other Vue.js infrastructure

--- js/load.js
+++ js/load.js
@@ -6,12 +6,33 @@ import VueRegistry from '../lib/pkp/js/classes/VueRegistry.js';
 
 // All Vue controllers
 import ListPanel from '../lib/pkp/js/controllers/list/ListPanel.vue';
+import SubmissionsListPanel from '../lib/pkp/js/controllers/list/submissions/SubmissionsListPanel.vue';
 
 // Expose Vue, the registry and controllers in a global var
 window.pkp = {
 	vue: Vue,
 	registry: VueRegistry,
+	eventBus: new Vue(),
 	controllers: {
 		'ListPanel': ListPanel,
+		'SubmissionsListPanel': SubmissionsListPanel,
 	},
+	/**
+	 * Helper function to determine if the current user has a role
+	 *
+	 * @param string|array role The key name of the role to check for
+	 * @return bool
+	 */
+	userHasRole: function(role) {
+
+		if (typeof role === 'string') {
+			role = [role];
+		}
+
+		for (var r in role) {
+			if ($.pkp.currentUser.accessRoles.indexOf($.pkp.app.accessRoles[role[r]]) > -1) {
+				return true;
+			}
+		}
+	}
 };
