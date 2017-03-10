01a67919b421005d9af5706fc6390575ea5cedd1
Alec Smecher <alec@smecher.bc.ca>
2017-03-10 11:30:33 -0800

    Update copyright dates for 2017

--- plugins/generic/xmlGalley/transform/test.xsl
+++ plugins/generic/xmlGalley/transform/test.xsl
@@ -3,7 +3,7 @@
 <!--
   * plugins/generic/xmlGalley/transform/test.xsl
   *
-  * Copyright (c) 2013-2016 Simon Fraser University
+  * Copyright (c) 2013-2017 Simon Fraser University
   * Copyright (c) 2003-2016 John Willinsky
   * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
   *
--- plugins/oaiMetadataFormats/rfc1807/OAIMetadataFormatPlugin_RFC1807.inc.php
+++ plugins/oaiMetadataFormats/rfc1807/OAIMetadataFormatPlugin_RFC1807.inc.php
@@ -3,7 +3,7 @@
 /**
  * @file plugins/oaiMetadataFormats/rfc1807/OAIMetadataFormatPlugin_RFC1807.inc.php
  *
- * Copyright (c) 2013-2016 Simon Fraser University
+ * Copyright (c) 2013-2017 Simon Fraser University
  * Copyright (c) 2003-2016 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
