fdcc0c0089bce000bc34d8bf37d8aecd239cdee4
Dimitris Efstathiou <defstat@gmail.com>
2017-03-30 20:46:30 +0300

    Navigation Plugin - Initial Commit

---
+++ plugins/generic/navigationMenus/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2003-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Wrapper for NavigationMenus plugin.
+ *
+ * @package plugins.generic.navigationMenus
+ *
+ */
+
+require_once('NavigationMenusPlugin.inc.php');
+return new NavigationMenusPlugin();
+
+?>
