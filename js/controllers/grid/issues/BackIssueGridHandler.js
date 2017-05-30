8b86f9b3e2389d539714599c202850a2294d1221
Nate Wright <natew@notthisway.com>
2017-05-30 14:41:04 +0100

    pkp/pkp-lib#2163 Update copyright info

--- js/controllers/grid/issues/BackIssueGridHandler.js
+++ js/controllers/grid/issues/BackIssueGridHandler.js
@@ -4,7 +4,7 @@
 /**
  * @file js/controllers/grid/issues/BackIssueGridHandler.js
  *
- * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2014-2017 Simon Fraser University
  * Copyright (c) 2000-2016 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
