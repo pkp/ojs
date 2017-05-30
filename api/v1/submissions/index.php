628940c115741d23fe6cbf4e10dfca6f40519fd2
Nate Wright <natew@notthisway.com>
2017-05-30 14:41:04 +0100

    pkp/pkp-lib#2163 Update copyright info

--- api/v1/submissions/index.php
+++ api/v1/submissions/index.php
@@ -7,7 +7,7 @@
 /**
  * @file api/v1/submissions/index.php
  *
- * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2014-2017 Simon Fraser University
  * Copyright (c) 2003-2016 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
