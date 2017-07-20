9f7195ad5c3dcf59f44fac7ba3066461d7ab5516
Nate Wright <natew@notthisway.com>
2017-07-20 11:03:13 +0100

    pkp/pkp-lib#2163 Correct copyright statement in api files

--- api/v1/galleys/GalleysHandler.inc.php
+++ api/v1/galleys/GalleysHandler.inc.php
@@ -3,7 +3,7 @@
 /**
  * @file api/v1/files/GalleysHandler.inc.php
  *
- * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2014-2017 Simon Fraser University
  * Copyright (c) 2003-2017 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
