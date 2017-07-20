f0ec6692bd24162887024593c33bc3a6cbc9a9c8
Nate Wright <natew@notthisway.com>
2017-07-20 10:58:16 +0100

    pkp/pkp-lib#2163 Update copyright date in api files

--- api/v1/_users/index.php
+++ api/v1/_users/index.php
@@ -8,7 +8,7 @@
  * @file api/v1/_users/index.php
  *
  * Copyright (c) 2014-2017 Simon Fraser University
- * Copyright (c) 2003-2016 John Willinsky
+ * Copyright (c) 2003-2017 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @ingroup api_v1_backend
