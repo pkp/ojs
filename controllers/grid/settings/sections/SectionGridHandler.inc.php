c89dfc96ac7d9a69952d9c2e16f52a53643f9276
ajnyga <ajnyga@gmail.com>
2017-09-11 08:40:35 +0300

    fix sql

--- controllers/grid/settings/sections/SectionGridHandler.inc.php
+++ controllers/grid/settings/sections/SectionGridHandler.inc.php
@@ -219,7 +219,7 @@ class SectionGridHandler extends SetupGridHandler {
 
 		AppLocale::requireComponents(LOCALE_COMPONENT_PKP_MANAGER);
 		$articleDao = DAORegistry::getDAO('ArticleDAO');
-		$checkSubmissions = $articleDao->retrieve('SELECT submission_id FROM submissions WHERE section_id = ? AND context_id = ?', (int) $request->getUserVar('sectionId'), (int) $journal->getId());
+		$checkSubmissions = $articleDao->retrieve('SELECT submission_id FROM submissions WHERE section_id = ? AND context_id = ?', array((int) $request->getUserVar('sectionId'), (int) $journal->getId()));
 
 		if ($checkSubmissions->numRows() > 0) {
 			return new JSONMessage(false, __('manager.sections.alertDelete'));
