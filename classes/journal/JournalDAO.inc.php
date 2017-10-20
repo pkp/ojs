93c132113f80be667c35f0a3f62afab3c8ac54f4
Alec Smecher <alec@smecher.bc.ca>
2017-10-19 20:50:11 -0700

    pkp/pkp-lib#1816 Fix delete function

--- classes/journal/JournalDAO.inc.php
+++ classes/journal/JournalDAO.inc.php
@@ -89,7 +89,7 @@ class JournalDAO extends ContextDAO {
 		$subscriptionDao->deleteByJournalId($journalId);
 
 		$subscriptionTypeDao = DAORegistry::getDAO('SubscriptionTypeDAO');
-		$subscriptionTypeDao->deleteJournal($journalId);
+		$subscriptionTypeDao->deleteByJournal($journalId);
 
 		$announcementDao = DAORegistry::getDAO('AnnouncementDAO');
 		$announcementDao->deleteByAssoc(ASSOC_TYPE_JOURNAL, $journalId);
