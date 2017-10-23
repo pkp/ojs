de5fcc05d252fa4dfcddeca3c8b2a78b286d3ed8
Bozana Bokan <bozana.bokan@posteo.net>
2017-10-23 21:39:17 +0200

    pkp/pkp-lib#2753 import constant containing class

--- classes/article/ArticleGalleyDAO.inc.php
+++ classes/article/ArticleGalleyDAO.inc.php
@@ -482,6 +482,7 @@ class ArticleGalleyDAO extends RepresentationDAO implements PKPPubIdPluginDAO {
 			$params[] = $pubIdSettingValue;
 		}
 
+		import('classes.article.Article'); // STATUS_DECLINED constant
 		$result = $this->retrieveRange(
 				'SELECT	sf.*, g.*
 			FROM	submission_galleys g
