86fc4de7036e732279811489b4d84df076c78ecc
Dimitris Efstathiou <defstat@gmail.com>
2018-01-13 15:21:24 +0200

    pkp/pkp-lib#3056 Port StaticPages as NMIs

--- classes/install/Upgrade.inc.php
+++ classes/install/Upgrade.inc.php
@@ -2575,6 +2575,33 @@ class Upgrade extends Installer {
 		return true;
 	}
 
+	/**
+	 * If StaticPages table exists we should port the data as NMIs
+	 */
+	function migrateStaticPagesToNavigationMenuItems() {
+		if ($this->tableExists('static_pages')) {
+			$contextDao = Application::getContextDAO();
+			$navigationMenuItemDao = DAORegistry::getDAO('NavigationMenuItemDAO');
+
+			import('plugins.generic.staticPages.classes.StaticPagesDAO');
+
+			$staticPagesDao = new StaticPagesDAO();
+
+			$contexts = $contextDao->getAll();
+			while ($context = $contexts->next()) {
+				$contextStaticPages = $staticPagesDao->getByContextId($context->getId())->toAssociativeArray();
+				foreach($contextStaticPages as $staticPage) {
+					$retNMIId = $navigationMenuItemDao->portStaticPage($staticPage);
+					if ($retNMIId) {
+						$staticPagesDao->deleteById($staticPage->getId());
+					}
+				}
+			}
+		}
+
+
+		return true;
+	}
 }
 
 ?>
