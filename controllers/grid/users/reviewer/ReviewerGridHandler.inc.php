b263f6fd53bb74a2530d8dd93d33ec23ebb6481e
Nate Wright <natew@notthisway.com>
2018-03-22 10:03:43 +0000

    pkp/pkp-lib#3130 Consolidate shared authorize method in pkp-lib

--- controllers/grid/users/reviewer/ReviewerGridHandler.inc.php
+++ controllers/grid/users/reviewer/ReviewerGridHandler.inc.php
@@ -42,35 +42,6 @@ class ReviewerGridHandler extends PKPReviewerGridHandler {
 		}
 		return parent::reviewRead($args, $request);
 	}
-
-	//
-	// Overridden methods from PKPHandler
-	//
-	/**
-	 * @copydoc PKPHandler::authorize()
-	 */
-	function authorize($request, &$args, $roleAssignments) {
-		$stageId = $request->getUserVar('stageId'); // This is being validated in WorkflowStageAccessPolicy
-
-		// Not all actions need a stageId. Some work off the reviewAssignment which has the type and round.
-		$this->_stageId = (int)$stageId;
-
-		// Get the stage access policy
-		import('lib.pkp.classes.security.authorization.WorkflowStageAccessPolicy');
-		$workflowStageAccessPolicy = new WorkflowStageAccessPolicy($request, $args, $roleAssignments, 'submissionId', $stageId, WORKFLOW_TYPE_EDITORIAL);
-
-		// Add policy to ensure there is a review round id.
-		import('lib.pkp.classes.security.authorization.internal.ReviewRoundRequiredPolicy');
-		$workflowStageAccessPolicy->addPolicy(new ReviewRoundRequiredPolicy($request, $args, 'reviewRoundId', $this->_getReviewRoundOps()));
-
-		// Add policy to ensure there is a review assignment for certain operations.
-		import('lib.pkp.classes.security.authorization.internal.ReviewAssignmentRequiredPolicy');
-		$workflowStageAccessPolicy->addPolicy(new ReviewAssignmentRequiredPolicy($request, $args, 'reviewAssignmentId', $this->_getReviewAssignmentOps()));
-		$this->addPolicy($workflowStageAccessPolicy);
-
-		return parent::authorize($request, $args, $roleAssignments);
-	}
-
 }
 
 ?>
