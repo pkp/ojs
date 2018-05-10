ad0e4330d8502a6fa1eac89912e2bcfc0f472df1
Alec Smecher <alec@smecher.bc.ca>
2018-05-10 13:14:16 -0700

    Interpret missing publishingMode as open access (0 vs null) (SQL comparison tweak)

--- classes/oai/ojs/OAIDAO.inc.php
+++ classes/oai/ojs/OAIDAO.inc.php
@@ -239,7 +239,7 @@ class OAIDAO extends PKPOAIDAO {
 				JOIN sections s ON (s.section_id = a.section_id)
 				JOIN journals j ON (j.journal_id = a.context_id)
 				LEFT JOIN journal_settings jsl ON (jsl.journal_id = j.journal_id AND jsl.setting_name=?)
-			WHERE	i.published = 1 AND j.enabled = 1 AND jsl.setting_value <> ? AND a.status <> ?
+			WHERE	i.published = 1 AND j.enabled = 1 AND (jsl.setting_value IS NULL OR jsl.setting_value <> ?) AND a.status <> ?
 				' . (isset($journalId) ?' AND j.journal_id = ?':'') . '
 				' . (isset($sectionId) ?' AND s.section_id = ?':'') . '
 				' . ($from?' AND GREATEST(a.last_modified, i.last_modified) >= ' . $this->datetimeToDB($from):'') . '
