acd20445fd7dd20d24a38f97ad3825f6f82c643a
Dimitris Efstathiou <defstat@gmail.com>
2018-05-14 14:29:34 +0300

    Initial Commit - ReviewAssignments Export

---
+++ plugins/importexport/native/filter/ReviewAssignmentNativeXmlFilter.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file plugins/importexport/native/filter/AuthorNativeXmlFilter.inc.php
+ *
+ * Copyright (c) 2014-2018 Simon Fraser University
+ * Copyright (c) 2000-2018 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AuthorNativeXmlFilter
+ * @ingroup plugins_importexport_native
+ *
+ * @brief Class that converts a Author to a Native XML document.
+ */
+
+import('lib.pkp.plugins.importexport.native.filter.PKPAuthorNativeXmlFilter');
+
+class AuthorNativeXmlFilter extends PKPAuthorNativeXmlFilter {
+	/**
+	 * Constructor
+	 * @param $filterGroup FilterGroup
+	 */
+	function __construct($filterGroup) {
+		parent::__construct($filterGroup);
+	}
+
+
+	//
+	// Implement template methods from PersistableFilter
+	//
+	/**
+	 * @copydoc PersistableFilter::getClassName()
+	 */
+	function getClassName() {
+		return 'plugins.importexport.native.filter.AuthorNativeXmlFilter';
+	}
+}
+
+?>
