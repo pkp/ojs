aeac4fa713c34a721af796728733b7e0914b8e0b
Kassim Machioudi <kassim@machioudi.com>
2018-06-04 03:27:22 +0100

    move submission tests which apply to OMP to parent PKPSubmissionsTest class in pkp-lib pkp/pkp-lib#2875

--- tests/api/SubmissionsTest.php
+++ tests/api/SubmissionsTest.php
@@ -13,28 +13,9 @@
  * @brief Test class for the /submissions endpoint
  */
 
-import('lib.pkp.tests.PKPApiTestCase');
+import('lib.pkp.tests.PKPSubmissionsTest');
 
-class SubmissionsTest extends PKPApiTestCase {
-	/**
-	 * @covers /submissions
-	 * @expectedException GuzzleHttp\Exception\ClientException
-	 */
-	public function testGetSubmissionsWithoutToken() {
-		$response = $this->_sendRequest('GET', '/submissions', array(), false);
-	}
-
-	/**
-	 * @covers /submissions
-	 */
-	public function testGetSubmissions() {
-		$response = $this->_sendRequest('GET', '/submissions');
-		$this->assertEquals(200, $response->getStatusCode());
-		
-		$data = $this->_getResponseData($response);
-		$this->assertArrayHasKey('itemsMax', $data);
-		$this->assertArrayHasKey('items', $data);
-	}
+class SubmissionsTest extends PKPSubmissionsTest {
 
 	/**
 	 * @covers /submissions/{submissionId}/galleys
@@ -70,103 +51,6 @@ class SubmissionsTest extends PKPApiTestCase {
 		$this->assertEquals(200, $response->getStatusCode());
 		$data = $this->_getResponseData($response);
 		$this->assertEquals(1, count($data));
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}
-	 * @expectedException GuzzleHttp\Exception\ClientException
-	 */
-	public function testGetSubmissionByIdWithValidIdWithoutToken() {
-		$response = $this->_sendRequest('GET', '/submissions/25', array(), false);
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}
-	 * @expectedException GuzzleHttp\Exception\ClientException
-	 */
-	public function testGetSubmissionByIdWithInvalidId() {
-		$response = $this->_sendRequest('GET', '/submissions/9999');
-		$this->assertSame(404, $response->getStatusCode());
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}
-	 */
-	public function testGetSubmissionByIdWithValidId() {
-		$response = $this->_sendRequest('GET', '/submissions/25');
-		$this->assertSame(200, $response->getStatusCode());
-
-		$data = $this->_getResponseData($response);
-		$this->assertArrayHasKey('id', $data);
-		$this->assertArrayHasKey('title', $data);
-		$this->assertArrayHasKey('abstract', $data);
-		$this->assertArrayHasKey('authors', $data);
-		$this->assertArrayHasKey('section', $data);
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}/participants
-	 * @expectedException GuzzleHttp\Exception\ClientException
-	 */
-	public function testGetSubmissionParticipantsWithoutToken() {
-		$response = $this->_sendRequest('GET', '/submissions/25/participants', array(), false);
-		$this->assertSame(404, $response->getStatusCode());
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}/participants
-	 * @expectedException GuzzleHttp\Exception\ClientException
-	 */
-	public function testGetSubmissionParticipantsWithInvalidId() {
-		$response = $this->_sendRequest('GET', '/submissions/9999/participants');
-		$this->assertSame(404, $response->getStatusCode());
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}/participants
-	 */
-	public function testGetSubmissionParticipants() {
-		$response = $this->_sendRequest('GET', '/submissions/25/participants');
-		$this->assertSame(200, $response->getStatusCode());
-
-		$data = $this->_getResponseData($response);
-		$this->assertTrue(is_array($data));
-		$this->assertNotEmpty($data);
-
-		$participant = (array) array_shift($data);
-		$this->assertArrayHasKey('id', $participant);
-		$this->assertArrayHasKey('_href', $participant);
-		$this->assertArrayHasKey('userName', $participant);
-		$this->assertArrayHasKey('email', $participant);
-		$this->assertArrayHasKey('groups', $participant);
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}/participants/{stageId}
-	 * @expectedException GuzzleHttp\Exception\ClientException
-	 */
-	public function testGetSubmissionParticipantsAssignedToStageWithoutToken() {
-		$response = $this->_sendRequest('GET', '/submissions/25/participants/1', array(), false);
-		$this->assertSame(404, $response->getStatusCode());
-	}
-
-	/**
-	 * @covers /submissions/{submissionId}/participants/{stageId}
-	 */
-	public function testGetSubmissionParticipantsAssignedToStage() {
-		$response = $this->_sendRequest('GET', '/submissions/25/participants/1');
-		$this->assertSame(200, $response->getStatusCode());
-
-		$data = $this->_getResponseData($response);
-		$this->assertTrue(is_array($data));
-		$this->assertNotEmpty($data);
-
-		$participant = (array) array_shift($data);
-		$this->assertArrayHasKey('id', $participant);
-		$this->assertArrayHasKey('_href', $participant);
-		$this->assertArrayHasKey('userName', $participant);
-		$this->assertArrayHasKey('email', $participant);
-		$this->assertArrayHasKey('groups', $participant);
 	}
 }
 
