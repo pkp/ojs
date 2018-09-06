5c7a15f5b3c0bad8e1b93f809959a9ea767e47d1
Alec Smecher <alec@smecher.bc.ca>
2018-09-06 14:21:49 -0700

    pkp/pkp-lib#4024 Add ContextAccessPolicy (or equivalents)

--- controllers/grid/settings/plugins/SettingsPluginGridHandler.inc.php
+++ controllers/grid/settings/plugins/SettingsPluginGridHandler.inc.php
@@ -85,6 +85,9 @@ class SettingsPluginGridHandler extends PluginGridHandler {
 					break;
 			}
 			$this->addPolicy(new PluginAccessPolicy($request, $args, $roleAssignments, $accessMode));
+		} else {
+			import('lib.pkp.classes.security.authorization.ContextAccessPolicy');
+			$this->addPolicy(new ContextAccessPolicy($request, $roleAssignments));
 		}
 		return parent::authorize($request, $args, $roleAssignments);
 	}
