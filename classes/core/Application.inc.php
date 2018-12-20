4752ffc15ab7d97483060326eb4bbf86d8bb6053
Alec Smecher <alec@smecher.bc.ca>
2018-12-19 17:23:24 -0800

    Move SubmissionFileDAO to fully shared

--- classes/core/Application.inc.php
+++ classes/core/Application.inc.php
@@ -111,7 +111,6 @@ class Application extends PKPApplication {
 			'PublishedArticleDAO' => 'classes.article.PublishedArticleDAO',
 			'ReviewerSubmissionDAO' => 'classes.submission.reviewer.ReviewerSubmissionDAO',
 			'SectionDAO' => 'classes.journal.SectionDAO',
-			'SubmissionFileDAO' => 'classes.article.SubmissionFileDAO',
 			'SubscriptionDAO' => 'classes.subscription.SubscriptionDAO',
 			'SubscriptionTypeDAO' => 'classes.subscription.SubscriptionTypeDAO',
 		));
