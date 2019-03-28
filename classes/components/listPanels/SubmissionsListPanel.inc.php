c5db97ff2fb3d7c71b5b83d54787c0d24cba84dd
Nate Wright <natew@notthisway.com>
2019-03-28 11:00:35 +0000

    pkp/pkp-lib#2906 Fix fatal error calling Application from within namespace

--- classes/components/listPanels/SubmissionsListPanel.inc.php
+++ classes/components/listPanels/SubmissionsListPanel.inc.php
@@ -23,7 +23,7 @@ class SubmissionsListPanel extends PKPSubmissionsListPanel {
 	public function getConfig() {
 		$config = parent::getConfig();
 
-		$request = Application::get()->getRequest();
+		$request = \Application::get()->getRequest();
 		if ($request->getContext()) {
 			$config['filters'][] = [
 				'heading' => __('section.sections'),
@@ -70,7 +70,7 @@ class SubmissionsListPanel extends PKPSubmissionsListPanel {
 	 * @return array
 	 */
 	public function getSectionFilters() {
-		$request = Application::get()->getRequest();
+		$request = \Application::get()->getRequest();
 		$context = $request->getContext();
 
 		if (!$context) {
