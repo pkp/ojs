fc30af6ec3a84d601e7a07db2ebc0bb212355e2c
Alec Smecher <alec@smecher.bc.ca>
2019-07-18 12:07:55 -0700

    Add warning for unresolvable article (master branch)

--- classes/install/Upgrade.inc.php
+++ classes/install/Upgrade.inc.php
@@ -1058,6 +1058,10 @@ class Upgrade extends Installer {
 				$creatorUserId = $managerUsers->next()->getId();
 			}
 			$article = $submissionDao->getById($row['article_id']);
+			if (!$article) {
+				error_log('WARNING: Unable to fetch article for article_supplementary_files.supp_id = ' . $row['supp_id'] . '. Skipping.');
+				continue;
+			}
 
 			// if it is a remote supp file and article is published, convert it to a remote galley
 			if (!$row['file_id'] && $row['remote_url'] != '' && $article->getStatus() == STATUS_PUBLISHED) {
