3c7262e8eb2afc3a680bfb2ac6c97241935d372d
Alec Smecher <alec@smecher.bc.ca>
2019-09-13 18:10:51 -0700

    pkp/pkp-lib#5017 Include submission subtitle in Crossref XML

--- plugins/importexport/crossref/filter/ArticleCrossrefXmlFilter.inc.php
+++ plugins/importexport/crossref/filter/ArticleCrossrefXmlFilter.inc.php
@@ -93,6 +93,7 @@ class ArticleCrossrefXmlFilter extends IssueCrossrefXmlFilter {
 		// title
 		$titlesNode = $doc->createElementNS($deployment->getNamespace(), 'titles');
 		$titlesNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'title', htmlspecialchars($submission->getTitle($submission->getLocale()), ENT_COMPAT, 'UTF-8')));
+		if ($subtitle = $submission->getSubtitle($submission->getLocale())) $titlesNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'subtitle', htmlspecialchars($subtitle, ENT_COMPAT, 'UTF-8')));
 		$journalArticleNode->appendChild($titlesNode);
 
 		// contributors
