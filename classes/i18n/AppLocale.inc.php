6944d2e3b5ad1475801d174334870131e4ae2c48
Alec Smecher <alec@smecher.bc.ca>
2019-09-18 16:01:32 -0700

    pkp/pkp-lib#4779 Adapt to PO files instead of XLIFF

--- classes/i18n/AppLocale.inc.php
+++ classes/i18n/AppLocale.inc.php
@@ -173,14 +173,14 @@ class AppLocale extends PKPLocale {
 	static function makeComponentMap($locale) {
 		$baseDir = "locale/$locale/";
 		return parent::makeComponentMap($locale) + array(
-			LOCALE_COMPONENT_APP_COMMON => $baseDir . 'locale.xml',
-			LOCALE_COMPONENT_APP_AUTHOR => $baseDir . 'author.xml',
-			LOCALE_COMPONENT_APP_SUBMISSION => $baseDir . 'submission.xml',
-			LOCALE_COMPONENT_APP_EDITOR => $baseDir . 'editor.xml',
-			LOCALE_COMPONENT_APP_MANAGER => $baseDir . 'manager.xml',
-			LOCALE_COMPONENT_APP_ADMIN => $baseDir . 'admin.xml',
-			LOCALE_COMPONENT_APP_DEFAULT => $baseDir . 'default.xml',
-			LOCALE_COMPONENT_APP_API => $baseDir . 'api.xml',
+			LOCALE_COMPONENT_APP_COMMON => $baseDir . 'locale.po',
+			LOCALE_COMPONENT_APP_AUTHOR => $baseDir . 'author.po',
+			LOCALE_COMPONENT_APP_SUBMISSION => $baseDir . 'submission.po',
+			LOCALE_COMPONENT_APP_EDITOR => $baseDir . 'editor.po',
+			LOCALE_COMPONENT_APP_MANAGER => $baseDir . 'manager.po',
+			LOCALE_COMPONENT_APP_ADMIN => $baseDir . 'admin.po',
+			LOCALE_COMPONENT_APP_DEFAULT => $baseDir . 'default.po',
+			LOCALE_COMPONENT_APP_API => $baseDir . 'api.po',
 		);
 	}
 }
