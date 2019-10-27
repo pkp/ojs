10f22ce277da51703e3fecd7daafa73395e8883e
Jonas Raoni Soares da Silva <jonas@raoni.org>
2019-10-27 23:38:23 +0300

    pkp/pkp-lib#4844 Initial fixes after review.

--- js/load.js
+++ js/load.js
@@ -17,6 +17,8 @@ import SelectListPanel from '@/components/SelectListPanel/SelectListPanel.vue';
 import SelectSubmissionsListPanel from '@/components/SelectListPanel/submissions/SelectSubmissionsListPanel.vue';
 import SelectReviewerListPanel from '@/components/SelectListPanel/users/SelectReviewerListPanel.vue';
 import Statistics from '@/components/Statistics/Statistics.vue';
+import StatisticsEditorial from '@/components/Statistics/StatisticsEditorial.vue';
+import StatisticsSubmissions from '@/components/Statistics/StatisticsSubmissions.vue';
 
 // Expose Vue, the registry and controllers in a global var
 window.pkp = Object.assign(PkpLoad, {
@@ -27,5 +29,7 @@ window.pkp = Object.assign(PkpLoad, {
 		SelectSubmissionsListPanel,
 		SelectReviewerListPanel,
 		Statistics,
+		StatisticsEditorial,
+		StatisticsSubmissions,
 	},
 });
