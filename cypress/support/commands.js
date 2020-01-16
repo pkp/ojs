0edc8dbceb3dfeb3670c20050828a5c35fade0c9
Nate Wright <natew@notthisway.com>
2020-01-16 16:42:19 +0000

    pkp/pkp-lib#5236 Fix publishing test looking for wrong text

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,7 +15,7 @@ Cypress.Commands.add('publish', (issueId, issueTitle) => {
 	cy.get('button[id="issue-button"]').click();
 	cy.get('select[id="issueEntry-issueId-control"]').select(issueId);
 	cy.get('div[id="issue"] button:contains("Save")').click();
-	cy.get('div:contains("The journal entry details have been updated.")');
+	cy.get('div:contains("The publication\'s issue details have been updated.")');
 	cy.get('div[id="publication"] button:contains("Schedule For Publication")').click();
 	cy.get('div:contains("All publication requirements have been met. This will be published immediately in ' + issueTitle + '. Are you sure you want to publish this?")');
 	cy.get('div.pkpWorkflow__publishModal button:contains("Publish")').click();
