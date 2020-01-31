925ace9d26cc93b670de14c6e246b9a9a104144b
Alec Smecher <alec@smecher.bc.ca>
2020-01-31 12:44:24 -0800

    Remove unnecessary reference

--- tools/rebuildSearchIndex.php
+++ tools/rebuildSearchIndex.php
@@ -64,7 +64,7 @@ class rebuildSearchIndex extends CommandLineTool {
 	 * when constructing galley/supp file download URLs.
 	 * @see PKPRequest::getBaseUrl()
 	 */
-	public function callbackBaseUrl($hookName, &$params) {
+	public function callbackBaseUrl($hookName, $params) {
 		$baseUrl =& $params[0];
 		$baseUrl = Config::getVar('general', 'base_url');
 		return true;
