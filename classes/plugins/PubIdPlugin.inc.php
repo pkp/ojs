0e6abd4e6e8354617713e329e641ed6eaf559270
ajnyga <ajnyga@gmail.com>
2020-02-06 17:46:52 +0200

    pkp/pkp-lib#5473 fix reassign DOIs

--- classes/plugins/PubIdPlugin.inc.php
+++ classes/plugins/PubIdPlugin.inc.php
@@ -32,22 +32,24 @@ abstract class PubIdPlugin extends PKPPubIdPlugin {
 				$suffixFieldName = $this->getSuffixFieldName();
 				$suffixGenerationStrategy = $this->getSetting($context->getId(), $suffixFieldName);
 				if ($suffixGenerationStrategy != 'customId') {
-					$submissionEnabled = $this->isObjectTypeEnabled('Submission', $context->getId());
+					$submissionEnabled = $this->isObjectTypeEnabled('Publication', $context->getId());
 					$representationEnabled = $this->isObjectTypeEnabled('Representation', $context->getId());
 					if ($submissionEnabled || $representationEnabled) {
-						$submissionDao = Application::getSubmissionDAO();
+						$publicationDao = DAORegistry::getDAO('PublicationDAO'); /* @var $publicationDao PublicationDAO */
 						$representationDao = Application::getRepresentationDAO();
-						$submissions = Services::getMany([
+						$submissions = Services::get('submission')->getMany([
 							'contextId' => $context->getId(),
 							'status' => STATUS_PUBLISHED,
 							'count' => 5000, // large upper limit
 						]);
 						foreach ($submissions as $submission) {
 							if ($submissionEnabled) {
-								$submissionPubId = $submission->getStoredPubId($this->getPubIdType());
-								if (empty($submissionPubId)) {
-									$submissionPubId = $this->getPubId($submission);
-									$submissionDao->changePubId($submission->getId(), $this->getPubIdType(), $submissionPubId);
+								foreach ($submission->getData('publications') as $publication) {
+									$publicationPubId = $publication->getStoredPubId($this->getPubIdType());
+									if (empty($publicationPubId)) {
+										$publicationPubId = $this->getPubId($publication);
+										$publicationDao->insertPubId($publication->getId(), $this->getPubIdType(), $publicationPubId);
+									}
 								}
 							}
 							if ($representationEnabled) {
