0b4cd8e53a4a9ebf8277d8ae7103a76edd849e84
Nate Wright <natew@notthisway.com>
2020-02-17 17:01:56 +0000

    pkp/pkp-lib#5430 Move some OJS-specific code into OJS

--- controllers/tab/pubIds/form/PublicIdentifiersForm.inc.php
+++ controllers/tab/pubIds/form/PublicIdentifiersForm.inc.php
@@ -15,6 +15,30 @@
 
 import('lib.pkp.controllers.tab.pubIds.form.PKPPublicIdentifiersForm');
 
-class PublicIdentifiersForm extends PKPPublicIdentifiersForm { }
+class PublicIdentifiersForm extends PKPPublicIdentifiersForm {
 
+	/**
+	 * Constructor.
+	 * @param $pubObject object
+	 * @param $stageId integer
+	 * @param $formParams array
+	 */
+	function __construct($pubObject, $stageId = null, $formParams = null) {
+		parent::__construct($pubObject, $stageId, $formParams);
+	}
+
+	/**
+	 * @copydoc Form::fetch()
+	 */
+	function fetch($request, $template = null, $display = false) {
+		$templateMgr = TemplateManager::getManager($request);
+		$enablePublisherId = $request->getContext()->getData('enablePublisherId');
+		$templateMgr->assign([
+			'enablePublisherId' => (is_a($this->getPubObject(), 'ArticleGalley') && in_array('galley', $enablePublisherId))
+		]);
+
+		return parent::fetch($request, $template, $display);
+	}
+
+}
 
