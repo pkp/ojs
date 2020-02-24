e8dab6838d3b65bb45c8ec636064234b4602c255
Nate Wright <natew@notthisway.com>
2020-02-24 10:49:33 +0000

    pkp/pkp-lib#5255 Prevent nested loops to get parent category title

--- classes/components/forms/publication/IssueEntryForm.inc.php
+++ classes/components/forms/publication/IssueEntryForm.inc.php
@@ -97,16 +97,11 @@ class IssueEntryForm extends FormComponent {
 
 		// Categories
 		$categoryOptions = [];
-		$result = \DAORegistry::getDAO('CategoryDAO')->getByContextId($publicationContext->getId())->toArray();
-		foreach ($result as $category) {
+		$categories = \DAORegistry::getDAO('CategoryDAO')->getByContextId($publicationContext->getId())->toAssociativeArray();
+		foreach ($categories as $category) {
 			$label = $category->getLocalizedTitle();
 			if ($category->getParentId()) {
-				$parentCategories = $result;
-				foreach ($parentCategories as $parentCategory) {
-					if ($parentCategory->getId() === $category->getParentId()) {
-						$label = $parentCategory->getLocalizedTitle() . ' > ' . $label;
-					}
-				}
+				$label = $categories[$category->getParentId()]->getLocalizedTitle() . ' > ' . $label;
 			}
 			$categoryOptions[] = [
 				'value' => (int) $category->getId(),
