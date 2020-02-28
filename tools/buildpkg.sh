dfdc4e3a7c14e5a5162f00ef088a92125e8ffe56
Alec Smecher <alec@smecher.bc.ca>
2020-02-28 12:50:25 -0800

    Add exclusion

--- tools/buildpkg.sh
+++ tools/buildpkg.sh
@@ -68,6 +68,7 @@ lib/pkp/lib/vendor/symfony/translation/Tests/						\
 lib/pkp/lib/vendor/symfony/process/Tests/						\
 lib/pkp/lib/vendor/pimple/pimple/src/Pimple/Tests/					\
 lib/pkp/lib/vendor/robloach/component-installer/tests/ComponentInstaller/Test/		\
+lib/pkp/lib/vendor/michelf/php-markdown/test
 plugins/generic/citationStyleLanguage/lib/vendor/satooshi/php-coveralls/tests/		\
 plugins/generic/citationStyleLanguage/lib/vendor/guzzle/guzzle/tests/			\
 plugins/generic/citationStyleLanguage/lib/vendor/seboettg/collection/tests/		\
