b766fe8665912eb69234199a139fc3f762775fb0
ajnyga <ajnyga@gmail.com>
2020-03-27 13:38:30 +0200

    Add ReviewerReviewStep3Form for OJS

---
+++ classes/submission/reviewer/form/ReviewerReviewStep3Form.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file classes/submission/reviewer/form/ReviewerReviewStep3Form.inc.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class ReviewerReviewStep3Form
+ * @ingroup submission_reviewer_form
+ *
+ * @brief Form for Step 3 of a review in OJS.
+ */
+
+import('lib.pkp.classes.submission.reviewer.form.PKPReviewerReviewStep3Form');
+
+class ReviewerReviewStep3Form extends PKPReviewerReviewStep3Form {
+
+	/**
+	 * @copydoc PKPReviewerReviewStep3Form::__construct()
+	 */
+	function __construct($request, $reviewerSubmission, $reviewAssignment) {
+		parent::__construct($request, $reviewerSubmission, $reviewAssignment, 3);
+		$this->addCheck(new FormValidatorCustom($this, 'recommendation', 'required', 'reviewer.submission.reviewFormResponse.form.recommendationRequired', function($recommendation) {
+			return isset($recommendation);
+		}));		
+	}
+}
+
+
