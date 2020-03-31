240163c8fb84a3cd31f3f11f8ce92146dcbc189c
Alec Smecher <alec@smecher.bc.ca>
2020-03-31 11:59:39 -0700

    Resolve PHP scrutinizer warnings

--- controllers/modals/editorDecision/EditorDecisionHandler.inc.php
+++ controllers/modals/editorDecision/EditorDecisionHandler.inc.php
@@ -57,7 +57,7 @@ class EditorDecisionHandler extends PKPEditorDecisionHandler {
 	 * Start a new review round
 	 * @param $args array
 	 * @param $request PKPRequest
-	 * @return string Serialized JSON object
+	 * @return JSONMessage
 	 */
 	function saveNewReviewRound($args, $request) {
 		// FIXME: this can probably all be managed somewhere.
@@ -105,7 +105,7 @@ class EditorDecisionHandler extends PKPEditorDecisionHandler {
 	/**
 	 * Get editor decision notification type and level by decision.
 	 * @param $decision int
-	 * @return array
+	 * @return int
 	 */
 	protected function _getNotificationTypeByEditorDecision($decision) {
 		switch ($decision) {
@@ -124,10 +124,8 @@ class EditorDecisionHandler extends PKPEditorDecisionHandler {
 				return NOTIFICATION_TYPE_EDITOR_DECISION_DECLINE;
 			case SUBMISSION_EDITOR_DECISION_SEND_TO_PRODUCTION:
 				return NOTIFICATION_TYPE_EDITOR_DECISION_SEND_TO_PRODUCTION;
-			default:
-				assert(false);
-				return null;
 		}
+		throw new Exception('Unknown editor decision.');
 	}
 
 	/**
