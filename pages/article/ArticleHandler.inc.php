208dfa282287acba209e5e4864fa659219675029
Alec Smecher <alec@smecher.bc.ca>
2020-08-25 15:06:16 -0700

    pkp/pkp-lib#6207 Ensure that submissions are matched against contexts

--- pages/article/ArticleHandler.inc.php
+++ pages/article/ArticleHandler.inc.php
@@ -75,6 +75,7 @@ class ArticleHandler extends Handler {
 
 		if (!$submission && ctype_digit((string) $urlPath)) {
 			$submission = Services::get('submission')->get($urlPath);
+			if ($request->getContext()->getId() != $submission->getContextId()) $submission = null;
 		}
 
 		if (!$submission || $submission->getData('status') !== STATUS_PUBLISHED) {
