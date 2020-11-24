814edf842da535b2302d37327c3991c4f63e228a
Nate Wright <natew@notthisway.com>
2020-11-24 16:48:42 +0000

    pkp/pkp-lib#4622 Add missing router for bulk email API

---
+++ api/v1/_email/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @defgroup api_v1__email User API requests
+ */
+
+/**
+ * @file api/v1/_email/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup api_v1__email
+ * @brief Handle requests for user API functions.
+ *
+ */
+
+import('lib.pkp.api.v1._email.PKPEmailHandler');
+return new PKPEmailHandler();
