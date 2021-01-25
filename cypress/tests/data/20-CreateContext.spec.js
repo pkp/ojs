053a6f24597ff5f1a5a9b2072d33c52028e5a31b
Nate Wright <natew@notthisway.com>
2021-01-25 14:51:10 +0000

    pkp/pkp-lib#6594 Fix tests after adding new context fields

--- cypress/tests/data/20-CreateContext.spec.js
+++ cypress/tests/data/20-CreateContext.spec.js
@@ -22,9 +22,13 @@ describe('Data suite tests', function() {
 		cy.get('div[id=context-name-error-en_US]').find('span').contains('This field is required.');
 		cy.get('div[id=context-acronym-error-en_US]').find('span').contains('This field is required.');
 		cy.get('div[id=context-urlPath-error]').find('span').contains('This field is required.');
+		cy.get('div[id=context-primaryLocale-error]').find('span').contains('This field is required.');
 		cy.get('input[name="name-en_US"]').type(Cypress.env('contextTitles')['en_US'], {delay: 0});
 		cy.get('input[name=acronym-en_US]').type('JPK', {delay: 0});
 		cy.get('span').contains('Enable this journal').siblings('input').check();
+		cy.get('input[name="supportedLocales"][value="en_US').check();
+		cy.get('input[name="supportedLocales"][value="fr_CA').check();
+		cy.get('input[name="primaryLocale"][value="en_US').check();
 
 		// Test invalid path characters
 		cy.get('input[name=urlPath]').type('public&-)knowledge', {delay: 0});
