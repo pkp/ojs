5daa89454d561537ffd739fe101d8c6fa3b2838b
Alec Smecher <alec@smecher.bc.ca>
2021-02-12 08:34:31 -0800

    pkp/pkp-lib#6751 Locales in wrong place (causes PHP8 type error)

--- pages/management/SettingsHandler.inc.php
+++ pages/management/SettingsHandler.inc.php
@@ -79,7 +79,7 @@ class SettingsHandler extends ManagementHandler {
 		// not need to be submitted. It's a dirty hack, but we can change this once
 		// an API is in place for plugins and plugin settings.
 		$plnPlugin = PluginRegistry::getPlugin('generic', 'plnplugin');
-		$archivePnForm = new \PKP\components\forms\FormComponent('archivePn', 'PUT', 'dummy', 'dummy', $supportedFormLocales);
+		$archivePnForm = new \PKP\components\forms\FormComponent('archivePn', 'PUT', 'dummy', $supportedFormLocales);
 		$archivePnForm->addPage([
 				'id' => 'default',
 				'submitButton' => null,
