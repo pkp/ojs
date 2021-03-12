4b9b4d228331f6d0acb420cfa5d0f32232dc93d0
Alec Smecher <alec@smecher.bc.ca>
2021-03-12 14:24:41 -0800

    pkp/pkp-lib#6807 Ensure issues and submissions have a valid last_modified date

--- classes/migration/upgrade/OJSv3_3_0UpgradeMigration.inc.php
+++ classes/migration/upgrade/OJSv3_3_0UpgradeMigration.inc.php
@@ -44,6 +44,10 @@ class OJSv3_3_0UpgradeMigration extends Migration {
 		Capsule::statement("DELETE FROM filters WHERE class_name IN ('plugins.importexport.medra.filter.IssueMedraXmlFilter', 'plugins.importexport.medra.filter.ArticleMedraXmlFilter', 'plugins.importexport.medra.filter.GalleyMedraXmlFilter')");
 		Capsule::statement("DELETE FROM filter_groups WHERE symbolic IN ('issue=>medra-xml', 'article=>medra-xml', 'galley=>medra-xml')");
 		Capsule::statement("DELETE FROM scheduled_tasks WHERE class_name='plugins.importexport.medra.MedraInfoSender'");
+
+		// pkp/pkp-lib#6807 Make sure all submission/issue last modification dates are set
+		Capsule::statement('UPDATE issues SET last_modified = date_published WHERE last_modified IS NULL');
+		Capsule::statement('UPDATE submissions SET last_modified = NOW() WHERE last_modified IS NULL');
 	}
 
 	/**
