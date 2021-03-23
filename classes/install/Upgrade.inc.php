9435c9eff92b5211579754f9b0c53c1251a527ab
Tom G. Christensen <tgc@kb.dk>
2021-03-23 13:18:52 +0100

    pkp/pkp-lib#6870 SQL logic error in upgrade when using PostgreSQL

--- classes/install/Upgrade.inc.php
+++ classes/install/Upgrade.inc.php
@@ -1128,7 +1128,7 @@ class Upgrade extends Installer {
 			case 'postgres7':
 			case 'postgres8':
 			case 'postgres9':
-				$stageAssignmentDao->update('UPDATE stage_assignments SET can_change_metadata=1 FROM stage_assignments sa JOIN user_groups ug ON (sa.user_group_id = ug.user_group_id) WHERE ug.role_id IN ' . $roleString);
+				$stageAssignmentDao->update('UPDATE stage_assignments sa SET can_change_metadata=1 FROM user_groups ug WHERE sa.user_group_id = ug.user_group_id AND ug.role_id IN ' . $roleString);
 				break;
 			default: fatalError("Unknown database type!");
 			}
