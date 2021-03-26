d29d4f82df1f23b772fee44ad21c4e95d57543a8
Alec Smecher <alec@smecher.bc.ca>
2021-03-26 14:01:32 -0700

    pkp/pkp-lib#6888 Exclude example code from distribution package

--- tools/buildpkg.sh
+++ tools/buildpkg.sh
@@ -66,6 +66,7 @@ lib/pkp/js/lib/pnotify/build-tools							\
 lib/pkp/lib/vendor/alex198710/pnotify/.git						\
 lib/pkp/lib/vendor/sebastian								\
 lib/pkp/lib/vendor/oyejorge/less.php/test						\
+lib/pkp/lib/vendor/moxiecode/plupload/examples/						\
 lib/pkp/tools/travis									\
 lib/pkp/lib/swordappv2/.git								\
 lib/pkp/lib/swordappv2/.git								\
@@ -77,6 +78,7 @@ plugins/paymethod/paypal/vendor/guzzle/guzzle/tests/					\
 plugins/generic/citationStyleLanguage/lib/vendor/symfony/debug/				\
 plugins/generic/citationStyleLanguage/lib/vendor/symfony/console/Tests/			\
 plugins/paymethod/paypal/vendor/symfony/http-foundation/Tests/				\
+plugins/paymethod/paypal/vendor/clue/stream-filter/tests/				\
 plugins/paymethod/paypal/vendor/symfony/event-dispatcher/				\
 plugins/generic/citationStyleLanguage/lib/vendor/symfony/filesystem/Tests/		\
 plugins/generic/citationStyleLanguage/lib/vendor/symfony/stopwatch/Tests/		\
@@ -94,6 +96,7 @@ plugins/generic/citationStyleLanguage/lib/vendor/satooshi/php-coveralls/tests/		
 plugins/generic/citationStyleLanguage/lib/vendor/guzzle/guzzle/tests/			\
 plugins/generic/citationStyleLanguage/lib/vendor/seboettg/collection/tests/		\
 plugins/generic/citationStyleLanguage/lib/vendor/seboettg/citeproc-php/tests/		\
+plugins/generic/citationStyleLanguage/lib/vendor/seboettg/citeproc-php/example/		\
 lib/pkp/lib/vendor/nikic/fast-route/test/						\
 lib/pkp/lib/vendor/ezyang/htmlpurifier/tests/						\
 lib/pkp/lib/vendor/ezyang/htmlpurifier/smoketests/					\
