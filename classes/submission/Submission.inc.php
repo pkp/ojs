9aa9e2539250b80d0bf82153a37293c5c163f391
withanaged <dulip.withanage@tib.eu>
2021-09-19 23:35:49 +0200

    GalleyDAO refactor

--- classes/submission/Submission.inc.php
+++ classes/submission/Submission.inc.php
@@ -26,7 +26,8 @@ namespace APP\submission;
 use APP\core\Application;
 use APP\core\Services;
 
-use PKP\db\DAORegistry;
+use APP\facades\Repo;
+use APP\i18n\AppLocale;
 use PKP\submission\PKPSubmission;
 
 class Submission extends PKPSubmission
@@ -92,8 +93,7 @@ class Submission extends PKPSubmission
                         case 'issue':
                             if ($publication->getData('issueId')) {
                                 // override to the issue's year if published as issue-based
-                                $issueDao = & DAORegistry::getDAO('IssueDAO');
-                                $issue = $issueDao->getBySubmissionId($this->getId());
+                                $issue = Repo::issue()->get($publication->getData('issueId'));
                                 if ($issue && $issue->getDatePublished()) {
                                     $fieldValue = date('Y', strtotime($issue->getDatePublished()));
                                 }
@@ -283,6 +283,31 @@ class Submission extends PKPSubmission
         return $galleys;
     }
 
+    /**
+     * Get the localized galleys for an article.
+     *
+     * @return array ArticleGalley
+     *
+     * @deprecated 3.2.0.0
+     */
+    public function getLocalizedGalleys()
+    {
+        $allGalleys = $this->getData('galleys');
+        $galleys = [];
+        foreach ([AppLocale::getLocale(), AppLocale::getPrimaryLocale()] as $tryLocale) {
+            foreach (array_keys($allGalleys) as $key) {
+                if ($allGalleys[$key]->getLocale() == $tryLocale) {
+                    $galleys[] = $allGalleys[$key];
+                }
+            }
+            if (!empty($galleys)) {
+                HookRegistry::call('ArticleGalleyDAO::getLocalizedGalleysByArticle', [&$galleys]);
+                return $galleys;
+            }
+        }
+
+        return $galleys;
+    }
 
     /**
      * Return option selection indicating if author should be hidden in issue ToC.
