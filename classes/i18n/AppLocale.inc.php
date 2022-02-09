86b2c87afcf0057c1bd737444d2938eec9c24dba
Jonas Raoni Soares da Silva <github@raoni.org>
2022-02-10 00:09:18 +0300

    pkp/pkp-lib#6328 Recovered most used methods from AppLocale class for backwards compatibility with external plugins

---
+++ classes/i18n/AppLocale.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @defgroup i18n I18N
+ * Implements localization concerns such as locale files, time zones, and country lists.
+ */
+
+/**
+ * @file classes/i18n/AppLocale.inc.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class PKPLocale
+ * @ingroup i18n
+ *
+ * @brief Deprecated class, kept only for backwards compatibility with external plugins
+ */
+
+namespace APP\i18n;
+
+use PKP\i18n\PKPLocale;
+
+if (!PKP_STRICT_MODE) {
+
+    /**
+     * @deprecated The class \APP\i18n\AppLocale has been replaced by PKP\facades\Locale
+     */
+    class AppLocale extends PKPLocale
+    {
+    }
+
+    class_alias('\APP\i18n\AppLocale', '\AppLocale');
+}
