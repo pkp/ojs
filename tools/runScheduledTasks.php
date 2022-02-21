326b68c105fc283e411896e3120c49eaf5f3f5de
Henrique Ramos <henrique@henriqueramos.eti.br>
2022-02-21 09:33:22 -0300

    pkp/pkp-lib#4622 Creating local SchedulerServiceProvider and forcing runScheduledTasks to use the SchedulerBag from ServiceProvider

--- tools/runScheduledTasks.php
+++ tools/runScheduledTasks.php
@@ -15,64 +15,7 @@
 
 require(dirname(__FILE__) . '/bootstrap.inc.php');
 
-class runScheduledTasks extends \PKP\cliTool\ScheduledTaskTool
-{
-}
-
-$tool = new runScheduledTasks($argv ?? []);
-$tool->execute();
-
-$taskDao = \PKP\db\DAORegistry::getDAO('ScheduledTaskDAO');
-
 $scheduleBag = app(\Illuminate\Console\Scheduling\Schedule::class);
-
-// Send automated reminders to reviewers to complete their assignments.
-$scheduleBag
-    ->call(function () {
-        (new \PKP\Commands\ReviewReminderCommand())->execute();
-    })
-    ->hourly()
-    ->name('ojs:reviewreminder:without-overlapping')
-    ->withoutOverlapping()
-    ->then(function () use ($taskDao) {
-        $taskDao->updateLastRunTime('lib.pkp.classes.task.ReviewReminder');
-    });
-
-// Send automated statistics reports to journal managers and editors.
-$scheduleBag
-    ->call(function () {
-        (new \PKP\Commands\StatisticsReportCommand())->execute();
-    })
-    ->daily()
-    ->name('ojs:statisticsreport:without-overlapping')
-    ->withoutOverlapping()
-    ->then(function () use ($taskDao) {
-        $taskDao->updateLastRunTime('lib.pkp.classes.task.StatisticsReport');
-    });
-
-// Send automated reminders about subscription expiry.
-$scheduleBag
-    ->call(function () {
-        (new \APP\Commands\SubscriptionExpiryReminderCommand())->execute();
-    })
-    ->daily()
-    ->name('ojs:subscriptionexpiryreminder:without-overlapping')
-    ->withoutOverlapping()
-    ->then(function () use ($taskDao) {
-        $taskDao->updateLastRunTime('classes.tasks.SubscriptionExpiryReminder');
-    });
-
-// Automatically deposit any outstanding DOIs to the configured registration agency
-$scheduleBag
-    ->call(function () {
-        (new \PKP\Commands\DepositDoisCommand())->execute();
-    })
-    ->hourly()
-    ->name('ojs:depositdois:without-overlapping')
-    ->withoutOverlapping()
-    ->then(function () use ($taskDao) {
-        $taskDao->updateLastRunTime('lib.pkp.classes.task.DepositDois');
-    });
 
 (new \PKP\core\PKPScheduler($scheduleBag))
     ->run();
