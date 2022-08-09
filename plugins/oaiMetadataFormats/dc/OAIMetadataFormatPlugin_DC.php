80a3019b065dcb6d8c74b4ed3c5a2a3cb064ee22
Jonas Raoni Soares da Silva <github@raoni.org>
2022-08-09 15:05:09 +0300

    pkp/pkp-lib#8139 Renamed and namespaced OAI Metadata DC plugin

--- plugins/oaiMetadataFormats/dc/OAIMetadataFormatPlugin_DC.php
+++ plugins/oaiMetadataFormats/dc/OAIMetadataFormatPlugin_DC.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * @file plugins/oaiMetadataFormats/dc/OAIMetadataFormatPlugin_DC.inc.php
+ * @file plugins/oaiMetadataFormats/dc/OAIMetadataFormatPlugin_DC.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2003-2022 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class OAIMetadataFormatPlugin_DC
@@ -14,6 +14,8 @@
  *
  * @brief dc metadata format plugin for OAI.
  */
+
+namespace APP\plugins\oaiMetadataFormats\dc;
 
 class OAIMetadataFormatPlugin_DC extends \PKP\plugins\oaiMetadataFormats\dc\PKPOAIMetadataFormatPlugin_DC
 {
