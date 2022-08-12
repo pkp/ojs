e871fc59eda2183bad45c641eb8810fade15841d
Alec Smecher <alec@smecher.bc.ca>
2022-08-12 16:33:47 -0700

    pkp/pkp-lib#6091 Convert resolver plugin to namespaces

--- plugins/gateways/resolver/index.php
+++ plugins/gateways/resolver/index.php
@@ -1,21 +1,14 @@
 <?php
 
 /**
- * @defgroup plugins_gateways_resolver Resolver Gateway Plugin
- */
-
-/**
  * @file plugins/gateways/resolver/index.php
  *
  * Copyright (c) 2014-2021 Simon Fraser University
  * Copyright (c) 2003-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
- * @ingroup plugins_gateways_resolver
  * @brief Wrapper for Resolver gateway plugin.
  *
  */
 
-require_once('ResolverPlugin.inc.php');
-
-return new ResolverPlugin();
+return new \APP\plugins\gateways\resolver\ResolverPlugin();
