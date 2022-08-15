5f6184a38a43438383fd699c528b160427113567
Alec Smecher <alec@smecher.bc.ca>
2022-08-15 15:14:26 -0700

    pkp/pkp-lib#6091 Move DC metadata plugin to namespace

--- plugins/generic/dublinCoreMeta/DublinCoreMetaPlugin.php
+++ plugins/generic/dublinCoreMeta/DublinCoreMetaPlugin.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * @file plugins/generic/dublinCoreMeta/DublinCoreMetaPlugin.inc.php
+ * @file plugins/generic/dublinCoreMeta/DublinCoreMetaPlugin.php
  *
  * Copyright (c) 2014-2021 Simon Fraser University
  * Copyright (c) 2003-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class DublinCoreMetaPlugin
- * @ingroup plugins_generic_dublinCoreMeta
- *
  * @brief Inject Dublin Core meta tags into article views to facilitate indexing.
  */
+
+namespace APP\plugins\generic\dublinCoreMeta;
 
 use APP\facades\Repo;
 use APP\submission\Submission;
