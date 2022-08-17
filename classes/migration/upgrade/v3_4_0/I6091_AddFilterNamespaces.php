88875feaa31d61f66f019cd822ab6a0874f306e8
Alec Smecher <alec@smecher.bc.ca>
2022-08-17 08:59:52 -0700

    pkp/pkp-lib#6091 Use FQCNs for metadata plugins

--- classes/migration/upgrade/v3_4_0/I6091_AddFilterNamespaces.php
+++ classes/migration/upgrade/v3_4_0/I6091_AddFilterNamespaces.php
@@ -58,6 +58,7 @@ class I6091_AddFilterNamespaces extends \PKP\migration\Migration
         foreach (self::FILTER_RENAME_MAP as $oldName => $newName) {
             DB::statement('UPDATE filters SET class_name = ? WHERE class_name = ?', [$newName, $oldName]);
         }
+        DB::statement('UPDATE filter_groups SET output_type=? WHERE output_type = ?', ['metadata::APP\plugins\metadata\dc11\schema\Dc11Schema(ARTICLE)', 'metadata::plugins.metadata.dc11.schema.Dc11Schema(ARTICLE)']);
     }
 
     /**
@@ -68,5 +69,6 @@ class I6091_AddFilterNamespaces extends \PKP\migration\Migration
         foreach (self::FILTER_RENAME_MAP as $oldName => $newName) {
             DB::statement('UPDATE filters SET class_name = ? WHERE class_name = ?', [$oldName, $newName]);
         }
+        DB::statement('UPDATE filter_groups SET output_type=? WHERE output_type = ?', ['metadata::plugins.metadata.dc11.schema.Dc11Schema(ARTICLE)', 'metadata::APP\plugins\metadata\dc11\schema\Dc11Schema(ARTICLE)']);
     }
 }
