5067d6cd056eb553a7e901dede5e177cbcdb8730
Bozana Bokan <bozana.bokan@posteo.net>
2022-09-01 12:50:10 +0200

    pkp/pkp-lib#7330 consider version in the HTML image download URL

--- plugins/generic/htmlArticleGalley/HtmlArticleGalleyPlugin.php
+++ plugins/generic/htmlArticleGalley/HtmlArticleGalleyPlugin.php
@@ -186,7 +186,7 @@ class HtmlArticleGalleyPlugin extends \PKP\plugins\GenericPlugin
             if (!$referredArticle || $referredArticle->getId() != $submissionId) {
                 $referredArticle = Repo::submission()->get($submissionId);
             }
-            $fileUrl = $request->url(null, 'article', 'download', [$referredArticle->getBestId(), $galley->getBestGalleyId(), $embeddableFile->getId(), $embeddableFile->getLocalizedData('name')], $params);
+            $fileUrl = $request->url(null, 'article', 'download', [$referredArticle->getBestId(), 'version', $galley->getData('publicationId'), $galley->getBestGalleyId(), $embeddableFile->getId(), $embeddableFile->getLocalizedData('name')], $params);
             $pattern = preg_quote(rawurlencode($embeddableFile->getLocalizedData('name')));
 
             $contents = preg_replace(
