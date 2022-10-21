328d0d62b609f8316ccaa6c571242919153c5fb4
Bozana Bokan <bozana.bokan@posteo.net>
2022-10-21 16:03:18 +0200

    pkp/pkp-lib#7318 consider issueIds param for stats API

--- api/v1/stats/publications/StatsPublicationHandler.php
+++ api/v1/stats/publications/StatsPublicationHandler.php
@@ -20,4 +20,29 @@ class StatsPublicationHandler extends \PKP\API\v1\stats\publications\PKPStatsPub
 {
     /** @var string The name of the section ids query param for this application */
     public $sectionIdsQueryParam = 'sectionIds';
+
+    /**
+     * @copydoc PKPStatsPublicationHandler::getAppSpecificParams()
+     */
+    public function getAppSpecificParams(): array
+    {
+        return ['issueIds'];
+    }
+
+    /**
+     * @copydoc PKPStatsPublicationHandler::_processAppSpecificAllowedParams()
+     */
+    protected function _processAppSpecificAllowedParams(string $requestParam, mixed $value, array &$returnParams): void
+    {
+        switch ($requestParam) {
+            case 'issueIds':
+                if (is_string($value) && str_contains($value, ',')) {
+                    $value = explode(',', $value);
+                } elseif (!is_array($value)) {
+                    $value = [$value];
+                }
+                $returnParams[$requestParam] = array_map('intval', $value);
+                break;
+        }
+    }
 }
---
+++ classes/services/queryBuilders/StatsPublicationQueryBuilder.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file classes/services/queryBuilders/StatsPublicationQueryBuilder.php
+ *
+ * Copyright (c) 2022 Simon Fraser University
+ * Copyright (c) 2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class StatsPublicationQueryBuilder
+ * @ingroup query_builders
+ *
+ * @brief Helper class to construct a query to fetch stats records from the
+ *  metrics_submission table.
+ */
+
+namespace APP\services\queryBuilders;
+
+use APP\submission\Submission;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
+use PKP\services\queryBuilders\PKPStatsPublicationQueryBuilder;
+use PKP\statistics\PKPStatisticsHelper;
+
+class StatsPublicationQueryBuilder extends PKPStatsPublicationQueryBuilder
+{
+    /** Include records for these issues */
+    protected array $issueIds = [];
+
+    /**
+     * Set the issues to get records for
+     */
+    public function filterByIssues(array $issueIds): self
+    {
+        $this->issueIds = $issueIds;
+        return $this;
+    }
+
+    /**
+     * @copydoc PKPStatsQueryBuilder::_getAppSpecificQuery()
+     */
+    protected function _getAppSpecificQuery(Builder &$q): void
+    {
+        if (!empty($this->issueIds)) {
+            $issueSubmissionIds = DB::table('publications as p')->select('p.submission_id')->distinct()
+                ->from('publications as p')
+                ->leftJoin('publication_settings as ps', 'ps.setting_name', '=', DB::raw('\'issueId\''))
+                ->where('p.status', Submission::STATUS_PUBLISHED)
+                ->whereIn('ps.setting_value', $this->issueIds);
+            $q->joinSub($issueSubmissionIds, 'is', function ($join) {
+                $join->on('metrics_submission.' . PKPStatisticsHelper::STATISTICS_DIMENSION_SUBMISSION_ID, '=', 'is.submission_id');
+            });
+        }
+    }
+}
