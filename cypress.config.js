8f87714bfdb8d93b82c814ab2f54bbd091136a17
withanage <dulip.withanage@gmail.com>
2022-10-25 10:04:53 +0200

    https://github.com/pkp/orcidProfile/issues/216 update cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
 	e2e: {
+		supportFile: false,
 		specPattern: 'cypress/tests/data/**',
 		experimentalSessionAndOrigin: true,
 		"chromeWebSecurity": false,
