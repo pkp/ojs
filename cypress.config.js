c12a18086afcb3b398233486cb1f22c5dd9180b3
withanage <dulip.withanage@gmail.com>
2022-10-26 18:22:23 +0200

    https://github.com/pkp/pkp-lib/issues/8239 Update cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,34 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-  env: {
-    contextTitles: {
-      en_US: 'Journal of Public Knowledge',
-      fr_CA: 'Journal de la connaissance du public',
-    },
-    contextDescriptions: {
-      en_US:
-        'The Journal of Public Knowledge is a peer-reviewed quarterly publication on the subject of public access to science.',
-      fr_CA:
-        "Le Journal de Public Knowledge est une publication trimestrielle évaluée par les pairs sur le thème de l'accès du public à la science.",
-    },
-    contextAcronyms: {
-      en_US: 'JPK',
-    },
-    defaultGenre: 'Article Text',
-  },
-  watchForFileChanges: false,
-  defaultCommandTimeout: 10000,
-  video: false,
-  numTestsKeptInMemory: 0,
+	env: {
+		contextTitles: {
+			en_US: 'Journal of Public Knowledge',
+			fr_CA: 'Journal de la connaissance du public',
+		},
+		contextDescriptions: {
+			en_US:
+				'The Journal of Public Knowledge is a peer-reviewed quarterly publication on the subject of public access to science.',
+			fr_CA:
+				"Le Journal de Public Knowledge est une publication trimestrielle évaluée par les pairs sur le thème de l'accès du public à la science.",
+		},
+		contextAcronyms: {
+			en_US: 'JPK',
+		},
+		defaultGenre: 'Article Text',
+	},
+	watchForFileChanges: false,
+	defaultCommandTimeout: 10000,
+	video: false,
+	numTestsKeptInMemory: 0,
 
-  e2e: {
+	e2e: {
 
-    setupNodeEvents(on, config) {
-      return require('./lib/pkp/cypress/plugins/index.js')(on, config)
-    },
-    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
-	  experimentalSessionAndOrigin: true,
-	  chromeWebSecurity: false,
-  },
+		setupNodeEvents(on, config) {
+			return require('./lib/pkp/cypress/plugins/index.js')(on, config)
+		},
+		specPattern: ['cypress/tests/**/*.cy.js', 'cypress/tests/**/**/*.cy.js'],
+		experimentalSessionAndOrigin: true,
+		chromeWebSecurity: false,
+	},
 })
