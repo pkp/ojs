1f14cd72e44486011792f42fefdf78ddd04217eb
withanage <dulip.withanage@gmail.com>
2022-12-11 15:47:14 +0100

    pkp/pkp-lib#8239 Cypress Update

---
+++ cypress.config.js
@@ -0,0 +1,32 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  env: {
+    contextTitles: {
+      en_US: 'Journal of Public Knowledge',
+      fr_CA: 'Journal de la connaissance du public',
+    },
+    contextDescriptions: {
+      en_US:
+        'The Journal of Public Knowledge is a peer-reviewed quarterly publication on the subject of public access to science.',
+      fr_CA:
+        "Le Journal de Public Knowledge est une publication trimestrielle évaluée par les pairs sur le thème de l'accès du public à la science.",
+    },
+    contextAcronyms: {
+      en_US: 'JPK',
+    },
+    defaultGenre: 'Article Text',
+  },
+  watchForFileChanges: false,
+  defaultCommandTimeout: 10000,
+  video: false,
+  numTestsKeptInMemory: 0,
+  e2e: {
+     setupNodeEvents(on, config) {
+      return require('./lib/pkp/cypress/plugins/index.js')(on, config)
+    },
+		specPattern: ['cypress/tests/data/*.cy.js', 'cypress/tests/data/60-content/*.cy.js','cypress/tests/integration/*.cy.js'],
+		experimentalSessionAndOrigin: true,
+		chromeWebSecurity: false,
+  },
+})
