ea82f13d8093f0064b5c1a5186f62693f826160a
Bozana Bokan <bozana.bokan@posteo.net>
2023-05-12 15:31:12 +0200

    pkp/pkp-lib#8987 adapt DOAJ API base URL

--- plugins/importexport/doaj/DOAJExportPlugin.php
+++ plugins/importexport/doaj/DOAJExportPlugin.php
@@ -18,8 +18,8 @@ define('DOAJ_XSD_URL', 'https://www.doaj.org/schemas/doajArticles.xsd');
 
 define('DOAJ_API_DEPOSIT_OK', 201);
 
-define('DOAJ_API_URL', 'https://doaj.org/api/v2/');
-define('DOAJ_API_URL_DEV', 'https://testdoaj.cottagelabs.com/api/v2/');
+define('DOAJ_API_URL', 'https://doaj.org/api/');
+define('DOAJ_API_URL_DEV', 'https://testdoaj.cottagelabs.com/api/');
 define('DOAJ_API_OPERATION', 'articles');
 
 use APP\core\Application;
