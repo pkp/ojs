6ac38631f79f4249eda0faa0af51a01b87ed0f8c
Vitalii Bezsheiko <vitaliybezsh@gmail.com>
2023-05-23 22:28:45 +0300

    pkp/pkp-lib#8973 Add missing email variables & remove unsupported template

--- classes/migration/upgrade/v3_4_0/I7264_UpdateEmailTemplates.php
+++ classes/migration/upgrade/v3_4_0/I7264_UpdateEmailTemplates.php
@@ -26,6 +26,8 @@ class I7264_UpdateEmailTemplates extends \PKP\migration\upgrade\v3_4_0\I7264_Upd
                 $newVariable = 'journalUrl';
             } elseif ($newVariable === 'contextSignature') {
                 $newVariable = 'journalSignature';
+            } elseif ($newVariable === 'contextAcronym') {
+                $newVariable = 'journalAcronym';
             }
         });
 
