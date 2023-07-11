fcda6ab83df6a20e4e14384e03d49b97c44dd5bd
Dimitris Efstathiou <defstat@gmail.com>
2023-07-11 12:52:59 +0300

    craft-oa-bo Initial Commit

--- config.TEMPLATE.inc.php
+++ config.TEMPLATE.inc.php
@@ -558,3 +558,6 @@ job_runner_max_memory = 80
 ; Remove failed jobs from the database after the following number of days.
 ; Remove this setting to leave failed jobs in the database.
 delete_failed_jobs_after = 180
+
+[invitations]
+expiration_days = 3
