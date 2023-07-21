dfcc2b22733559864283f910bd7c575459470f09
Dimitris Efstathiou <defstat@gmail.com>
2023-07-21 12:16:32 +0300

    craft-oa-bo Remove AccessKeyManager, AccessKeyDAO, AccessKey

--- pages/reviewer/ReviewerHandler.php
+++ pages/reviewer/ReviewerHandler.php
@@ -16,21 +16,13 @@
 
 namespace APP\pages\reviewer;
 
-use APP\core\Application;
-use APP\core\Request;
-use APP\facades\Repo;
 use APP\submission\reviewer\form\ReviewerReviewStep3Form;
 use APP\submission\Submission;
 use PKP\core\PKPRequest;
-use PKP\db\DAORegistry;
 use PKP\pages\reviewer\PKPReviewerHandler;
-use PKP\security\AccessKeyManager;
 use PKP\security\authorization\SubmissionAccessPolicy;
 use PKP\security\Role;
-use PKP\security\Validation;
-use PKP\session\SessionManager;
 use PKP\submission\reviewAssignment\ReviewAssignment;
-use PKP\submission\reviewAssignment\ReviewAssignmentDAO;
 use PKP\submission\reviewer\form\ReviewerReviewForm;
 use PKP\user\User;
 
@@ -62,12 +54,6 @@ class ReviewerHandler extends PKPReviewerHandler
      */
     public function authorize($request, &$args, $roleAssignments)
     {
-        $context = $request->getContext();
-        if ($context->getData('reviewerAccessKeysEnabled')) {
-            $this->_validateAccessKey($request);
-        }
-
-        $router = $request->getRouter();
         $this->addPolicy(new SubmissionAccessPolicy(
             $request,
             $args,
@@ -76,65 +62,6 @@ class ReviewerHandler extends PKPReviewerHandler
 
 
         return parent::authorize($request, $args, $roleAssignments);
-    }
-
-    /**
-     * Tests if the request contains a valid access token. If this is the case
-     * the regular login process will be skipped
-     *
-     * @param Request $request
-     */
-    protected function _validateAccessKey($request)
-    {
-        $accessKeyCode = $request->getUserVar('key');
-        $reviewId = $request->getUserVar('reviewId');
-        if (!($accessKeyCode && $reviewId)) {
-            return;
-        }
-
-        // Check if the user is already logged in
-        $sessionManager = SessionManager::getManager();
-        $session = $sessionManager->getUserSession();
-        if ($session->getUserId()) {
-            return;
-        }
-
-        $reviewAssignmentDao = DAORegistry::getDAO('ReviewAssignmentDAO'); /** @var ReviewAssignmentDAO $reviewAssignmentDao */
-        $reviewAssignment = $reviewAssignmentDao->getById($reviewId);
-        if (!$reviewAssignment) {
-            return;
-        } // e.g. deleted review assignment
-
-        $reviewSubmission = $this->getAuthorizedContextObject(Application::ASSOC_TYPE_SUBMISSION);
-        if (!$reviewSubmission || ($reviewSubmission->getId() != $reviewAssignment->getSubmissionId())) {
-            return;
-        } // e.g. deleted review assignment
-
-        // Validate the access key
-        $context = $request->getContext();
-        $accessKeyManager = new AccessKeyManager();
-        $accessKeyHash = $accessKeyManager->generateKeyHash($accessKeyCode);
-        $accessKey = $accessKeyManager->validateKey(
-            $context->getId(),
-            $reviewAssignment->getReviewerId(),
-            $accessKeyHash
-        );
-        if (!$accessKey) {
-            return;
-        }
-
-        // Get the reviewer user object
-        $user = Repo::user()->get($accessKey->getUserId());
-        if (!$user) {
-            return;
-        }
-
-        // Register the user object in the session
-        $reason = null;
-        if (Validation::registerUserSession($user, $reason)) {
-            $this->submission = $reviewSubmission;
-            $this->user = $user;
-        }
     }
 
     /**
