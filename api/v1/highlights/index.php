ad3b5b7b3a3bb5fa218fb3726a8a04e14dfa618d
Nate Wright <natew@notthisway.com>
2023-09-07 16:36:04 +0100

    pkp/pkp-lib#9262 Add backend support for Highlights

---
+++ api/v1/highlights/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @defgroup api_v1_highlights Highlights API requests
+ */
+
+/**
+ * @file api/v1/highlights/index.php
+ *
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2003-2023 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup api_v1_highlights
+ *
+ * @brief Handle API requests for highlights.
+ */
+
+return new \PKP\API\v1\highlights\HighlightsHandler();
