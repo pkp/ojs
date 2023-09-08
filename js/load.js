d0e794c2da82ef8b352a843783b9b376f99f1a21
MrRob <43947674+MrRob100@users.noreply.github.com>
2023-09-08 13:25:08 -0300

    adding new component to load.js

--- js/load.js
+++ js/load.js
@@ -12,6 +12,7 @@ import PkpLoad from '../lib/pkp/js/load.js';
 
 // Import controllers used by OJS
 import Container from '@/components/Container/Container.vue';
+import AdvancedSearchReviewerContainer from '@/components/Container/AdvancedSearchReviewerContainer.vue';
 import Page from '@/components/Container/Page.vue';
 import AccessPage from '@/components/Container/AccessPage.vue';
 import AddContextContainer from '@/components/Container/AddContextContainer.vue';
@@ -34,6 +35,7 @@ window.pkp = Object.assign(PkpLoad, {
 		Page,
 		AccessPage,
 		AddContextContainer,
+		AdvancedSearchReviewerContainer,
 		AdminPage,
 		ImportExportPage,
 		SettingsPage,
