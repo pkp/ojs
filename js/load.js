ca9a432466a4b6f9aebdedbb7daa2d4dedc7e443
Bozana Bokan <bozana.bokan@posteo.net>
2023-12-12 16:43:54 +0100

    pkp/pkp-lib#9666 COUNTER R5 TSV reports

--- js/load.js
+++ js/load.js
@@ -21,6 +21,7 @@ import PageOJS from '@/components/Container/PageOJS.vue';
 import AccessPage from '@/components/Container/AccessPage.vue';
 import AddContextContainer from '@/components/Container/AddContextContainer.vue';
 import AdminPage from '@/components/Container/AdminPage.vue';
+import CounterReportsPage from '@/components/Container/CounterReportsPage.vue';
 import DoiPage from '@/components/Container/DoiPageOJS.vue';
 import DecisionPage from '@/components/Container/DecisionPage.vue';
 import ImportExportPage from '@/components/Container/ImportExportPage.vue';
@@ -47,6 +48,7 @@ window.pkp = Object.assign(PkpLoad, window.pkp || {}, {
 		AdminPage,
 		AdvancedSearchReviewerContainer,
 		Container,
+		CounterReportsPage,
 		DoiPage,
 		DecisionPage,
 		ImportExportPage,
