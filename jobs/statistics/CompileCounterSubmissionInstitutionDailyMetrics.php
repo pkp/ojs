978b0601aa7b02c563e8101f398c84c782871ae4
Jonas Raoni Soares da Silva <github@raoni.org>
2024-02-22 22:27:26 +0300

    pkp/pkp-lib#9762 Used default number of retries (3x)

--- jobs/statistics/CompileCounterSubmissionInstitutionDailyMetrics.php
+++ jobs/statistics/CompileCounterSubmissionInstitutionDailyMetrics.php
@@ -25,11 +25,6 @@ use PKP\jobs\BaseJob;
 class CompileCounterSubmissionInstitutionDailyMetrics extends BaseJob
 {
     /**
-     * The number of times the job may be attempted.
-     */
-    public $tries = 1;
-
-    /**
      * Create a new job instance.
      *
      * @param string $loadId Usage stats log file name
