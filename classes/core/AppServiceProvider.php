5a080000e7e0a5c468150ed471f215637089d2ed
Touhidur Rahman <abir.ruet@gmail.com>
2024-07-09 19:57:56 +0600

    pkp/pkp-lib#9913 Removed pimple container dependency

--- classes/core/AppServiceProvider.php
+++ classes/core/AppServiceProvider.php
@@ -3,13 +3,11 @@
 /**
  * @file classes/core/AppServiceProvider.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2014-2024 Simon Fraser University
+ * Copyright (c) 2003-2024 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class AppServiceProvider
- *
- * @ingroup core
  *
  * @brief Resolves requests for application classes such as the request handler
  *   to support dependency injection
@@ -17,6 +15,11 @@
 
 namespace APP\core;
 
+use APP\services\ContextService;
+use APP\services\NavigationMenuService;
+use APP\services\StatsEditorialService;
+use APP\services\StatsIssueService;
+use APP\services\StatsPublicationService;
 use PKP\core\PKPRequest;
 
 class AppServiceProvider extends \PKP\core\AppServiceProvider
@@ -29,5 +32,20 @@ class AppServiceProvider extends \PKP\core\AppServiceProvider
         parent::register();
 
         $this->app->bind(Request::class, PKPRequest::class);
+
+        // Navigation Menu service
+        $this->app->singleton('navigationMenu', fn ($app) => new NavigationMenuService());
+
+        // Context service
+        $this->app->singleton('context', fn ($app) => new ContextService());
+
+        // Publication statistics service
+        $this->app->singleton('publicationStats', fn ($app) => new StatsPublicationService());
+
+        // Issue statistics service
+        $this->app->singleton('issueStats', fn ($app) => new StatsIssueService());
+
+        // Editorial statistics service
+        $this->app->singleton('editorialStats', fn ($app) => new StatsEditorialService());
     }
 }
