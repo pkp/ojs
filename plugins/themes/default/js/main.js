65a0025fd424ca67f965e30b086a04931cc84e97
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-07-12 01:48:25 +0800

    pkp/pkp-lib#9753 [main] Adjust chart.js config option to make it compatible with v4

--- plugins/themes/default/js/main.js
+++ plugins/themes/default/js/main.js
@@ -60,7 +60,7 @@
 	// Modify the Chart.js display options used by UsageStats plugin
 	document.addEventListener('usageStatsChartOptions.pkp', function(e) {
 		e.chartOptions.elements.line.backgroundColor = 'rgba(0, 122, 178, 0.6)';
-		e.chartOptions.elements.rectangle.backgroundColor = 'rgba(0, 122, 178, 0.6)';
+		e.chartOptions.elements.bar.backgroundColor = 'rgba(0, 122, 178, 0.6)';
 	});
 
 	// Toggle display of consent checkboxes in site-wide registration
