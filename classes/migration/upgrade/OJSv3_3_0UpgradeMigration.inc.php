4c22b42437da782699caa784676730f77ce6cd20
Jonas Raoni Soares da Silva <github@raoni.org>
2024-07-31 11:02:38 +0300

    pkp/pkp-lib#10249 Updated affected version

--- classes/migration/upgrade/OJSv3_3_0UpgradeMigration.inc.php
+++ classes/migration/upgrade/OJSv3_3_0UpgradeMigration.inc.php
@@ -167,7 +167,7 @@ class OJSv3_3_0UpgradeMigration extends Migration {
 		$primaryKeys = [];
 		try {
 			$primaryKeys = $tableDetails->getPrimaryKeyColumns();
-		} catch(Exception $e) {
+		} catch (Exception $e) {
 			foreach ($tableDetails->getIndexes() as $index) {
 					if($index->isPrimary() || $index->isUnique()) {
 						$primaryKeys = $index->getColumns();
