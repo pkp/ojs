2b5afec12d29829c6c40ccd88fcf8e4a2ee81416
Touhidur Rahman <abir.ruet@gmail.com>
2024-12-06 17:35:09 +0600

    pkp/pkp-lib#1660 default recommendation at new context add

--- classes/migration/upgrade/v3_6_0/I1660_ReviewerRecommendations.php
+++ classes/migration/upgrade/v3_6_0/I1660_ReviewerRecommendations.php
@@ -15,24 +15,12 @@
 
 namespace APP\migration\upgrade\v3_6_0;
 
+use PKP\submission\reviewer\recommendation\ReviewerRecommendation;
+
 class I1660_ReviewerRecommendations extends \PKP\migration\upgrade\v3_6_0\I1660_ReviewerRecommendations
 {
-    public const SUBMISSION_REVIEWER_RECOMMENDATION_ACCEPT = 1;
-    public const SUBMISSION_REVIEWER_RECOMMENDATION_PENDING_REVISIONS = 2;
-    public const SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_HERE = 3;
-    public const SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_ELSEWHERE = 4;
-    public const SUBMISSION_REVIEWER_RECOMMENDATION_DECLINE = 5;
-    public const SUBMISSION_REVIEWER_RECOMMENDATION_SEE_COMMENTS = 6;
-
     protected function systemDefineNonRemovableRecommendations(): array
     {
-        return [
-            static::SUBMISSION_REVIEWER_RECOMMENDATION_ACCEPT => 'reviewer.article.decision.accept',
-            static::SUBMISSION_REVIEWER_RECOMMENDATION_PENDING_REVISIONS => 'reviewer.article.decision.pendingRevisions',
-            static::SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_HERE => 'reviewer.article.decision.resubmitHere',
-            static::SUBMISSION_REVIEWER_RECOMMENDATION_RESUBMIT_ELSEWHERE => 'reviewer.article.decision.resubmitElsewhere',
-            static::SUBMISSION_REVIEWER_RECOMMENDATION_DECLINE => 'reviewer.article.decision.decline',
-            static::SUBMISSION_REVIEWER_RECOMMENDATION_SEE_COMMENTS => 'reviewer.article.decision.seeComments',
-        ];
+        return ReviewerRecommendation::seedableRecommendations();
     }
 }
