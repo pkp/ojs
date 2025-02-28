59fec1509b6cd21a7b59af0b89fc79d3dd935379
Alec Smecher <alec@smecher.bc.ca>
2025-02-28 15:00:47 -0800

    Enable minification on stable branch

--- config.TEMPLATE.inc.php
+++ config.TEMPLATE.inc.php
@@ -109,7 +109,7 @@ show_upgrade_warning = On
 
 ; Set the following parameter to off if you want to work with the uncompiled (non-minified) JavaScript
 ; source for debugging or if you are working off a development branch without compiled JavaScript.
-enable_minified = Off
+enable_minified = On
 
 ; Provide a unique site ID and OAI base URL to PKP for statistics and security
 ; alert purposes only.
