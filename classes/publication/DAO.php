232aa76f93ac0f826844ee0a9f0624c9d5ebbb70
Dimitris Efstathiou <defstat@gmail.com>
2025-05-05 14:08:50 +0300

    pkp/pkp-lib#4860 Final fixes

--- classes/publication/DAO.php
+++ classes/publication/DAO.php
@@ -30,7 +30,6 @@ class DAO extends \PKP\publication\DAO
         'submissionId' => 'submission_id',
         'status' => 'status',
         'urlPath' => 'url_path',
-        'version' => 'version',
         'doiId' => 'doi_id',
         'issueId' => 'issue_id',
         'versionStage' => 'version_stage',
