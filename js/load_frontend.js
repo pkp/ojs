f649b1ada16d66485eb6a54842d25483329f42e3
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-08-05 14:36:11 +0200

    pkp/pkp-lib#11698 Initial set of components for reader-ui

--- js/load_frontend.js
+++ js/load_frontend.js
@@ -1,5 +1,14 @@
-import * as vue from 'vue';
+// styles
+import '@/frontend/styles/pkp-variables.css';
 
-window.pkp = {
-	modules: {vue},
-};
+import PkpLoad from '../lib/pkp/js/load_frontend.js';
+import {usePageStore} from '../lib/ui-library/src/frontend/stores/pkpPageStore.js';
+window.pkp = Object.assign(PkpLoad, window.pkp || {});
+
+document.addEventListener('DOMContentLoaded', () => {
+	const pageStore = usePageStore();
+	if (pkp?._piniaData) {
+		pageStore.setData(pkp._piniaData);
+	}
+	pkp.registry.initVueFromAttributes();
+});
