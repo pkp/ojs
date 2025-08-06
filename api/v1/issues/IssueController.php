36856b5b70173e51bb1e0462965d0ebe63af00bd
Touhidur Rahman <abir.ruet@gmail.com>
2025-08-06 23:07:41 +0600

    pkp/pkp-lib#9295 separate component for issue selection

--- api/v1/issues/IssueController.php
+++ api/v1/issues/IssueController.php
@@ -18,6 +18,7 @@
 namespace APP\API\v1\issues;
 
 use APP\core\Application;
+use APP\issue\enums\IssueAssignment;
 use APP\facades\Repo;
 use APP\issue\Collector;
 use APP\security\authorization\OjsIssueRequiredPolicy;
@@ -86,6 +87,9 @@ class IssueController extends PKPBaseController
         Route::get('{issueId}', $this->get(...))
             ->name('issue.getIssue')
             ->whereNumber('issueId');
+        
+        Route::get('assignmentOptions', $this->getAssignmentOptions(...))
+            ->name('issue.getAssignmentOptions');
     }
 
     /**
@@ -265,6 +269,15 @@ class IssueController extends PKPBaseController
         );
 
         return response()->json($data, Response::HTTP_OK);
+    }
+
+    /**
+     * Get issue assignment options
+     */
+    public function getAssignmentOptions(Request $illuminateRequest): JsonResponse
+    {
+        $options = IssueAssignment::getAssignmentOptions();
+        return response()->json($options, Response::HTTP_OK);
     }
 
     protected function getUserGroups(int $contextId): Collection
