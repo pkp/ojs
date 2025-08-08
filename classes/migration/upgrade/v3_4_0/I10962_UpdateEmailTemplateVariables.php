911cf018c56cca2350f2ce2f9ff158f34eddf980
Hafsa-Naeem <hafsanaeem321@hotmail.com>
2025-08-08 13:03:33 +0500

    pkp/pkp-lib#10962 fix {$journalAcronym} in Assign Editor (Production) and standardize on context

--- classes/migration/upgrade/v3_4_0/I10962_UpdateEmailTemplateVariables.php
+++ classes/migration/upgrade/v3_4_0/I10962_UpdateEmailTemplateVariables.php
@@ -9,25 +9,53 @@
  *
  * @class I10962_UpdateEmailTemplateVariables
  *
- * @brief Remap {$contextAcronym} to {$journalAcronym} in the COPYEDIT_REQUEST email template.
+ * @brief Remap {$journalAcronym} to {$contextAcronym} in the COPYEDIT_REQUEST email template.
  */
 
 namespace APP\migration\upgrade\v3_4_0;
 
-use PKP\migration\upgrade\v3_4_0\I7264_UpdateEmailTemplates as BaseMigration;
+use Illuminate\Support\Facades\DB;
+use PKP\install\DowngradeNotSupportedException;
+use PKP\migration\Migration;
 
-class I10962_UpdateEmailTemplateVariables extends BaseMigration
+class I10962_UpdateEmailTemplateVariables extends Migration
 {
     public function up(): void
     {
-        $map = $this->oldNewVariablesMap();
-        $this->renameTemplateVariables($map);
+        DB::transaction(function () {
+            $map = [
+                'journalAcronym' => 'contextAcronym',
+                'journalName' => 'contextName',
+                'journalUrl' => 'contextUrl',
+                'journalSignature' => 'contextSignature',
+            ];
+
+            foreach ($map as $old => $new) {
+                $oldToken = '{$' . $old . '}';
+                $newToken = '{$' . $new . '}';
+                $like = '%' . $oldToken . '%';
+
+                DB::update(
+                    'UPDATE email_templates_default_data
+                       SET subject = replace(subject, ?, ?),
+                           body    = replace(body,    ?, ?)
+                     WHERE subject LIKE ? OR body LIKE ?',
+                    [$oldToken, $newToken, $oldToken, $newToken, $like, $like]
+                );
+
+                DB::update(
+                    'UPDATE email_templates_settings
+                        SET setting_value = replace(setting_value, ?, ?)
+                      WHERE setting_name IN (?, ?)
+                        AND setting_value LIKE ?',
+                    [$oldToken, $newToken, 'subject', 'body', $like]
+                );
+            }
+        });
     }
 
-    protected function oldNewVariablesMap(): array
+    public function down(): void
     {
-        return [
-            'COPYEDIT_REQUEST' => ['contextAcronym' => 'journalAcronym'],
-        ];
+        throw new DowngradeNotSupportedException(__CLASS__);
     }
 }
