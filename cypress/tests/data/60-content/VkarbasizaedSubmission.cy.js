818315075cad1da4fb5664b8e7b108228ad0247c
Touhidur Rahman <abir.ruet@gmail.com>
2025-08-15 16:35:52 +0600

    pkp/pkp-lib#9295 cypress test fixing

--- cypress/tests/data/60-content/VkarbasizaedSubmission.cy.js
+++ cypress/tests/data/60-content/VkarbasizaedSubmission.cy.js
@@ -226,9 +226,12 @@ describe('Data suite tests', function() {
 		cy.openSubmission(author.familyName);
 		cy.openWorkflowMenu('Version of Record 1.0', 'Issue')
 
-		cy.get('button:contains("Change Issue")').click();
-		cy.get('select[id="assignToIssue-issueId-control"]').select('Vol. 1 No. 2 (2014)');
-		cy.get('div[id^="assign-"] button:contains("Save")').click();
+		// update issue selection
+		cy.get('label:Contains("Assign To Current/Back Issue")').click();
+		cy.wait(500); // wait for the issues to load
+		cy.get('select[name="issueId"]').select('Vol. 1 No. 2 (2014)');
+		$('button:Contains("Save")')
+
 		cy.get('button').contains('Schedule For Publication').click();
 		cy.get('div[id^="publish-"] button:contains("Publish")').click();
 		cy.isInIssue('Antimicrobial, heavy metal resistance', 'Vol. 1 No. 2 (2014)');
