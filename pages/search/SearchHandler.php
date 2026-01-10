65a470f5c8b5abc2fc04190ac3af07535b5c8ded
Alec Smecher <alec@smecher.bc.ca>
2026-01-09 18:19:02 -0800

    pkp/pkp-lib#8920 Move SearchHandler guts into pkp-lib

--- pages/search/SearchHandler.php
+++ pages/search/SearchHandler.php
@@ -3,27 +3,20 @@
 /**
  * @file pages/search/SearchHandler.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2014-2025 Simon Fraser University
+ * Copyright (c) 2003-2025 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class SearchHandler
  *
- * @ingroup pages_search
- *
- * @brief Handle site index requests.
+ * @brief Handle search requests.
  */
 
 namespace APP\pages\search;
 
-use APP\facades\Repo;
-use APP\handler\Handler;
-use APP\search\SubmissionSearchResult;
 use APP\security\authorization\OjsJournalMustPublishPolicy;
-use APP\template\TemplateManager;
-use PKP\core\PKPRequest;
 
-class SearchHandler extends Handler
+class SearchHandler extends \PKP\pages\search\SearchHandler
 {
     /**
      * @copydoc PKPHandler::authorize()
@@ -36,117 +29,5 @@ class SearchHandler extends Handler
         }
 
         return parent::authorize($request, $args, $roleAssignments);
-    }
-
-    /**
-     * Show the search form
-     */
-    public function index($args, $request)
-    {
-        $this->validate(null, $request);
-        $this->search($args, $request);
-    }
-
-    /**
-     * Show the search form
-     */
-    public function search(array $args, PKPRequest $request): void
-    {
-        $this->validate(null, $request);
-
-        $rangeInfo = $this->getRangeInfo($request, 'search');
-        $builder = (new SubmissionSearchResult())->builderFromRequest($request, $rangeInfo);
-        $results = $builder->paginate($rangeInfo->getCount(), 'submissions', $rangeInfo->getPage());
-
-        $this->setupTemplate($request);
-
-        $templateMgr = TemplateManager::getManager($request);
-
-        // Assign the year range.
-        $collector = Repo::publication()->getCollector();
-        $context = $request->getContext();
-        $collector->filterByContextIds($context ? [$context->getId()] : null);
-        $yearRange = Repo::publication()->getDateBoundaries($collector);
-        $yearStart = substr($yearRange->min_date_published, 0, 4);
-        $yearEnd = substr($yearRange->max_date_published, 0, 4);
-
-        $this->_assignDateFromTo($request, $templateMgr);
-
-        $templateMgr->assign([
-            'query' => $builder->query,
-            'results' => $results,
-            'searchContext' => $context?->getId(),
-            'yearStart' => $yearStart,
-            'yearEnd' => $yearEnd,
-        ]);
-
-        if (!$context) {
-            $templateMgr->assign('searchableContexts', $this->getSearchableContexts());
-        }
-
-        $templateMgr->display('frontend/pages/search.tpl');
-    }
-
-    /**
-     * Assign dateFrom* and dateTo* variables to template
-     *
-     */
-    public function _assignDateFromTo(PKPRequest $request, TemplateManager &$templateMgr)
-    {
-        // Special case: publication date filters.
-        foreach (['From', 'To'] as $fromTo) {
-            $month = $request->getUserVar("date{$fromTo}Month");
-            $day = $request->getUserVar("date{$fromTo}Day");
-            $year = $request->getUserVar("date{$fromTo}Year");
-            if (empty($year)) {
-                $date = null;
-                $hasEmptyFilters = true;
-            } else {
-                $defaultMonth = ($fromTo == 'From' ? 1 : 12);
-                $defaultDay = ($fromTo == 'From' ? 1 : 31);
-                $date = date(
-                    'Y-m-d H:i:s',
-                    mktime(
-                        0,
-                        0,
-                        0,
-                        empty($month) ? $defaultMonth : $month,
-                        empty($day) ? $defaultDay : $day,
-                        $year
-                    )
-                );
-                $hasActiveFilters = true;
-            }
-
-            $templateMgr->assign([
-                "date{$fromTo}Month" => $month,
-                "date{$fromTo}Day" => $day,
-                "date{$fromTo}Year" => $year,
-                "date{$fromTo}" => $date
-            ]);
-        }
-    }
-
-    /**
-     * Setup common template variables.
-     *
-     * @param \APP\core\Request $request
-     */
-    public function setupTemplate($request)
-    {
-        parent::setupTemplate($request);
-        $templateMgr = TemplateManager::getManager($request);
-        $journal = $request->getJournal();
-        if (!$journal || !$journal->getData('restrictSiteAccess')) {
-            $templateMgr->setCacheability(TemplateManager::CACHEABILITY_PUBLIC);
-        }
-    }
-
-    protected function getSearchableContexts(): array
-    {
-        $contextService = app()->get('context');
-        return $contextService->getManySummary([
-            'isEnabled' => true,
-        ]);
     }
 }
