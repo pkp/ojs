ccb7fa75dcd45523c7ec666ce667c75c2a8c82cb
Kaitlin Newson <kaitlin.newson@gmail.com>
2026-03-11 09:57:56 -0300

    pkp/pkp-lib#11894 fix for issue galley links showing restricted when not logged in

--- classes/mail/variables/IssueEmailVariable.php
+++ classes/mail/variables/IssueEmailVariable.php
@@ -78,7 +78,7 @@ class IssueEmailVariable extends Variable
         $request = Application::get()->getRequest();
         $templateMgr = TemplateManager::getManager($request);
 
-        IssueHandler::_setupIssueTemplate($request, $this->issue, $this->getContext(), true, false);
+        IssueHandler::setupIssueTemplate($request, $this->issue, $this->getContext(), false);
 
         $templateMgr->assign([
             'includeIssuePublishDate' => false,
