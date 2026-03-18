f9cdb7fdd8738caa30a8164cc66fe8a993e7ebb7
Alec Smecher <alec@smecher.bc.ca>
2026-03-18 09:26:55 -0700

    pkp/pkp-lib#12468 Make the plugin gallery URL configurable (configuration change)

--- config.TEMPLATE.inc.php
+++ config.TEMPLATE.inc.php
@@ -320,6 +320,11 @@ allowed_title_html = "b,i,u,sup,sub"
 ;  off: Plugins cannot be installed.
 allow_plugin_install = on
 
+; The URLs to use to fetch the plugin gallery plugin list
+; BEWARE: You should not extend the gallery with custom plugin gallery listing that collide with the official ones!
+;plugin_gallery_urls = '["https://pkp.sfu.ca/ojs/xml/plugins.xml"]'
+
+
 ;;;;;;;;;;;;;;;;;;
 ; Email Settings ;
 ;;;;;;;;;;;;;;;;;;
