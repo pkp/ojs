763dd403a3266047af4582f907440eb8f62dfdd8
Erik Hanson <erik@erikhanson.dev>
2026-05-19 14:45:10 -0700

    pkp/pkp-lib#12673 Include reviewer names and review status on article listings

--- classes/search/SubmissionSearchResult.php
+++ classes/search/SubmissionSearchResult.php
@@ -15,16 +15,15 @@ namespace APP\search;
 use APP\core\Application;
 use APP\facades\Repo;
 use APP\issue\IssueAction;
+use Illuminate\Support\LazyCollection;
 
 class SubmissionSearchResult extends \PKP\search\SubmissionSearchResult
 {
     /**
      * @see Illuminate\Database\Eloquent\HasCollection.
      */
-    public function newCollection(array $models = [])
+    public function newCollection(array $models = []): LazyCollection
     {
-        $collection = parent::newCollection($models);
-
         $issueAction = new IssueAction();
         $issueCache = [];
         $issueAvailabilityCache = [];
@@ -32,21 +31,29 @@ class SubmissionSearchResult extends \PKP\search\SubmissionSearchResult
         $request = Application::get()->getRequest();
         $user = $request->getUser();
 
-        foreach ($collection as $result) {
-            $submissionId = $result['submission']->getId();
-            $issueId = $result['currentPublication']->getData('issueId');
-            $issue = $issueId ? ($issueCache[$issueId] ??= Repo::issue()->get($issueId)) : null;
+        return parent::newCollection($models)
+            ->map(function (array $result) use (
+                $issueAction,
+                &$issueCache,
+                &$issueAvailabilityCache,
+                $request,
+                $user,
+            ) {
+                $submissionId = $result['submission']->getId();
+                $issueId = $result['currentPublication']->getData('issueId');
+                $issue = $issueId ? ($issueCache[$issueId] ??= Repo::issue()->get($issueId)) : null;
 
-            if ($issue) {
-                $issueAvailabilityCache[$issueId] ??= !$issueAction->subscriptionRequired($issue, $result['context'])
-                    || $issueAction->subscribedUser($user, $result['context'], $issueId, $submissionId)
-                    || $issueAction->subscribedDomain($request, $result['context'], $issueId, $submissionId);
-            }
-            yield [
-                ...$result,
-                'issue' => $issue,
-                'issueAvailable' => $issue ? $issueAvailabilityCache[$issueId] : true,
-            ];
-        }
+                if ($issue) {
+                    $issueAvailabilityCache[$issueId] ??= !$issueAction->subscriptionRequired($issue, $result['context'])
+                        || $issueAction->subscribedUser($user, $result['context'], $issueId, $submissionId)
+                        || $issueAction->subscribedDomain($request, $result['context'], $issueId, $submissionId);
+                }
+
+                return [
+                    ...$result,
+                    'issue' => $issue,
+                    'issueAvailable' => $issue ? $issueAvailabilityCache[$issueId] : true,
+                ];
+            });
     }
 }
