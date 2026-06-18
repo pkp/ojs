5dff989f96c8b454ab05aaa5a833d97510d5c601
Nate Wright <natew@notthisway.com>
2026-06-18 13:14:22 +0100

    Add: homepage blocks registry

---
+++ classes/view/HomepageBlocksRegistry.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file classes/view/MetadataBlockRepository.php
+ *
+ * Copyright (c) 2026 Simon Fraser University
+ * Copyright (c) 2026 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class Repository
+ *
+ * @brief A repository to register and load metadata blocks.
+ */
+
+namespace APP\view;
+
+use PKP\view\HomepageBlock;
+
+class HomepageBlocksRegistry extends \PKP\view\HomepageBlocksRegistry
+{
+    protected function registerDefaultBlocks(): void
+    {
+        parent::registerDefaultBlocks();
+
+        $this->register(
+            new HomepageBlock(
+                component: 'homepage.issue-summary',
+                title: __('manager.homepageBlocks.issueSummary'),
+            )
+        );
+    }
+}
